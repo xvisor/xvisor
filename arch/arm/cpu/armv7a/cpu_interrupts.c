669dfeca9df53d2f8665972d457665010da33135
Anup Patel <anup@brainfault.org>
2011-07-27 12:03:03 +0530

    [ARM] Updated ARM interrupt processing based on scheduler changes.
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- arch/arm/cpu/armv7a/cpu_interrupts.c
+++ arch/arm/cpu/armv7a/cpu_interrupts.c
@@ -27,8 +27,6 @@
 #include <vmm_stdio.h>
 #include <vmm_guest.h>
 #include <vmm_devtree.h>
-#include <vmm_host_irq.h>
-#include <vmm_vcpu_irq.h>
 #include <vmm_scheduler.h>
 #include <cpu_inline_asm.h>
 #include <cpu_vcpu_emulate_arm.h>
@@ -65,7 +63,7 @@ void do_undefined_instruction(vmm_user_regs_t * uregs)
 		vmm_printf("%s: error %d\n", __func__, rc);
 	}
 
-	vmm_vcpu_irq_process(uregs);
+	vmm_scheduler_irq_process(CPU_UNDEF_INST_IRQ, uregs, FALSE, TRUE);
 }
 
 void do_software_interrupt(vmm_user_regs_t * uregs)
@@ -96,7 +94,7 @@ void do_software_interrupt(vmm_user_regs_t * uregs)
 		vmm_printf("%s: error %d\n", __func__, rc);
 	}
 
-	vmm_vcpu_irq_process(uregs);
+	vmm_scheduler_irq_process(CPU_SOFT_IRQ, uregs, FALSE, TRUE);
 }
 
 void do_prefetch_abort(vmm_user_regs_t * uregs)
@@ -164,7 +162,7 @@ void do_prefetch_abort(vmm_user_regs_t * uregs)
 				__func__, vcpu->num, ifar, ifsr);
 	}
 
-	vmm_vcpu_irq_process(uregs);
+	vmm_scheduler_irq_process(CPU_PREFETCH_ABORT_IRQ, uregs, FALSE, TRUE);
 }
 
 void do_data_abort(vmm_user_regs_t * uregs)
@@ -239,7 +237,7 @@ void do_data_abort(vmm_user_regs_t * uregs)
 				__func__, vcpu->num, dfar, dfsr);
 	}
 
-	vmm_vcpu_irq_process(uregs);
+	vmm_scheduler_irq_process(CPU_DATA_ABORT_IRQ, uregs, FALSE, TRUE);
 }
 
 void do_not_used(vmm_user_regs_t * uregs)
@@ -249,16 +247,12 @@ void do_not_used(vmm_user_regs_t * uregs)
 
 void do_irq(vmm_user_regs_t * uregs)
 {
-	vmm_host_irq_exec(CPU_EXTERNAL_IRQ, uregs);
-
-	vmm_vcpu_irq_process(uregs);
+	vmm_scheduler_irq_process(CPU_EXTERNAL_IRQ, uregs, TRUE, TRUE);
 }
 
 void do_fiq(vmm_user_regs_t * uregs)
 {
-	vmm_host_irq_exec(CPU_EXTERNAL_FIQ, uregs);
-
-	vmm_vcpu_irq_process(uregs);
+	vmm_scheduler_irq_process(CPU_EXTERNAL_FIQ, uregs, TRUE, TRUE);
 }
 
 int vmm_cpu_irq_setup(void)
