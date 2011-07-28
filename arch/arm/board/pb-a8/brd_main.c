e0edeab341387681419d9081b10ffb05c157f24b
Anup Patel <anup@brainfault.org>
2011-07-28 11:56:22 +0530

    [ARM] Updated ARM code to support reset and shutdown commands.
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- arch/arm/board/pb-a8/brd_main.c
+++ arch/arm/board/pb-a8/brd_main.c
@@ -27,11 +27,14 @@
 #include <vmm_devtree.h>
 #include <vmm_libfdt.h>
 #include <vmm_devdrv.h>
+#include <vmm_host_io.h>
 #include <vmm_host_aspace.h>
 #include <pba8_board.h>
 #include <realview/realview_timer.h>
 
 extern u32 dt_blob_start;
+virtual_addr_t pba8_csr_base;
+virtual_addr_t pba8_sctl_base;
 
 int vmm_devtree_populate(vmm_devtree_node_t ** root,
 			 char **string_buffer, size_t * string_buffer_size)
@@ -58,6 +61,25 @@ int vmm_board_getclock(vmm_devtree_node_t * node, u32 * clock)
 	return VMM_OK;
 }
 
+int vmm_board_reset(void)
+{
+	/* FIXME: Write 0x1 to RESETCTL just like linux does */
+	vmm_writel(0x100, 
+		   (void *)(pba8_csr_base + REALVIEW_SYS_RESETCTL_OFFSET));
+#if 0
+	vmm_writel(REALVIEW_SYS_CTRL_RESET_CONFIGCLR, 
+		   (void *)(pba8_csr_base + REALVIEW_SYS_RESETCTL_OFFSET));
+#endif
+
+	return VMM_OK;
+}
+
+int vmm_board_shutdown(void)
+{
+	/* FIXME: TBD */
+	return VMM_OK;
+}
+
 int vmm_board_early_init(void)
 {
 	/*
@@ -73,47 +95,50 @@ int vmm_board_final_init(void)
 {
 	int rc;
 	vmm_devtree_node_t *node;
-	virtual_addr_t pba8_sctl_base, pba8_timer_base;
+	virtual_addr_t timer_base;
 
 	/* All VMM API's are available here */
 	/* We can register a Board specific resource here */
 
-	/* Initialize Realview timers */
+	/* Map control registers */
+	pba8_csr_base = vmm_host_iomap(REALVIEW_CSR_BASE, 0x1000);
 	pba8_sctl_base = vmm_host_iomap(REALVIEW_SCTL_BASE, 0x1000);
-	pba8_timer_base = vmm_host_iomap(REALVIEW_PBA8_TIMER0_1_BASE, 0x1000);
-	rc = realview_timer_init(pba8_sctl_base, pba8_timer_base,
+
+	/* Initialize Realview timers */
+	timer_base = vmm_host_iomap(REALVIEW_PBA8_TIMER0_1_BASE, 0x1000);
+	rc = realview_timer_init(pba8_sctl_base, timer_base,
 				 REALVIEW_TIMER1_EnSel);
 	if (rc) {
 		return rc;
 	}
-	rc = realview_timer_init(pba8_sctl_base, pba8_timer_base + 0x20,
+	rc = realview_timer_init(pba8_sctl_base, timer_base + 0x20,
 				 REALVIEW_TIMER2_EnSel);
 	if (rc) {
 		return rc;
 	}
-	rc = vmm_host_iounmap(pba8_timer_base, 0x1000);
+	rc = vmm_host_iounmap(timer_base, 0x1000);
 	if (rc) {
 		return rc;
 	}
-	pba8_timer_base = vmm_host_iomap(REALVIEW_PBA8_TIMER2_3_BASE, 0x1000);
-	rc = realview_timer_init(pba8_sctl_base, pba8_timer_base,
+	timer_base = vmm_host_iomap(REALVIEW_PBA8_TIMER2_3_BASE, 0x1000);
+	rc = realview_timer_init(pba8_sctl_base, timer_base,
 				 REALVIEW_TIMER3_EnSel);
 	if (rc) {
 		return rc;
 	}
-	rc = realview_timer_init(pba8_sctl_base, pba8_timer_base + 0x20,
+	rc = realview_timer_init(pba8_sctl_base, timer_base + 0x20,
 				 REALVIEW_TIMER4_EnSel);
 	if (rc) {
 		return rc;
 	}
-	rc = vmm_host_iounmap(pba8_timer_base, 0x1000);
+	rc = vmm_host_iounmap(timer_base, 0x1000);
 	if (rc) {
 		return rc;
 	}
-	rc = vmm_host_iounmap(pba8_sctl_base, 0x1000);
-	if (rc) {
-		return rc;
-	}
+
+	/* Unlock Lockable reigsters */
+	vmm_writel(REALVIEW_SYS_LOCKVAL, 
+		   (void *)(pba8_csr_base + REALVIEW_SYS_LOCK_OFFSET));
 
 	/* Do Probing using device driver framework */
 	node = vmm_devtree_getnode(VMM_DEVTREE_PATH_SEPRATOR_STRING
