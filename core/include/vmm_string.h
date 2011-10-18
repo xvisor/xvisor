94379b21e1d2f5e573ae7f27c1a2cb5755c3beee
Anup Patel <anup@brainfault.org>
2011-10-18 19:50:59 +0530

    [CORE] Removed unused mem* functions.
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- core/include/vmm_string.h
+++ core/include/vmm_string.h
@@ -38,8 +38,4 @@ void *vmm_memcpy(void *dest, const void *src, size_t count);
 void *vmm_memset(void *dest, int c, size_t count);
 int vmm_memcmp(const void *s1, const void *s2, size_t count);
 
-void *memcpy(void *dest, const void *src, size_t count);
-void *memset(void *dest, int c, size_t count);
-int memcmp(const void *s1, const void *s2, size_t count);
-
 #endif
