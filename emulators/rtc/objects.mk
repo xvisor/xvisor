68bbcca9763d81c6da4bb6f0cc8b6181761942b3
Anup Patel <anup@brainfault.org>
2012-02-11 08:35:33 +0530

    [EMULATORS] Added PrimeCell PL031 RTC emulator.
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- emulators/rtc/objects.mk
+++ emulators/rtc/objects.mk
@@ -16,15 +16,10 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 #
-# @file openconf.cfg
+# @file objects.mk
 # @author Anup Patel (anup@brainfault.org)
-# @brief config file for emulator options
+# @brief list of rtc emulator objects
 # */
 
-menu "Device Emulators"
-     source "emulators/sys/openconf.cfg"
-     source "emulators/pic/openconf.cfg"
-     source "emulators/timer/openconf.cfg"
-     source "emulators/serial/openconf.cfg"
-     source "emulators/rtc/openconf.cfg"
-endmenu
+emulators-objs-$(CONFIG_EMU_RTC_PL031)+= rtc/pl031.o
+
