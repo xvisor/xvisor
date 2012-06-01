e35eebb963c6da50a784de6068b36ed824e47d40
Anup Patel <anup@brainfault.org>
2012-06-01 10:17:38 +0530

    [ARM] arm32: Added macros & defines accessing cache configuration.
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- arch/arm/cpu/arm32/include/cpu_inline_asm.h
+++ arch/arm/cpu/arm32/include/cpu_inline_asm.h
@@ -66,6 +66,26 @@
 
 #endif
 
+#define read_ccsidr()		({ u32 rval; asm volatile(\
+				" mrc     p15, 1, %0, c0, c0, 0\n\t" \
+				: "=r" (rval) : : "memory", "cc"); rval;})
+
+#define read_csselr()		({ u32 rval; asm volatile(\
+				" mrc     p15, 2, %0, c0, c0, 0\n\t" \
+				: "=r" (rval) : : "memory", "cc"); rval;})
+
+#define write_csselr(val)	asm volatile(\
+				" mcr     p15, 2, %0, c0, c0, 0\n\t" \
+				:: "r" ((val)) : "memory", "cc")
+
+#define read_clidr()		({ u32 rval; asm volatile(\
+				" mrc     p15, 1, %0, c0, c0, 1\n\t" \
+				: "=r" (rval) : : "memory", "cc"); rval;})
+
+#define read_ctr()		({ u32 rval; asm volatile(\
+				" mrc     p15, 0, %0, c0, c0, 1\n\t" \
+				: "=r" (rval) : : "memory", "cc"); rval;})
+
 #define read_mpidr()		({ u32 rval; asm volatile(\
 				" mrc     p15, 0, %0, c0, c0, 5\n\t" \
 				: "=r" (rval) : : "memory", "cc"); rval;})
