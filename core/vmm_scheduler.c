50ba5c35386e66c1eb38b419be2d3188ee76a77b
Anup Patel <anup@brainfault.org>
2012-06-21 19:15:38 +0530

    [CORE] Remove ev_name and idle_vcpu_name from struct vmm_scheduler_ctrl
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- core/vmm_scheduler.c
+++ core/vmm_scheduler.c
@@ -43,11 +43,9 @@ struct vmm_scheduler_ctrl {
 	void * rq;
 	struct vmm_vcpu *current_vcpu;
 	struct vmm_vcpu *idle_vcpu;
-	char idle_vcpu_name[16];
 	u8 idle_vcpu_stack[IDLE_VCPU_STACK_SZ];
 	bool irq_context;
 	bool yield_on_irq_exit;
-	char ev_name[16];
 	struct vmm_timer_event *ev;
 };
 
@@ -313,6 +311,7 @@ static void idle_orphan(void)
 int __init vmm_scheduler_init(void)
 {
 	int rc;
+	char vcpu_name[32], ev_name[32];
 	u32 cpu = vmm_smp_processor_id();
 	struct vmm_scheduler_ctrl *schedp = &this_cpu(sched);
 
@@ -335,8 +334,8 @@ int __init vmm_scheduler_init(void)
 	schedp->yield_on_irq_exit = FALSE;
 
 	/* Create timer event and start it. (Per Host CPU) */
-	vmm_sprintf(schedp->ev_name, "sched/%d", cpu);
-	schedp->ev = vmm_timer_event_create(schedp->ev_name, 
+	vmm_sprintf(ev_name, "sched/%d", cpu);
+	schedp->ev = vmm_timer_event_create(ev_name, 
 					    &vmm_scheduler_timer_event, 
 					    NULL);
 	if (!schedp->ev) {
@@ -344,9 +343,8 @@ int __init vmm_scheduler_init(void)
 	}
 
 	/* Create idle orphan vcpu with default time slice. (Per Host CPU) */
-	vmm_sprintf(schedp->idle_vcpu_name, "idle/%d", cpu);
-	schedp->idle_vcpu = vmm_manager_vcpu_orphan_create(
-	schedp->idle_vcpu_name,
+	vmm_sprintf(vcpu_name, "idle/%d", cpu);
+	schedp->idle_vcpu = vmm_manager_vcpu_orphan_create(vcpu_name,
 	(virtual_addr_t)&idle_orphan,
 	(virtual_addr_t)&schedp->idle_vcpu_stack[IDLE_VCPU_STACK_SZ - 4],
 	IDLE_VCPU_PRIORITY, IDLE_VCPU_TIMESLICE);
