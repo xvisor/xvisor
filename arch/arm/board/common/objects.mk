c9bfd521b9229a161861a3ab0d8852624e4e8013
Anup Patel <anup@brainfault.org>
2012-06-24 19:16:48 +0530

    [ARM] vexpress-a9: Added support for local timers for per host CPU clockchip.
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- arch/arm/board/common/objects.mk
+++ arch/arm/board/common/objects.mk
@@ -23,4 +23,5 @@
 
 board-common-objs-$(CONFIG_ARM_GIC)+=gic.o
 board-common-objs-$(CONFIG_ARM_TIMER_SP804)+=sp804_timer.o
+board-common-objs-$(CONFIG_ARM_TWD)+=smp_twd.o
 board-common-objs-$(CONFIG_ARM_PL190)+=pl190.o
