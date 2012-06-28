6ee2e8af25a21b880e87ebe54c5ac834e2937f54
Anup Patel <anup@brainfault.org>
2012-06-28 19:17:34 +0530

    [CORE] Updated spinlock APIs to prevent compiler re-ordering on UP machines
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- core/include/vmm_spinlocks.h
+++ core/include/vmm_spinlocks.h
@@ -65,7 +65,7 @@ extern void vmm_scheduler_preempt_enable(void);
  *  PROTOTYPE: bool vmm_spin_lock_check(vmm_spinlock_t * lock)
  */
 #if defined(CONFIG_SMP)
-#define vmm_spin_lock_check(lock)	arch_spin_lock_check((lock)->__tlock)
+#define vmm_spin_lock_check(lock)	arch_spin_lock_check(&(lock)->__tlock)
 #else
 #define vmm_spin_lock_check(lock)	FALSE
 #endif
@@ -81,6 +81,7 @@ extern void vmm_scheduler_preempt_enable(void);
 #else
 #define vmm_spin_lock(lock)		do { \
 					vmm_scheduler_preempt_disable(); \
+					(void)(lock); \
 					} while (0)
 #endif
 
@@ -95,6 +96,7 @@ extern void vmm_scheduler_preempt_enable(void);
 #else
 #define vmm_spin_unlock(lock)		do { \
 					vmm_scheduler_preempt_enable(); \
+					(void)(lock); \
 					} while (0)
 #endif
 
@@ -109,6 +111,7 @@ extern void vmm_scheduler_preempt_enable(void);
 #else
 #define vmm_spin_lock_irq(lock) 	do { \
 					arch_cpu_irq_disable(); \
+					(void)(lock); \
 					} while (0)
 #endif
 
@@ -123,6 +126,7 @@ extern void vmm_scheduler_preempt_enable(void);
 #else
 #define vmm_spin_unlock_irq(lock) 	do { \
 					arch_cpu_irq_enable(); \
+					(void)(lock); \
 					} while (0)
 #endif
 
@@ -139,6 +143,7 @@ extern void vmm_scheduler_preempt_enable(void);
 #define vmm_spin_lock_irqsave(lock, flags) \
 					do { \
 					flags = arch_cpu_irq_save(); \
+					(void)(lock); \
 					} while (0)
 #endif
 
@@ -156,6 +161,7 @@ extern void vmm_scheduler_preempt_enable(void);
 #define vmm_spin_unlock_irqrestore(lock, flags) \
 					do { \
 					arch_cpu_irq_restore(flags); \
+					(void)(lock); \
 					} while (0)
 #endif
 
