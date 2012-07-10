821fe754a5531cfb8464c62313fe6efb1fac50f2
Anup Patel <anup@brainfault.org>
2012-07-10 20:01:22 +0530

    [ARM] arm32: Remove SMP fix in SCTLR write emulation
    With recent changes in VTLB, SCTLR write, and DACR write emulation we can boot SMP linux guest on QEMU and BeagleBoard-xM
    
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- arch/arm/cpu/arm32/cpu_vcpu_cp15.c
+++ arch/arm/cpu/arm32/cpu_vcpu_cp15.c
@@ -1269,18 +1269,13 @@ bool cpu_vcpu_cp15_write(struct vmm_vcpu * vcpu,
 
 			/* ??? Lots of these bits are not implemented.  */
 
-			if (arm_feature(vcpu, ARM_FEATURE_V7MP)) {
-				/* For SMP guests always flush entire VTLB.
-				 * This is for stability. ???
-				 */
-				cpu_vcpu_cp15_vtlb_flush(vcpu);
-			} else if (tmp != (arm_priv(vcpu)->cp15.c1_sctlr & SCTLR_MMU_MASK)) {
-				/* For single-core guests flush VTLB only when
-				 * SCTLR changes
+			if (tmp != (arm_priv(vcpu)->cp15.c1_sctlr & SCTLR_MMU_MASK)) {
+				/* When MMU related bits of SCTLR changes flush
+				 * entire VTLB
 				 */
 				cpu_vcpu_cp15_vtlb_flush(vcpu);
 			} else {
-				/* If no change in SCTLR then flush 
+				/* For rest of the SCTLR changes flush 
 				 * non-global pages from VTLB
 				 */
 				cpu_vcpu_cp15_vtlb_flush_ng(vcpu);
