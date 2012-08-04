4f6f5db90d59bd6d906fe14a919eb57c0d6628b1
Jean-Christophe DUBOIS <jcd@tribudubois.net>
2012-08-04 09:47:56 +0530

    [ARM] Fix build failure for arm32 & arm32ve
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- arch/arm/cpu/arm32ve/include/arch_elf.h
+++ arch/arm/cpu/arm32ve/include/arch_elf.h
@@ -81,17 +81,16 @@
 
 struct elf32_hdr;
 struct elf32_shdr;
-typedef struct elf32_shdr Elf32_Shdr;
 
 int arch_elf_check_hdr(const struct elf32_hdr *x);
 
-int arch_elf_apply_relocate(Elf32_Shdr *sechdrs, 
+int arch_elf_apply_relocate(struct elf32_shdr *sechdrs, 
 			    const char *strtab, 
 			    unsigned int symindex,
 			    unsigned int relindex, 
 			    struct vmm_module *mod);
 
-int arch_elf_apply_relocate_add(Elf32_Shdr *sechdrs, 
+int arch_elf_apply_relocate_add(struct elf32_shdr *sechdrs, 
 				const char *strtab,
 				unsigned int symindex, 
 				unsigned int relsec, 
