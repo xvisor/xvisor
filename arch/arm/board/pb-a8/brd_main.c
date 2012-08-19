bc6c53422a8e919cd1ddacdae1afaad8e824f80d
Anup Patel <anup@brainfault.org>
2012-08-19 16:28:09 +0530

    [ARM] pb-a8: setup CLCD system data
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- arch/arm/board/pb-a8/brd_main.c
+++ arch/arm/board/pb-a8/brd_main.c
@@ -30,6 +30,9 @@
 #include <vmm_stdio.h>
 #include <vmm_chardev.h>
 #include <rtc/vmm_rtcdev.h>
+#include <linux/amba/bus.h>
+#include <linux/amba/clcd.h>
+#include <versatile/clcd.h>
 #include <libfdt.h>
 #include <realview_plat.h>
 #include <pba8_board.h>
@@ -134,6 +137,99 @@ int __init arch_board_early_init(void)
 	return 0;
 }
 
+/*
+ * CLCD support.
+ */
+#define SYS_CLCD_NLCDIOON	(1 << 2)
+#define SYS_CLCD_VDDPOSSWITCH	(1 << 3)
+#define SYS_CLCD_PWR3V5SWITCH	(1 << 4)
+#define SYS_CLCD_ID_MASK	(0x1f << 8)
+#define SYS_CLCD_ID_SANYO_3_8	(0x00 << 8)
+#define SYS_CLCD_ID_UNKNOWN_8_4	(0x01 << 8)
+#define SYS_CLCD_ID_EPSON_2_2	(0x02 << 8)
+#define SYS_CLCD_ID_SANYO_2_5	(0x07 << 8)
+#define SYS_CLCD_ID_VGA		(0x1f << 8)
+
+/*
+ * Disable all display connectors on the interface module.
+ */
+static void realview_clcd_disable(struct clcd_fb *fb)
+{
+	void *sys_clcd = (void *)pba8_sys_base + REALVIEW_SYS_CLCD_OFFSET;
+	u32 val;
+
+	val = vmm_readl(sys_clcd);
+	val &= ~SYS_CLCD_NLCDIOON | SYS_CLCD_PWR3V5SWITCH;
+	vmm_writel(val, sys_clcd);
+}
+
+/*
+ * Enable the relevant connector on the interface module.
+ */
+static void realview_clcd_enable(struct clcd_fb *fb)
+{
+	void *sys_clcd = (void *)pba8_sys_base + REALVIEW_SYS_CLCD_OFFSET;
+	u32 val;
+
+	/*
+	 * Enable the PSUs
+	 */
+	val = vmm_readl(sys_clcd);
+	val |= SYS_CLCD_NLCDIOON | SYS_CLCD_PWR3V5SWITCH;
+	vmm_writel(val, sys_clcd);
+}
+
+/*
+ * Detect which LCD panel is connected, and return the appropriate
+ * clcd_panel structure.  Note: we do not have any information on
+ * the required timings for the 8.4in panel, so we presently assume
+ * VGA timings.
+ */
+static int realview_clcd_setup(struct clcd_fb *fb)
+{
+	void *sys_clcd = (void *)pba8_sys_base + REALVIEW_SYS_CLCD_OFFSET;
+	const char *panel_name, *vga_panel_name;
+	unsigned long framesize;
+	u32 val;
+
+	/* XVGA, 16bpp 
+	 * (Assuming machine is always realview-pb-a8 and not realview-eb)
+	 */
+	framesize = 1024 * 768 * 2;
+	vga_panel_name = "XVGA";
+
+	val = vmm_readl(sys_clcd) & SYS_CLCD_ID_MASK;
+	if (val == SYS_CLCD_ID_SANYO_3_8)
+		panel_name = "Sanyo TM38QV67A02A";
+	else if (val == SYS_CLCD_ID_SANYO_2_5)
+		panel_name = "Sanyo QVGA Portrait";
+	else if (val == SYS_CLCD_ID_EPSON_2_2)
+		panel_name = "Epson L2F50113T00";
+	else if (val == SYS_CLCD_ID_VGA)
+		panel_name = vga_panel_name;
+	else {
+		vmm_printf("CLCD: unknown LCD panel ID 0x%08x, using VGA\n", val);
+		panel_name = vga_panel_name;
+	}
+
+	fb->panel = versatile_clcd_get_panel(panel_name);
+	if (!fb->panel)
+		return VMM_EINVALID;
+
+	return versatile_clcd_setup(fb, framesize);
+}
+
+struct clcd_board clcd_system_data = {
+	.name		= "PB-A8",
+	.caps		= CLCD_CAP_ALL,
+	.check		= clcdfb_check,
+	.decode		= clcdfb_decode,
+	.disable	= realview_clcd_disable,
+	.enable		= realview_clcd_enable,
+	.setup		= realview_clcd_setup,
+	.remove		= versatile_clcd_remove,
+};
+
 int __init arch_board_final_init(void)
 {
 	int rc;
@@ -153,11 +249,20 @@ int __init arch_board_final_init(void)
 	vmm_writel(REALVIEW_SYS_LOCKVAL, 
 		   (void *)(pba8_sys_base + REALVIEW_SYS_LOCK_OFFSET));
 
+	/* Setup CLCD system data (before probing) */
+	node = vmm_devtree_getnode(VMM_DEVTREE_PATH_SEPARATOR_STRING
+				   VMM_DEVTREE_HOSTINFO_NODE_NAME
+				   VMM_DEVTREE_PATH_SEPARATOR_STRING "nbridge"
+				   VMM_DEVTREE_PATH_SEPARATOR_STRING "sbridge"
+				   VMM_DEVTREE_PATH_SEPARATOR_STRING "clcd");
+	if (node) {
+		node->system_data = &clcd_system_data;
+	}
+
 	/* Do Probing using device driver framework */
 	node = vmm_devtree_getnode(VMM_DEVTREE_PATH_SEPARATOR_STRING
 				   VMM_DEVTREE_HOSTINFO_NODE_NAME
 				   VMM_DEVTREE_PATH_SEPARATOR_STRING "nbridge");
-
 	if (!node) {
 		return VMM_ENOTAVAIL;
 	}
