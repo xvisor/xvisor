f922500d84f3ce33c2890554741696c8feeb34ac
Anup Patel <anup@brainfault.org>
2012-08-21 17:56:19 +0530

    [ARM] Add clocking library for versatile platform.
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- arch/arm/board/common/versatile/objects.mk
+++ arch/arm/board/common/versatile/objects.mk
@@ -21,5 +21,6 @@
 # @brief list of versatile platform objects.
 # */
 
+board-common-objs-$(CONFIG_VERSATILE_CLOCK)+= versatile/clock.o
 board-common-objs-$(CONFIG_VERSATILE_CLCD)+= versatile/clcd.o
 
