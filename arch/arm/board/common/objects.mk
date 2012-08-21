ae436c219087802683d342c853d8b0d97b665eb5
Anup Patel <anup@brainfault.org>
2012-08-21 17:40:50 +0530

    [ARM] Added icst clock generation library.
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- arch/arm/board/common/objects.mk
+++ arch/arm/board/common/objects.mk
@@ -25,3 +25,4 @@ board-common-objs-$(CONFIG_ARM_GIC)+=gic.o
 board-common-objs-$(CONFIG_ARM_TIMER_SP804)+=sp804_timer.o
 board-common-objs-$(CONFIG_ARM_TWD)+=smp_twd.o
 board-common-objs-$(CONFIG_ARM_PL190)+=pl190.o
+board-common-objs-$(CONFIG_ICST)+=icst.o
