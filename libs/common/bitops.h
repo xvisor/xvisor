422f25923405af7627a0cb37d3868409e3698730
Anup Patel <anup@brainfault.org>
2012-08-24 19:07:50 +0530

    [LIBS] Support linux compatible ffs() and __ffs() operations.
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- libs/common/bitops.h
+++ libs/common/bitops.h
@@ -45,12 +45,49 @@
 #define BIT_WORD_OFFSET(bit)		((bit) & (BITS_PER_LONG - 1))
 
 /**
- * ffs - find first bit in word.
+ * ffs - Find first bit set
+ * @x: the word to search
+ *
+ * This is defined the same way as
+ * the libc and compiler builtin ffs routines, therefore
+ * differs in spirit from the above ffz (man ffs).
+ */
+static inline int ffs(int x)
+{
+	int r = 1;
+
+	if (!x)
+		return 0;
+	if (!(x & 0xffff)) {
+		x >>= 16;
+		r += 16;
+	}
+	if (!(x & 0xff)) {
+		x >>= 8;
+		r += 8;
+	}
+	if (!(x & 0xf)) {
+		x >>= 4;
+		r += 4;
+	}
+	if (!(x & 3)) {
+		x >>= 2;
+		r += 2;
+	}
+	if (!(x & 1)) {
+		x >>= 1;
+		r += 1;
+	}
+	return r;
+}
+
+/**
+ * __ffs - find first bit in word.
  * @word: The word to search
  *
  * Undefined if no bit exists, so code should check against 0 first.
  */
-static inline __always_inline int ffs(unsigned long word)
+static inline __always_inline int __ffs(unsigned long word)
 {
 	int num = 0;
 
@@ -87,15 +124,52 @@ static inline __always_inline int ffs(unsigned long word)
  *
  * Undefined if no zero exists, so code should check against ~0UL first.
  */
-#define ffz(x)  ffs(~(x))
+#define ffz(x)  __ffs(~(x))
 
 /**
- * fls - find last (most-significant) set bit in a long word
+ * fls - find last (most-significant) bit set
+ * @x: the word to search
+ *
+ * This is defined the same way as ffs.
+ * Note fls(0) = 0, fls(1) = 1, fls(0x80000000) = 32.
+ */
+
+static inline __always_inline int fls(int x)
+{
+	int r = 32;
+
+	if (!x)
+		return 0;
+	if (!(x & 0xffff0000u)) {
+		x <<= 16;
+		r -= 16;
+	}
+	if (!(x & 0xff000000u)) {
+		x <<= 8;
+		r -= 8;
+	}
+	if (!(x & 0xf0000000u)) {
+		x <<= 4;
+		r -= 4;
+	}
+	if (!(x & 0xc0000000u)) {
+		x <<= 2;
+		r -= 2;
+	}
+	if (!(x & 0x80000000u)) {
+		x <<= 1;
+		r -= 1;
+	}
+	return r;
+}
+
+/**
+ * __fls - find last (most-significant) set bit in a long word
  * @word: the word to search
  *
  * Undefined if no set bit exists, so code should check against 0 first.
  */
-static inline __always_inline int fls(unsigned long word)
+static inline __always_inline unsigned long __fls(unsigned long word)
 {
 	int num = BITS_PER_LONG - 1;
 
@@ -150,7 +224,7 @@ static inline __always_inline int fls64(u64 x)
 {
 	if (x == 0)
 		return 0;
-	return fls(x) + 1;
+	return __fls(x) + 1;
 }
 #else
 #error BITS_PER_LONG not 32 or 64
