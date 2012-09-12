54d0771cbb2de7576542ee4ff038cb52d2859319
Anup Patel <anup@brainfault.org>
2012-09-12 19:28:20 +0530

    [CORE] Made block device framework more generic and flexible.
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- core/include/block/vmm_blockdev.h
+++ core/include/block/vmm_blockdev.h
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010 Anup Patel.
+ * Copyright (c) 2012 Anup Patel.
  * All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
@@ -26,44 +26,89 @@
 
 #include <vmm_types.h>
 #include <vmm_devdrv.h>
+#include <vmm_spinlocks.h>
 
 #define VMM_BLOCKDEV_CLASS_NAME				"block"
 #define VMM_BLOCKDEV_CLASS_IPRIORITY			1
+#define VMM_BLOCKDEV_MAX_NAME_SIZE			32
 
-struct vmm_blockdev;
-
-typedef int (*vmm_blockdev_ioctl_t) (struct vmm_blockdev * bdev,
-				     int cmd, void *buf, size_t buf_len);
-typedef int (*vmm_blockdev_readblk_t) (struct vmm_blockdev * bdev,
-				       void *dest, u32 blknum, u32 blkcount);
-typedef int (*vmm_blockdev_writeblk_t) (struct vmm_blockdev * bdev,
-					void *src, u32 blknum, u32 blkcount);
-
-struct vmm_blockdev {
-	char name[32];
-	struct vmm_device *dev;
-	vmm_blockdev_ioctl_t ioctl;
-	vmm_blockdev_readblk_t readblk;
-	vmm_blockdev_writeblk_t writeblk;
+enum vmm_request_type {
+	VMM_REQUEST_READ=0,
+	VMM_REQUEST_WRITE=1
 };
 
-/** Do ioctl operation on a block device */
-int vmm_blockdev_doioctl(struct vmm_blockdev * bdev,
-			 int cmd, void *buf, size_t buf_len);
+struct vmm_request {
+	struct vmm_blockdev *bdev; /* No need to set this field. 
+				    * submit_request() will set this field.
+				    */
 
-/** Do read blocks operation on a block device */
-int vmm_blockdev_doreadblk(struct vmm_blockdev * bdev,
-			   void *dest, u32 blknum, u32 blkcount);
+	enum vmm_request_type type;
+	u64 lba;
+	u32 bcnt;
+	void *data;
 
-/** Do write blocks operation on a block device */
-int vmm_blockdev_dowriteblk(struct vmm_blockdev * bdev,
-			    void *src, u32 blknum, u32 blkcount);
+	void (*completed)(struct vmm_request *);
+	void (*failed)(struct vmm_request *);
+	void *priv;
+};
+
+struct vmm_request_queue {
+	vmm_spinlock_t lock;
+
+	int (*make_request)(struct vmm_request_queue *rq, 
+			    struct vmm_request *r);
+	int (*abort_request)(struct vmm_request_queue *rq, 
+			    struct vmm_request *r);
+
+	void *priv;
+};
+
+struct vmm_blockdev {
+	struct dlist head;
+	struct vmm_blockdev *parent;
+	
+	vmm_spinlock_t child_lock;
+	u32 child_count;
+	struct dlist child_list;
+
+	char name[VMM_BLOCKDEV_MAX_NAME_SIZE];
+	struct vmm_device *dev;
+	
+	u64 start_lba;
+	u64 num_blocks;
+	u32 block_size;
+
+	struct vmm_request_queue *rq;
+};
+
+/** Generic block IO submit request */
+int vmm_blockdev_submit_request(struct vmm_blockdev *bdev,
+				struct vmm_request *r);
+
+/** Generic block IO complete reuest */
+int vmm_blockdev_complete_request(struct vmm_request *r);
+
+/** Generic block IO fail reuest */
+int vmm_blockdev_fail_request(struct vmm_request *r);
+
+/** Generic block IO abort request */
+int vmm_blockdev_abort_request(struct vmm_request *r);
+
+/** Allocate block device */
+struct vmm_blockdev *vmm_blockdev_alloc(void);
+
+/** Free block device */
+void vmm_blockdev_free(struct vmm_blockdev *bdev);
 
 /** Register block device to device driver framework */
-int vmm_blockdev_register(struct vmm_blockdev * bdev);
+int vmm_blockdev_register(struct vmm_blockdev *bdev);
+
+/** Add child block device and register it. */
+int vmm_blockdev_add_child(struct vmm_blockdev *bdev, 
+			   u64 start_lba, u64 num_blocks);
 
 /** Unregister block device from device driver framework */
-int vmm_blockdev_unregister(struct vmm_blockdev * bdev);
+int vmm_blockdev_unregister(struct vmm_blockdev *bdev);
 
 /** Find a block device in device driver framework */
 struct vmm_blockdev *vmm_blockdev_find(const char *name);
