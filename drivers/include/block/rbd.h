fd7ec8592cd6b33dfff19f591e2a8e3be72c5d42
Anup Patel <anup@brainfault.org>
2012-09-12 22:06:51 +0530

    [DRIVERS] First-cut implementation of RAM backed block device support.
    Signed-off-by: Anup Patel <anup@brainfault.org>

---
+++ drivers/include/block/rbd.h
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2012 Anup Patel.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * @file rbd.h
+ * @author Anup Patel (anup@brainfault.org)
+ * @brief Interface for RAM backed block device driver.
+ */
+
+#ifndef __RBD_H_
+#define __RBD_H_
+
+#include <vmm_types.h>
+#include <vmm_workqueue.h>
+#include <block/vmm_blockdev.h>
+
+#define RBD_BLOCK_SIZE				512
+#define RBD_MAX_REQUEST				32
+
+/* RAM backed device (RBD) request */
+struct rbd_request {
+	struct vmm_work w;
+	struct vmm_request *r;
+	struct vmm_request_queue *rq;
+};
+
+/* RAM backed device (RBD) context */
+struct rbd {
+	struct vmm_blockdev *bdev;
+	physical_addr_t addr;
+	physical_size_t size;
+	vmm_spinlock_t reqs_lock;
+	u32 reqs_pos;
+	u32 reqs_free_count;
+	struct rbd_request reqs[RBD_MAX_REQUEST];
+};
+
+/** Create RBD instance */
+struct rbd *rbd_create(const char *name, 
+			physical_addr_t pa, 
+			physical_size_t sz);
+
+/** Destroy RBD instance */
+void rbd_destroy(struct rbd *d);
+
+#endif /* __RBD_H_ */
