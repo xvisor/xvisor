8589997eeec11c2f9edd2340685bfddad2886b7f
Jean-Christophe DUBOIS <jcd@tribudubois.net>
2013-01-27 12:18:26 +0530

    [DRIVERS] Exynos4: Add S3C RTC driver
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- drivers/include/asm/irq.h
+++ drivers/include/asm/irq.h
@@ -3,4 +3,13 @@
 
 #include <arch_cpu_irq.h>
 
+/*
+ * Use this value to indicate lack of interrupt
+ * capability
+ */
+#ifndef NO_IRQ
+#define NO_IRQ  ((unsigned int)(-1))
+#endif
+
+
 #endif /* _ASM_IRQ_H_ */
