7130c2a6802c7c58c0ad84dc72ab304d1efa8acc
Anup Patel <anup@brainfault.org>
2013-02-16 21:08:03 +0530

    [EMULATORS] virtio_net: Fix and improve virtio network
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- emulators/net/virtio_net.c
+++ emulators/net/virtio_net.c
@@ -64,41 +64,73 @@ struct virtio_net_dev {
 	u32 features;
 
 	int mode;
-	struct vmm_netport		*port;
+	struct vmm_netport *port;
 	char name[64];
 };
 
+static u32 virtio_net_get_host_features(struct virtio_device *dev)
+{
+	return 1UL << VIRTIO_NET_F_MAC
+		| 1UL << VIRTIO_NET_F_CSUM
+		| 1UL << VIRTIO_NET_F_HOST_UFO
+		| 1UL << VIRTIO_NET_F_HOST_TSO4
+		| 1UL << VIRTIO_NET_F_HOST_TSO6
+		| 1UL << VIRTIO_NET_F_GUEST_UFO
+		| 1UL << VIRTIO_NET_F_GUEST_TSO4
+		| 1UL << VIRTIO_NET_F_GUEST_TSO6
+		| 1UL << VIRTIO_RING_F_EVENT_IDX
+#if 0
+		| 1UL << VIRTIO_RING_F_INDIRECT_DESC
+#endif
+		;
+}
+
+static void virtio_net_set_guest_features(struct virtio_device *dev,
+					  u32 features)
+{
+	struct virtio_net_dev *ndev = dev->emu_data;
+
+	ndev->features = features;
+}
+
 static int virtio_net_init_vq(struct virtio_device *dev,
-			      u32 vq, u32 page_size, u32 align,
+			      u32 vqnum, u32 page_size, u32 align,
 			      u32 pfn)
 {
-	struct virtio_net_dev *ndev = (struct virtio_net_dev *) dev->emu_data;
-	struct virtio_queue *queue;
-	int rc = VMM_OK;
+	struct virtio_net_dev *ndev = dev->emu_data;
 
-	queue = &ndev->vqs[vq];
-	queue->pfn = pfn;
+	return virtio_queue_setup(&ndev->vqs[vqnum], dev->guest, 
+				pfn, page_size, VIRTIO_NET_QUEUE_SIZE, align);
+}
 
-	rc = virtio_queue_map_vq_space(dev->guest, queue, pfn * page_size,
-				vring_size(VIRTIO_NET_QUEUE_SIZE, align));
-	if (rc != VMM_OK)
-		return VMM_EFAIL;
+static int virtio_net_get_pfn_vq(struct virtio_device *dev, u32 vq)
+{
+	struct virtio_net_dev *ndev = dev->emu_data;
 
-	vring_init(&queue->vring, VIRTIO_NET_QUEUE_SIZE,
-				queue->guest_vq_mapper, align);
+	return virtio_queue_guest_pfn(&ndev->vqs[vq]);
+}
 
-	return VMM_OK;
+static int virtio_net_get_size_vq(struct virtio_device *dev, u32 vq)
+{
+	/* FIXME: dynamic */
+	return VIRTIO_NET_QUEUE_SIZE;
+}
+
+static int virtio_net_set_size_vq(struct virtio_device *dev, u32 vq, int size)
+{
+	/* FIXME: dynamic */
+	return size;
 }
 
 static int virtio_net_tx_thread(struct virtio_device *dev,
 				struct virtio_net_dev *ndev,
 				int queue)
 {
+	u16 head = 0;
+	u32 ret_iov_cnt = 0, pkt_len = 0, ret_total_len = 0;
 	struct virtio_queue *vq;
 	struct virtio_iovec *iov = ndev->tx_iov;
 	struct vmm_mbuf *mb;
-	u32 ret_iov_cnt = 0, pkt_len = 0, ret_total_len = 0;
-	u16 head = 0;
 
 	vq = &ndev->vqs[VIRTIO_NET_TX_QUEUE];
 
@@ -124,7 +156,9 @@ static int virtio_net_tx_thread(struct virtio_device *dev,
 
 		virtio_queue_set_used_elem(vq, head, ret_total_len);
 
-		/* We should interrupt guest right now, otherwise latency is huge. */
+		/* We should interrupt guest right now, 
+		 * otherwise latency is huge. 
+		 */
 		if (virtio_queue_should_signal(&ndev->vqs[VIRTIO_NET_TX_QUEUE]))
 			dev->tra->notify(dev, VIRTIO_NET_TX_QUEUE);
 
@@ -133,121 +167,30 @@ static int virtio_net_tx_thread(struct virtio_device *dev,
 	return 0;
 }
 
-static void virtio_net_handle_callback(struct virtio_device *dev,
-				       struct virtio_net_dev *ndev,
-				       int queue)
+static int virtio_net_notify_vq(struct virtio_device *dev, u32 vq)
 {
-	switch (queue) {
+	struct virtio_net_dev *ndev = dev->emu_data;
+
+	switch (vq) {
 	case VIRTIO_NET_TX_QUEUE:
-		virtio_net_tx_thread(dev, ndev, queue);
+		virtio_net_tx_thread(dev, ndev, vq);
 		break;
 	case VIRTIO_NET_RX_QUEUE:
 		break;
 	default:
-		vmm_printf("Unknown queue index %u", queue);
-	}
-}
-
-
-static int virtio_net_notify_vq(struct virtio_device *dev,
-				u32 vq)
-{
-	struct virtio_net_dev *ndev = (struct virtio_net_dev *) dev->emu_data;
-
-	virtio_net_handle_callback(dev, ndev, vq);
-
-	return 0;
-}
-
-static int virtio_net_get_pfn_vq(struct virtio_device *dev,
-		      u32 vq)
-{
-	struct virtio_net_dev *ndev = (struct virtio_net_dev *) dev->emu_data;
-
-	return ndev->vqs[vq].pfn;
-}
-
-static int virtio_net_get_size_vq(struct virtio_device *dev,
-				  u32 vq)
-{
-	/* FIXME: dynamic */
-	return VIRTIO_NET_QUEUE_SIZE;
-}
-
-static int virtio_net_set_size_vq(struct virtio_device *dev,
-		       u32 vq, int size)
-{
-	/* FIXME: dynamic */
-	return size;
-}
-
-static u32 virtio_net_get_host_features(struct virtio_device *dev)
-{
-	return 1UL << VIRTIO_NET_F_MAC
-		| 1UL << VIRTIO_NET_F_CSUM
-		| 1UL << VIRTIO_NET_F_HOST_UFO
-		| 1UL << VIRTIO_NET_F_HOST_TSO4
-		| 1UL << VIRTIO_NET_F_HOST_TSO6
-		| 1UL << VIRTIO_NET_F_GUEST_UFO
-		| 1UL << VIRTIO_NET_F_GUEST_TSO4
-		| 1UL << VIRTIO_NET_F_GUEST_TSO6
-		| 1UL << VIRTIO_RING_F_EVENT_IDX
-#if 0
-		| 1UL << VIRTIO_RING_F_INDIRECT_DESC
-#endif
-		;
-}
-
-static void virtio_net_set_guest_features(struct virtio_device *dev,
-					  u32 features)
-{
-	struct virtio_net_dev *ndev = (struct virtio_net_dev *) dev->emu_data;
-
-	ndev->features = features;
-}
-
-int virtio_net_write_config(struct virtio_device *dev,
-			    u32 offset,
-			    void *src,
-			    u32 src_len)
-{
-	int i = 0;
-	struct virtio_net_dev *ndev = (struct virtio_net_dev *) dev->emu_data;
-	u8 *dst = (u8 *) &ndev->config;
-
-	for (i = 0; i < src_len; i++) {
-		dst[i] = *((u8 *) src + i);
+		vmm_printf("Unknown queue index %u", vq);
+		break;
 	}
 
 	return VMM_OK;
 }
 
-void virtio_net_read_config(struct virtio_device *dev,
-			    u32 offset,
-			    void *dst,
-			    u32 dst_len)
+static void virtio_net_set_link(struct vmm_netport *p)
 {
-	int i = 0;
-	struct virtio_net_dev *ndev = (struct virtio_net_dev *) dev->emu_data;
-
-	u8 *src = (u8 *) &ndev->config;
-
-	for (i = 0; i < dst_len; i++) {
-		*((u8 *) dst + i) = src[offset + i];
-	}
+	/* FIXME: */
 }
 
-void virtio_net_reset(struct virtio_device *dev)
-{
-	// TBD::
-}
-
-static void virtio_net_set_link(struct vmm_netport *port)
-{
-	// TBD::
-}
-
-static int virtio_net_can_receive(struct vmm_netport *port)
+static int virtio_net_can_receive(struct vmm_netport *p)
 {
 	return 1;
 }
@@ -255,18 +198,18 @@ static int virtio_net_can_receive(struct vmm_netport *port)
 static int virtio_net_switch2port_xfer(struct vmm_netport *p,
 				       struct vmm_mbuf *mb)
 {
-	struct virtio_queue *vq;
-	struct virtio_net_dev *ndev = (struct virtio_net_dev *) p->priv;
-	struct virtio_iovec *iov = ndev->rx_iov;
-	struct virtio_device *dev = ndev->vdev;
 	u16 head;
 	u32 ret_iov_cnt, ret_total_len, pkt_len;
+	struct virtio_queue *vq;
+	struct virtio_net_dev *ndev = p->priv;
+	struct virtio_iovec *iov = ndev->rx_iov;
+	struct virtio_device *dev = ndev->vdev;
 
-	vq = &ndev->vqs[VIRTIO_NET_RX_QUEUE];
-	pkt_len = min(VIRTIO_NET_MTU, mb->m_pktlen);
 	head = 0;
 	ret_iov_cnt = 0;
 	ret_total_len = 0;
+	vq = &ndev->vqs[VIRTIO_NET_RX_QUEUE];
+	pkt_len = min(VIRTIO_NET_MTU, mb->m_pktlen);
 
 	if (virtio_queue_available(vq)) {
 		head = virtio_queue_get_iovec(vq, iov, VIRTIO_NET_QUEUE_SIZE,
@@ -290,39 +233,85 @@ static int virtio_net_switch2port_xfer(struct vmm_netport *p,
 	return VMM_OK;
 }
 
-int virtio_net_connect(struct virtio_device *dev, struct virtio_emulator *emu)
+static int virtio_net_read_config(struct virtio_device *dev, 
+				  u32 offset, void *dst, u32 dst_len)
 {
+	int i = 0;
+	struct virtio_net_dev *ndev = dev->emu_data;
+	u8 *src = (u8 *)&ndev->config;
+
+	for (i = 0; i < dst_len; i++) {
+		*((u8 *)dst + i) = src[offset + i];
+	}
+
+	return VMM_OK;
+}
+
+static int virtio_net_write_config(struct virtio_device *dev,
+				   u32 offset, void *src, u32 src_len)
+{
+	int i = 0;
+	struct virtio_net_dev *ndev = dev->emu_data;
+	u8 *dst = (u8 *)&ndev->config;
+
+	for (i = 0; i < src_len; i++) {
+		dst[i] = *((u8 *) src + i);
+	}
+
+	return VMM_OK;
+}
+
+static int virtio_net_reset(struct virtio_device *dev)
+{
+	int rc;
+	struct virtio_net_dev *ndev = dev->emu_data;
+
+	rc = virtio_queue_cleanup(&ndev->vqs[VIRTIO_NET_RX_QUEUE]);
+	if (rc) {
+		return rc;
+	}
+
+	rc = virtio_queue_cleanup(&ndev->vqs[VIRTIO_NET_TX_QUEUE]);
+	if (rc) {
+		return rc;
+	}
+
+	return VMM_OK;
+}
+
+static int virtio_net_connect(struct virtio_device *dev, 
+			      struct virtio_emulator *emu)
+{
+	int i, rc;
+	char *attr;
 	struct virtio_net_dev *ndev;
 	struct vmm_netswitch *nsw;
-	char *attr;
-	int i;
 
-	ndev = vmm_malloc(sizeof(struct virtio_net_dev));
-
+	ndev = vmm_zalloc(sizeof(struct virtio_net_dev));
 	if (!ndev) {
 		vmm_printf("Failed to allocate virtio net device....\n");
 		return VMM_EFAIL;
 	}
 
-	memset(ndev, 0, sizeof(struct virtio_net_dev));
-
 	ndev->vdev = dev;
 	vmm_snprintf(ndev->name, VIRTIO_DEVICE_MAX_NAME_LEN, "%s", dev->name);
 	ndev->port = vmm_netport_alloc(ndev->name, VMM_NETPORT_DEF_QUEUE_SIZE);
-
 	ndev->port->mtu = VIRTIO_NET_MTU;
 	ndev->port->link_changed = virtio_net_set_link;
 	ndev->port->can_receive = virtio_net_can_receive;
 	ndev->port->switch2port_xfer = virtio_net_switch2port_xfer;
-
 	ndev->port->priv = ndev;
 
-	vmm_netport_register(ndev->port);
+	rc = vmm_netport_register(ndev->port);
+	if (rc) {
+		vmm_netport_free(ndev->port);
+		vmm_free(ndev);
+		return rc;
+	}
 
 	attr = vmm_devtree_attrval(dev->edev->node, "switch");
 	if (attr) {
 		nsw = vmm_netswitch_find((char *)attr);
-
 		if (!nsw) {
 			vmm_printf("%s: Cannot find netswitch \"%s\"\n",
 					__func__, (char *)attr);
@@ -341,8 +330,13 @@ int virtio_net_connect(struct virtio_device *dev, struct virtio_emulator *emu)
 	return VMM_OK;
 }
 
-void virtio_net_disconnect(struct virtio_device *dev)
+static void virtio_net_disconnect(struct virtio_device *dev)
 {
+	struct virtio_net_dev *ndev = dev->emu_data;
+
+	vmm_netport_unregister(ndev->port);
+	vmm_netport_free(ndev->port);
+	vmm_free(ndev);
 }
 
 struct virtio_device_id virtio_net_emu_id[] = {
@@ -353,11 +347,6 @@ struct virtio_device_id virtio_net_emu_id[] = {
 struct virtio_emulator virtio_net = {
 	.name = "virtio_net",
 	.id_table = virtio_net_emu_id,
-	.reset = virtio_net_reset,
-	.connect = virtio_net_connect,
-	.disconnect = virtio_net_disconnect,
-	.read_config = virtio_net_read_config,
-	.write_config = virtio_net_write_config,
 
 	/* VirtIO operations */
 	.get_host_features      = virtio_net_get_host_features,
@@ -367,6 +356,13 @@ struct virtio_emulator virtio_net = {
 	.get_size_vq            = virtio_net_get_size_vq,
 	.set_size_vq            = virtio_net_set_size_vq,
 	.notify_vq              = virtio_net_notify_vq,
+
+	/* Emulator operations */
+	.read_config = virtio_net_read_config,
+	.write_config = virtio_net_write_config,
+	.reset = virtio_net_reset,
+	.connect = virtio_net_connect,
+	.disconnect = virtio_net_disconnect,
 };
 
 static int __init virtio_net_init(void)
@@ -379,12 +375,9 @@ static void __exit virtio_net_exit(void)
 	virtio_unregister_emulator(&virtio_net);
 }
 
-
 VMM_DECLARE_MODULE(MODULE_DESC,
 			MODULE_AUTHOR,
 			MODULE_LICENSE,
 			MODULE_IPRIORITY,
 			MODULE_INIT,
 			MODULE_EXIT);
-
-
