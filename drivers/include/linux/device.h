affae1ea2825a3af29b23f8819e385d6bcf118b6
Anup Patel <anup@brainfault.org>
2013-04-19 11:43:52 +0530

    [DRIVERS] More linux compatible headers for driver porting
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- drivers/include/linux/device.h
+++ drivers/include/linux/device.h
@@ -11,7 +11,30 @@
 #include <linux/printk.h>
 #include <linux/module.h>
 
-#define device	vmm_device
+struct device;
+
+/* Dummy driver pm ops */
+struct dev_pm_ops {
+	int (*suspend)(struct device *dev);
+	int (*resume)(struct device *dev);
+};
+
+/* Dummy driver structure */
+struct driver {
+	struct dev_pm_ops *pm;
+};
+
+/* Dummy device_type structure */
+struct device_type {
+};
+
+/* Dummy device structure */
+struct device {
+	struct vmm_device *d;
+
+	struct driver *driver;
+	struct device_type *type;
+};
 
 /* FIXME: This file is just a place holder in most cases. */
 
