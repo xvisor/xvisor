c0ca6296a5bffc9489407ba791e720929475df03
Anup Patel <anup@brainfault.org>
2013-04-20 21:34:59 +0530

    [DRIVERS] First-cut implementation or light-weight MMC/SD/SDIO card support
    This patch introduces a light-weight framework for MMC/SD/SDIO card support adapted from u-boot.
    
    The whole idea here is to provide linux compatible MMC/SD/SDIO host controller driver APIs using u-boot like simple framework.
    
    For now, we don't have linux compatible header under drivers/include/linux/mmc but in future we will provide this headers using drivers/include/drv/mmc_core.h
    
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- drivers/mmc/host/objects.mk
+++ drivers/mmc/host/objects.mk
@@ -1,5 +1,5 @@
 #/**
-# Copyright (c) 2010 Anup Patel.
+# Copyright (c) 2013 Anup Patel.
 # All rights reserved.
 #
 # This program is free software; you can redistribute it and/or modify
@@ -16,18 +16,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 #
-# @file openconf.cfg
+# @file objects.mk
 # @author Anup Patel (anup@brainfault.org)
-# @brief config file for driver options
+# @brief list of MMC/SD host controller drivers objects
 # */
 
-menu "Device Drivers"
-     source "drivers/serial/openconf.cfg"
-     source "drivers/rtc/openconf.cfg"
-     source "drivers/block/openconf.cfg"
-     source "drivers/mmc/openconf.cfg"
-     source "drivers/input/openconf.cfg"
-     source "drivers/video/openconf.cfg"
-     source "drivers/net/openconf.cfg"
-     source "drivers/clk/openconf.cfg"
-endmenu
