8682b7c6988bb9c9da05a9f1e1b9153bd8709854
Anup Patel <anup@brainfault.org>
2013-05-10 15:54:07 +0530

    [x86] Light-weight arch_defterm_getc() using VTEMU and Input handlers
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- arch/x86/board/x86_64_generic/brd_console.c
+++ arch/x86/board/x86_64_generic/brd_console.c
@@ -25,17 +25,104 @@
 #include <vmm_error.h>
 #include <vmm_compiler.h>
 #include <vmm_host_aspace.h>
-#include <vmm_compiler.h>
+#include <input/vmm_input.h>
+#include <libs/vtemu.h>
+#include <libs/fifo.h>
 
 extern void cls();
 extern void putch(unsigned char c);
 extern void settextcolor(u8 forecolor, u8 backcolor);
 extern void init_console(void);
 
-int arch_defterm_getc(u8 *ch)
+#if defined(CONFIG_VTEMU)
+static struct fifo *defterm_fifo;
+static u32 defterm_key_flags;
+static struct vmm_input_handler defterm_hndl;
+static bool defterm_key_handler_registered;
+
+static int defterm_key_event(struct vmm_input_handler *ihnd, 
+			     struct vmm_input_dev *idev, 
+			     unsigned int type, unsigned int code, int value)
 {
+	int rc, i, len;
+	char str[16];
+	u32 key_flags;
+
+	if (value) { /* value=1 (key-up) or value=2 (auto-repeat) */
+		/* Update input key flags */
+		key_flags = vtemu_key2flags(code);
+		if ((key_flags & VTEMU_KEYFLAG_LOCKS) &&
+		    (defterm_key_flags & key_flags)) {
+			defterm_key_flags &= ~key_flags;
+		} else {
+			defterm_key_flags |= key_flags;
+		}
+
+		/* Retrive input key string */
+		rc = vtemu_key2str(code, defterm_key_flags, str);
+		if (rc) {
+			return VMM_OK;
+		}
+
+		/* Add input key string to input buffer */
+		len = strlen(str);
+		for (i = 0; i < len; i++) {
+			fifo_enqueue(defterm_fifo, &str[i], TRUE);
+		}
+	} else { /* value=0 (key-down) */
+		/* Update input key flags */
+		key_flags = vtemu_key2flags(code);
+		if (!(key_flags & VTEMU_KEYFLAG_LOCKS)) {
+			defterm_key_flags &= ~key_flags;
+		}
+	}
+
 	return VMM_OK;
 }
+
+int arch_defterm_getc(u8 *ch)
+{
+	int rc;
+
+	if (!defterm_key_handler_registered) {
+		memset(&defterm_hndl, 0, sizeof(defterm_hndl));
+		defterm_hndl.name = "defterm";
+		defterm_hndl.evbit[0] |= BIT_MASK(EV_KEY);
+		defterm_hndl.event = defterm_key_event;
+		defterm_hndl.priv = NULL;
+
+		rc = vmm_input_register_handler(&defterm_hndl); 
+		if (rc) {
+			return rc;
+		}
+
+		rc = vmm_input_connect_handler(&defterm_hndl); 
+		if (rc) {
+			return rc;
+		}
+
+		defterm_key_handler_registered = TRUE;
+	}
+
+	if (defterm_fifo && !fifo_isempty(defterm_fifo)) {
+		if (!fifo_dequeue(defterm_fifo, ch)) {
+			return VMM_ENOTAVAIL;
+		}
+
+		return VMM_OK;
+	}
+
+	return VMM_EFAIL;
+}
+
+#else
+
+int arch_defterm_getc(u8 *ch)
+{
+	return VMM_EFAIL;
+}
+
+#endif
 
 int arch_defterm_putc(u8 ch)
 {
@@ -47,5 +134,16 @@ int arch_defterm_putc(u8 ch)
 int __init arch_defterm_init(void)
 {
         init_console();
+
+#if defined(CONFIG_VTEMU)
+	defterm_fifo = fifo_alloc(128, sizeof(u8));
+	if (!defterm_fifo) {
+		return VMM_ENOMEM;
+	}
+
+	defterm_key_flags = 0;
+	defterm_key_handler_registered = FALSE;
+#endif
+
 	return VMM_OK;
 }
