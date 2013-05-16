5bc9e9108c4c99ff3e74dd38e4be661fd3210f5b
Anup Patel <anup@brainfault.org>
2013-05-16 21:04:13 +0530

    [DRIVERS] Add linux compatible completion event APIs
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- drivers/include/linux/completion.h
+++ drivers/include/linux/completion.h
@@ -11,7 +11,36 @@
 #include <vmm_completion.h>
 
 #include <linux/wait.h>
+#include <linux/jiffies.h>
 
-/* FIXME: This file just a place holder for most cases */
+#define completion				vmm_completion
+
+#define init_completion(cmpl)			INIT_COMPLETION(cmpl)
+#define complete(cmpl)				vmm_completion_complete(cmpl)
+
+#define wait_for_completion(cmpl)		vmm_completion_wait(cmpl)
+
+/**
+ * wait_for_completion_timeout: - waits for completion of a task (w/timeout)
+ * @x:  holds the state of this particular completion
+ * @timeout:  timeout value in jiffies
+ *
+ * This waits for either a completion of a specific task to be signaled or for a
+ * specified timeout to expire. The timeout is in jiffies. It is not
+ * interruptible.
+ *
+ * The return value is 0 if timed out, and positive (at least 1, or number of
+ * jiffies left till timeout) if completed.
+ */
+static inline unsigned long wait_for_completion_timeout(struct completion *x,
+							unsigned long timeout)
+{
+	u64 __r = timeout;
+	__r = __r * (1000000000 / HZ);
+	vmm_completion_wait_timeout(x, &__r);
+	__r = udiv64(__r, 1000000000);
+	__r = udiv64(__r, HZ);
+	return (unsigned long)__r;
+}
 
 #endif
