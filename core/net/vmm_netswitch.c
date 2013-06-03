a9c088c9036f0d17e78c9317af55045f78a11174
Jean-Christophe DUBOIS <jcd@tribudubois.net>
2013-06-03 19:11:05 +0530

    [CORE] vmm_netswitch.c: use strlcpy instead of strncpy.
    Signed-off-by: Jean-Christophe DUBOIS <jcd@tribudubois.net>

--- core/net/vmm_netswitch.c
+++ core/net/vmm_netswitch.c
@@ -459,7 +459,11 @@ int vmm_netswitch_register(struct vmm_netswitch *nsw,
 	}
 
 	INIT_LIST_HEAD(&cd->head);
-	strcpy(cd->name, nsw->name);
+	if (strlcpy(cd->name, nsw->name, sizeof(cd->name)) >=
+	    sizeof(cd->name)) {
+		rc = VMM_EOVERFLOW;
+		goto fail_nsw_reg;
+	}
 	cd->dev = nsw->dev;
 	cd->priv = nsw;
 
@@ -482,8 +486,6 @@ int vmm_netswitch_register(struct vmm_netswitch *nsw,
 	return rc;
 
 fail_nsw_reg:
-	cd->dev = NULL;
-	cd->priv = NULL;
 	vmm_free(cd);
 ret:
 	return rc;
@@ -572,18 +574,25 @@ int __init vmm_netswitch_init(void)
 	}
 
 	INIT_LIST_HEAD(&c->head);
-	strcpy(c->name, VMM_NETSWITCH_CLASS_NAME);
+	if (strlcpy(c->name, VMM_NETSWITCH_CLASS_NAME, sizeof(c->name)) >=
+	    sizeof(c->name)) {
+		rc = VMM_EOVERFLOW;
+		goto free_class;
+	}
 	INIT_LIST_HEAD(&c->classdev_list);
 
 	rc = vmm_devdrv_register_class(c);
 	if (rc) {
 		vmm_printf("Failed to register %s class\n",
 			VMM_NETSWITCH_CLASS_NAME);
-		vmm_free(c);
-		return rc;
+		goto free_class;
 	}
 
 	return VMM_OK;
+
+free_class:
+	vmm_free(c);
+	return rc;
 }
 
 void __exit vmm_netswitch_exit(void)
