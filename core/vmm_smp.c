9f8a0edab3f66aa6764a86f38a3f73f3d624187f
Anup Patel <anup@brainfault.org>
2013-07-30 23:42:19 +0530

    [CORE] Simplify SMP IPIs using FIFO APIs
    This patch reduces duplicate code in implementing per host CPU
    IPI list by using FIFO APIs to implement per host CPU IPI FIFO.
    
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- core/vmm_smp.c
+++ core/vmm_smp.c
@@ -25,8 +25,7 @@
 #include <vmm_smp.h>
 #include <vmm_stdio.h>
 #include <vmm_timer.h>
-#include <libs/list.h>
-#include <arch_locks.h>
+#include <libs/fifo.h>
 
 #define MAX_SMP_IPI_PER_CPU		(CONFIG_CPU_COUNT)
 
@@ -37,100 +36,46 @@ struct smp_ipi_call {
 	void *arg2;
 };
 
-struct smp_ipi_ctrl {
-	arch_spinlock_t lock;
-	u32 head;
-	u32 tail;
-	u32 avail;
-	struct smp_ipi_call queue[MAX_SMP_IPI_PER_CPU];
-} __cacheline_aligned_in_smp;
-
-static struct smp_ipi_ctrl smp_ipi[CONFIG_CPU_COUNT];
+static struct fifo *smp_ipi_fifo[CONFIG_CPU_COUNT];
 
 static void smp_ipi_submit(u32 cpu, struct smp_ipi_call *ipic)
 {
-	struct smp_ipi_call *ipic_p;
-	struct smp_ipi_ctrl *ctrl;
-
-	if ((cpu >= CONFIG_CPU_COUNT) || !ipic) {
+	if ((cpu >= CONFIG_CPU_COUNT) || !ipic || !ipic->func) {
 		return;
 	}
 
-	ctrl = &smp_ipi[cpu];
-
-	arch_spin_lock(&ctrl->lock);
-
-	if (ctrl->avail >= MAX_SMP_IPI_PER_CPU) {
+	if (!fifo_enqueue(smp_ipi_fifo[cpu], ipic, FALSE)) {
 		WARN(1, "CPU%d: IPI queue full\n", cpu);
-		arch_spin_unlock(&ctrl->lock);
 		return;
 	}
-
-	ipic_p = &ctrl->queue[ctrl->head];
-	ipic_p->func = ipic->func;
-	ipic_p->arg0 = ipic->arg0;
-	ipic_p->arg1 = ipic->arg1;
-	ipic_p->arg2 = ipic->arg2;
-
-	ctrl->head++;
-	if (ctrl->head >= MAX_SMP_IPI_PER_CPU) {
-		ctrl->head = 0;
-	}
-
-	ctrl->avail++;
-
-	arch_spin_unlock(&ctrl->lock);
 }
 
 static u32 smp_ipi_pending_count(u32 cpu)
 {
-	u32 ret;
-	struct smp_ipi_ctrl *ctrl;
-
 	if (cpu >= CONFIG_CPU_COUNT) {
 		return 0;
 	}
 
-	ctrl = &smp_ipi[cpu];
-
-	arch_spin_lock(&ctrl->lock);
-	ret = ctrl->avail;
-	arch_spin_unlock(&ctrl->lock);
-
-	return ret;
+	return fifo_avail(smp_ipi_fifo[cpu]);
 }
 
 void vmm_smp_ipi_exec(void)
 {
 	u32 cpu = vmm_smp_processor_id();
-	struct smp_ipi_call *ipic;
-	struct smp_ipi_ctrl *ctrl;
+	struct smp_ipi_call ipic;
+	struct fifo *ififo;
 
 	if (cpu >= CONFIG_CPU_COUNT) {
 		return;
 	}
 
-	ctrl = &smp_ipi[cpu];
+	ififo = smp_ipi_fifo[cpu];
 
-	arch_spin_lock(&ctrl->lock);
-
-	while (ctrl->avail) {
-		ipic = &ctrl->queue[ctrl->tail];
-
-		if (ipic->func) {
-			ipic->func(ipic->arg0, ipic->arg1, ipic->arg2);
+	while (fifo_dequeue(ififo, &ipic)) {
+		if (ipic.func) {
+			ipic.func(ipic.arg0, ipic.arg1, ipic.arg2);
 		}
-		ipic->func = NULL;
-
-		ctrl->tail++;
-		if (ctrl->tail >= MAX_SMP_IPI_PER_CPU) {
-			ctrl->tail = 0;
-		}
-
-		ctrl->avail--;
 	}
-
-	arch_spin_unlock(&ctrl->lock);
 }
 
 void vmm_smp_ipi_async_call(const struct vmm_cpumask *dest,
@@ -228,20 +173,16 @@ int vmm_smp_ipi_sync_call(const struct vmm_cpumask *dest,
 int __cpuinit vmm_smp_ipi_init(void)
 {
 	int rc;
-	u32 c, q, cpu = vmm_smp_processor_id();
+	u32 c, cpu = vmm_smp_processor_id();
 
-	/* Initialize IPI list */
+	/* Initialize IPI FIFOs */
 	if (!cpu) {
 		for (c = 0; c < CONFIG_CPU_COUNT; c++) {
-			ARCH_SPIN_LOCK_INIT(&smp_ipi[c].lock);
-			smp_ipi[c].head = 0;
-			smp_ipi[c].tail = 0;
-			smp_ipi[c].avail = 0;
-			for (q = 0; q < MAX_SMP_IPI_PER_CPU; q++) {
-				smp_ipi[c].queue[q].func = NULL;
-				smp_ipi[c].queue[q].arg0 = NULL;
-				smp_ipi[c].queue[q].arg1 = NULL;
-				smp_ipi[c].queue[q].arg2 = NULL;
+			smp_ipi_fifo[c] = 
+				fifo_alloc(sizeof(struct smp_ipi_call), 
+					   MAX_SMP_IPI_PER_CPU);
+			if (!smp_ipi_fifo[c]) {
+				return VMM_ENOMEM;
 			}
 		}
 	}
