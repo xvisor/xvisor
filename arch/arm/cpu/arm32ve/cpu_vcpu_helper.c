6a991eb390b336d4cb746304bcbd990b0f6d6a39
Anup Patel <anup@brainfault.org>
2013-09-07 10:49:32 +0530

    [ARM] arm32ve: Add UNDEF, PABT, and DABT exception injection
    Sooner or later we would be having HVC or SMC based PSCI interface
    shared by arm32ve and arm64.
    
    The common PSCI interface emulation would require arm32ve specific
    and arm64 specific exception injection mechanisms.
    
    This patch adds helper APIs for injecting undefined, prefetch abort
    and data abort exceptions.
    
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- arch/arm/cpu/arm32ve/cpu_vcpu_helper.c
+++ arch/arm/cpu/arm32ve/cpu_vcpu_helper.c
@@ -438,6 +438,141 @@ int cpu_vcpu_spsr_update(struct vmm_vcpu *vcpu,
 	return VMM_OK;
 }
 
+int cpu_vcpu_inject_undef(struct vmm_vcpu *vcpu,
+			  arch_regs_t *regs)
+{
+	u32 old_cpsr, new_cpsr, sctlr;
+
+	/* Sanity checks */
+	if (!vcpu || !regs) {
+		return VMM_EFAIL;
+	}
+	if (vcpu != vmm_scheduler_current_vcpu()) {
+		/* This function should only be called for current VCPU */
+		vmm_panic("%d not called for current vcpu\n", __func__);
+	}
+
+	/* Retrive current SCTLR */
+	sctlr = read_sctlr();
+
+	/* Compute CPSR changes */
+	old_cpsr = new_cpsr = regs->cpsr;
+	new_cpsr &= ~CPSR_MODE_MASK;
+	new_cpsr |= (CPSR_MODE_UNDEFINED | CPSR_IRQ_DISABLED);
+	new_cpsr &= ~(CPSR_IT2_MASK | 
+			CPSR_IT1_MASK | 
+			CPSR_JAZZLE_ENABLED | 
+			CPSR_BE_ENABLED | 
+			CPSR_THUMB_ENABLED);
+	if (sctlr & SCTLR_TE_MASK) {
+		new_cpsr |= CPSR_THUMB_ENABLED;
+	}
+	if (sctlr & SCTLR_EE_MASK) {
+		new_cpsr |= CPSR_BE_ENABLED;
+	}
+
+	/* Update CPSR, SPSR, LR and PC */
+	asm volatile (" msr     SPSR_und, %0\n\t"
+		      ::"r" (old_cpsr) :"memory", "cc");
+	arm_priv(vcpu)->spsr_und = old_cpsr;
+	regs->lr = regs->pc - ((old_cpsr & CPSR_THUMB_ENABLED) ? 2 : 4);
+	if (sctlr & SCTLR_V_MASK) {
+		regs->pc = CPU_IRQ_HIGHVEC_BASE;
+	} else {
+		regs->pc = read_vbar();
+	}
+	regs->pc += 4;
+	regs->cpsr = new_cpsr;
+
+	return VMM_OK;
+}
+
+static int __cpu_vcpu_inject_abt(struct vmm_vcpu *vcpu,
+				 arch_regs_t *regs,
+				 bool is_pabt,
+				 virtual_addr_t addr)
+{
+	u32 old_cpsr, new_cpsr, sctlr, ttbcr;
+
+	/* Sanity checks */
+	if (!vcpu || !regs) {
+		return VMM_EFAIL;
+	}
+	if (vcpu != vmm_scheduler_current_vcpu()) {
+		/* This function should only be called for current VCPU */
+		vmm_panic("%d not called for current vcpu\n", __func__);
+	}
+
+	/* Retrive current SCTLR */
+	sctlr = read_sctlr();
+
+	/* Compute CPSR changes */
+	old_cpsr = new_cpsr = regs->cpsr;
+	new_cpsr &= ~CPSR_MODE_MASK;
+	new_cpsr |= (CPSR_MODE_ABORT | 
+			CPSR_ASYNC_ABORT_DISABLED | 
+			CPSR_IRQ_DISABLED);
+	new_cpsr &= ~(CPSR_IT2_MASK | 
+			CPSR_IT1_MASK | 
+			CPSR_JAZZLE_ENABLED | 
+			CPSR_BE_ENABLED | 
+			CPSR_THUMB_ENABLED);
+	if (sctlr & SCTLR_TE_MASK) {
+		new_cpsr |= CPSR_THUMB_ENABLED;
+	}
+	if (sctlr & SCTLR_EE_MASK) {
+		new_cpsr |= CPSR_BE_ENABLED;
+	}
+
+	/* Update CPSR, SPSR, LR and PC */
+	asm volatile (" msr     SPSR_abt, %0\n\t"
+		      ::"r" (old_cpsr) :"memory", "cc");
+	arm_priv(vcpu)->spsr_und = old_cpsr;
+	regs->lr = regs->pc - ((old_cpsr & CPSR_THUMB_ENABLED) ? 4 : 0);
+	if (sctlr & SCTLR_V_MASK) {
+		regs->pc = CPU_IRQ_HIGHVEC_BASE;
+	} else {
+		regs->pc = read_vbar();
+	}
+	regs->pc += (is_pabt) ? 12 : 16;
+	regs->cpsr = new_cpsr;
+
+	/* Update abort registers */
+	ttbcr = read_ttbcr();
+	if (is_pabt) {
+		/* Set IFAR and IFSR */
+		write_ifar(addr);
+		if (ttbcr >> 31) { /* LPAE MMU */
+			write_ifsr((1 << 9) | 0x22);
+		} else { /* Legacy ARMv6 MMU */
+			write_ifsr(0x2);
+		}
+	} else {
+		/* Set DFAR and DFSR */
+		write_dfar(addr);
+		if (ttbcr >> 31) { /* LPAE MMU */
+			write_dfsr((1 << 9) | 0x22);
+		} else { /* Legacy ARMv6 MMU */
+			write_dfsr(0x2);
+		}
+	}
+
+	return VMM_OK;
+}
+
+int cpu_vcpu_inject_pabt(struct vmm_vcpu *vcpu,
+			 arch_regs_t *regs)
+{
+	return __cpu_vcpu_inject_abt(vcpu, regs, TRUE, regs->pc);
+}
+
+int cpu_vcpu_inject_dabt(struct vmm_vcpu *vcpu,
+			 arch_regs_t *regs,
+			 virtual_addr_t addr)
+{
+	return __cpu_vcpu_inject_abt(vcpu, regs, FALSE, addr);
+}
+
 int arch_guest_init(struct vmm_guest *guest)
 {
 	if (!guest->reset_count) {
--- arch/arm/cpu/arm32ve/cpu_vcpu_irq.c
+++ arch/arm/cpu/arm32ve/cpu_vcpu_irq.c
@@ -72,6 +72,7 @@ u32 arch_vcpu_irq_priority(struct vmm_vcpu *vcpu, u32 irq_no)
 int arch_vcpu_irq_assert(struct vmm_vcpu *vcpu, u32 irq_no, u32 reason)
 {
 	u32 hcr;
+	bool update_hcr;
 
 	/* Skip IRQ & FIQ if VGIC available */
 	if (arm_vgic_avail(vcpu) &&
@@ -81,28 +82,28 @@ int arch_vcpu_irq_assert(struct vmm_vcpu *vcpu, u32 irq_no, u32 reason)
 	}
 
 	hcr = arm_priv(vcpu)->hcr;
+	update_hcr = FALSE;
 
 	switch(irq_no) {
-	case CPU_DATA_ABORT_IRQ:
-		hcr |= HCR_VA_MASK;
-		/* VA bit is auto-cleared */
-		break;
 	case CPU_EXTERNAL_IRQ:
 		hcr |= HCR_VI_MASK;
 		/* VI bit will be cleared on deassertion */
+		update_hcr = TRUE;
 		break;
 	case CPU_EXTERNAL_FIQ:
 		hcr |= HCR_VF_MASK;
 		/* VF bit will be cleared on deassertion */
+		update_hcr = TRUE;
 		break;
 	default:
-		return VMM_EFAIL;
 		break;
 	};
 
-	arm_priv(vcpu)->hcr = hcr;
-	if (vmm_scheduler_current_vcpu() == vcpu) {
-		write_hcr(hcr);
+	if (update_hcr) {
+		arm_priv(vcpu)->hcr = hcr;
+		if (vmm_scheduler_current_vcpu() == vcpu) {
+			write_hcr(hcr);
+		}
 	}
 
 	return VMM_OK;
@@ -113,6 +114,8 @@ int arch_vcpu_irq_execute(struct vmm_vcpu *vcpu,
 			  arch_regs_t *regs, 
 			  u32 irq_no, u32 reason)
 {
+	int rc;
+
 	/* Skip IRQ & FIQ if VGIC available */
 	if (arm_vgic_avail(vcpu) &&
 	    ((irq_no == CPU_EXTERNAL_IRQ) || 
@@ -120,14 +123,34 @@ int arch_vcpu_irq_execute(struct vmm_vcpu *vcpu,
 		return VMM_OK;
 	}
 
+	/* Undefined, Data abort, and Prefetch abort 
+	 * can only be emulated in normal context.
+	 */
+	switch(irq_no) {
+	case CPU_UNDEF_INST_IRQ:
+		rc = cpu_vcpu_inject_undef(vcpu, regs);
+		break;
+	case CPU_PREFETCH_ABORT_IRQ:
+		rc = cpu_vcpu_inject_pabt(vcpu, regs);
+		break;
+	case CPU_DATA_ABORT_IRQ:
+		rc = cpu_vcpu_inject_dabt(vcpu, regs, (virtual_addr_t)reason);
+		break;
+	default:
+		rc = VMM_OK;
+		break;
+	};
+
+	/* Update HCR in HW */
 	write_hcr(arm_priv(vcpu)->hcr);
 
-	return VMM_OK;
+	return rc;
 }
 
 int arch_vcpu_irq_deassert(struct vmm_vcpu *vcpu, u32 irq_no, u32 reason)
 {
 	u32 hcr;
+	bool update_hcr;
 
 	/* Skip IRQ & FIQ if VGIC available */
 	if (arm_vgic_avail(vcpu) &&
@@ -137,22 +160,26 @@ int arch_vcpu_irq_deassert(struct vmm_vcpu *vcpu, u32 irq_no, u32 reason)
 	}
 
 	hcr = arm_priv(vcpu)->hcr;
+	update_hcr = FALSE;
 
 	switch(irq_no) {
 	case CPU_EXTERNAL_IRQ:
 		hcr &= ~HCR_VI_MASK;
+		update_hcr = TRUE;
 		break;
 	case CPU_EXTERNAL_FIQ:
 		hcr &= ~HCR_VF_MASK;
+		update_hcr = TRUE;
 		break;
 	default:
-		return VMM_EFAIL;
 		break;
 	};
 
-	arm_priv(vcpu)->hcr = hcr;
-	if (vmm_scheduler_current_vcpu() == vcpu) {
-		write_hcr(hcr);
+	if (update_hcr) {
+		arm_priv(vcpu)->hcr = hcr;
+		if (vmm_scheduler_current_vcpu() == vcpu) {
+			write_hcr(hcr);
+		}
 	}
 
 	return VMM_OK;
