d120fb9f00effd027c29481c06057473117e14f8
Anup Patel <anup@brainfault.org>
2013-11-19 08:41:05 +0530

    [CORE] Update vmm_limits.h with max values of integer types
    Integer data types (i.e. short, int, unsigned int, unsigned long, ...)
    are widely used everywhere in our code. Its good to know the max limits
    of this integer data types hence this patch.
    
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- core/include/vmm_limits.h
+++ core/include/vmm_limits.h
@@ -24,6 +24,22 @@
 #ifndef __VMM_LIMITS_H__
 #define __VMM_LIMITS_H__
 
+#include <vmm_types.h>
+
+#define USHRT_MAX	((u16)(~0U))
+#define SHRT_MAX	((s16)(USHRT_MAX>>1))
+#define SHRT_MIN	((s16)(-SHRT_MAX - 1))
+#define INT_MAX		((int)(~0U>>1))
+#define INT_MIN		(-INT_MAX - 1)
+#define UINT_MAX	(~0U)
+#define LONG_MAX	((long)(~0UL>>1))
+#define LONG_MIN	(-LONG_MAX - 1)
+#define ULONG_MAX	(~0UL)
+#define LLONG_MAX	((long long)(~0ULL>>1))
+#define LLONG_MIN	(-LLONG_MAX - 1)
+#define ULLONG_MAX	(~0ULL)
+#define SIZE_MAX	(~(size_t)0)
+
 #define VMM_FIELD_LICENSE_SIZE	32
 #define VMM_FIELD_AUTHOR_SIZE	32
 #define VMM_FIELD_TYPE_SIZE	32
