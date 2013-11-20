9d790dfcbc12a0cd98afa8324594b7129ff1874a
Anup Patel <anup@brainfault.org>
2013-11-20 23:47:35 +0530

    [CORE] Add round_down() and round_up() helper macros
    This patch add useful integer round_down() and round_up()
    macros to vmm_macros.h
    
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- core/include/vmm_macros.h
+++ core/include/vmm_macros.h
@@ -201,4 +201,14 @@
 
 #define field_sizeof(t, f)		(sizeof(((t*)0)->f))
 
+/*
+ * This looks more complex than it should be. But we need to
+ * get the type for the ~ right in round_down (it needs to be
+ * as wide as the result!), and we want to evaluate the macro
+ * arguments just once each.
+ */
+#define __round_mask(x, y) ((__typeof__(x))((y)-1))
+#define round_up(x, y) ((((x)-1) | __round_mask(x, y))+1)
+#define round_down(x, y) ((x) & ~__round_mask(x, y))
+
 #endif /* __VMM_MACROS_H__ */
