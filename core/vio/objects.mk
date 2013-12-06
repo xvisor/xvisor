7071b956379c957544aa5038e3e09881b7e6ee5f
Anup Patel <anup@brainfault.org>
2013-12-06 23:58:02 +0530

    [CORE] vio: Add vmm_keymaps APIs
    This patch adapts QEMU keymap APIs so that we can now convert
    key press events from VNC or GUI daemons into intermediate scancodes.
    
    Further, these intermediate scancodes will be used in keyboard and
    mouse emulators for emulating keyboard/mouse events for Guest.
    
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- core/vio/objects.mk
+++ core/vio/objects.mk
@@ -23,3 +23,51 @@
 
 core-objs-$(CONFIG_VSERIAL)+= vio/vmm_vserial.o
 
+core-objs-$(CONFIG_VINPUT)+= vio/vmm_vinput_core.o
+
+vmm_vinput_core-y+= vmm_keymaps.o
+vmm_vinput_core-y+= keymaps/modifiers.o
+vmm_vinput_core-y+= keymaps/common.o
+vmm_vinput_core-$(CONFIG_VINPUT_KEYMAP_AR)+= keymaps/ar.o
+vmm_vinput_core-$(CONFIG_VINPUT_KEYMAP_BEPO)+= keymaps/bepo.o
+vmm_vinput_core-$(CONFIG_VINPUT_KEYMAP_CZ)+= keymaps/cz.o
+vmm_vinput_core-$(CONFIG_VINPUT_KEYMAP_DA)+= keymaps/da.o
+vmm_vinput_core-$(CONFIG_VINPUT_KEYMAP_DE_CH)+= keymaps/de-ch.o
+vmm_vinput_core-$(CONFIG_VINPUT_KEYMAP_DE)+= keymaps/de.o
+vmm_vinput_core-$(CONFIG_VINPUT_KEYMAP_EN_GB)+= keymaps/en-gb.o
+vmm_vinput_core-$(CONFIG_VINPUT_KEYMAP_EN_US)+= keymaps/en-us.o
+vmm_vinput_core-$(CONFIG_VINPUT_KEYMAP_ES)+= keymaps/es.o
+vmm_vinput_core-$(CONFIG_VINPUT_KEYMAP_ET)+= keymaps/et.o
+vmm_vinput_core-$(CONFIG_VINPUT_KEYMAP_FI)+= keymaps/fi.o
+vmm_vinput_core-$(CONFIG_VINPUT_KEYMAP_FO)+= keymaps/fo.o
+vmm_vinput_core-$(CONFIG_VINPUT_KEYMAP_FR_BE)+= keymaps/fr-be.o
+vmm_vinput_core-$(CONFIG_VINPUT_KEYMAP_FR_CA)+= keymaps/fr-ca.o
+vmm_vinput_core-$(CONFIG_VINPUT_KEYMAP_FR_CH)+= keymaps/fr-ch.o
+vmm_vinput_core-$(CONFIG_VINPUT_KEYMAP_FR)+= keymaps/fr.o
+vmm_vinput_core-$(CONFIG_VINPUT_KEYMAP_HR)+= keymaps/hr.o
+vmm_vinput_core-$(CONFIG_VINPUT_KEYMAP_HU)+= keymaps/hu.o
+vmm_vinput_core-$(CONFIG_VINPUT_KEYMAP_IS)+= keymaps/is.o
+vmm_vinput_core-$(CONFIG_VINPUT_KEYMAP_IT)+= keymaps/it.o
+vmm_vinput_core-$(CONFIG_VINPUT_KEYMAP_JA)+= keymaps/ja.o
+vmm_vinput_core-$(CONFIG_VINPUT_KEYMAP_LT)+= keymaps/lt.o
+vmm_vinput_core-$(CONFIG_VINPUT_KEYMAP_LV)+= keymaps/lv.o
+vmm_vinput_core-$(CONFIG_VINPUT_KEYMAP_MK)+= keymaps/mk.o
+vmm_vinput_core-$(CONFIG_VINPUT_KEYMAP_NL_BE)+= keymaps/nl-be.o
+vmm_vinput_core-$(CONFIG_VINPUT_KEYMAP_NL)+= keymaps/nl.o
+vmm_vinput_core-$(CONFIG_VINPUT_KEYMAP_NO)+= keymaps/no.o
+vmm_vinput_core-$(CONFIG_VINPUT_KEYMAP_PL)+= keymaps/pl.o
+vmm_vinput_core-$(CONFIG_VINPUT_KEYMAP_PT_BR)+= keymaps/pt-br.o
+vmm_vinput_core-$(CONFIG_VINPUT_KEYMAP_PT)+= keymaps/pt.o
+vmm_vinput_core-$(CONFIG_VINPUT_KEYMAP_RU)+= keymaps/ru.o
+vmm_vinput_core-$(CONFIG_VINPUT_KEYMAP_SL)+= keymaps/sl.o
+vmm_vinput_core-$(CONFIG_VINPUT_KEYMAP_SV)+= keymaps/sv.o
+vmm_vinput_core-$(CONFIG_VINPUT_KEYMAP_TH)+= keymaps/th.o
+vmm_vinput_core-$(CONFIG_VINPUT_KEYMAP_TR)+= keymaps/tr.o
+
+%/vmm_vinput_core.o: $(foreach obj,$(vmm_vinput_core-y),%/$(obj))
+	$(call merge_objs,$@,$^)
+
+%/vmm_vinput_core.dep: $(foreach dep,$(vmm_vinput_core-y:.o=.dep),%/$(dep))
+	$(call merge_deps,$@,$^)
+
+
