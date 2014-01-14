e6649b648d473a5837c0f60b95ce5bfdae78e145
Anup Patel <anup@brainfault.org>
2014-01-14 18:10:06 +0530

    [DRIVERS] Enrich RBD driver
    This patch adds more useful APIs such as rbd_count(), rbd_get(), and
    rbd_find() for the RBD (RAM backed block device) driver.
    
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- drivers/block/rbd.c
+++ drivers/block/rbd.c
@@ -23,6 +23,7 @@
 
 #include <vmm_error.h>
 #include <vmm_heap.h>
+#include <vmm_spinlocks.h>
 #include <vmm_host_ram.h>
 #include <vmm_host_aspace.h>
 #include <vmm_modules.h>
@@ -34,9 +35,12 @@
 #define MODULE_DESC			"RAM Backed Block Driver"
 #define MODULE_AUTHOR			"Anup Patel"
 #define MODULE_LICENSE			"GPL"
-#define MODULE_IPRIORITY		(VMM_BLOCKDEV_CLASS_IPRIORITY+1)
+#define MODULE_IPRIORITY		(RBD_IPRIORITY)
 #define	MODULE_INIT			rbd_driver_init
 #define	MODULE_EXIT			rbd_driver_exit
+
+static LIST_HEAD(rbd_list);
+static DEFINE_SPINLOCK(rbd_list_lock);
 
 static int rbd_make_request(struct vmm_request_queue *rq, 
 			    struct vmm_request *r)
@@ -78,6 +82,7 @@ static struct rbd *__rbd_create(struct vmm_device *dev,
 				physical_size_t sz)
 {
 	struct rbd *d;
+	irq_flags_t flags;
 
 	if (!name) {
 		return NULL;
@@ -87,6 +92,7 @@ static struct rbd *__rbd_create(struct vmm_device *dev,
 	if (!d) {
 		goto free_nothing;
 	}
+	INIT_LIST_HEAD(&d->head);
 	d->addr = pa;
 	d->size = sz;
 
@@ -120,6 +126,11 @@ static struct rbd *__rbd_create(struct vmm_device *dev,
 		goto unreg_bdev;
 	}
 
+	/* Add to list of RBD instances */
+	vmm_spin_lock_irqsave(&rbd_list_lock, flags);
+	list_add_tail(&d->head, &rbd_list);
+	vmm_spin_unlock_irqrestore(&rbd_list_lock, flags);
+
 	return d;
 
 unreg_bdev:
@@ -138,14 +149,117 @@ struct rbd *rbd_create(const char *name,
 {
 	return __rbd_create(NULL, name, pa, sz);
 }
+VMM_EXPORT_SYMBOL(rbd_create);
 
 void rbd_destroy(struct rbd *d)
 {
-	vmm_host_ram_reserve(d->addr, d->size);
+	irq_flags_t flags;
+
+	/* Remove from list of RBD instances */
+	vmm_spin_lock_irqsave(&rbd_list_lock, flags);
+	list_del(&d->head);
+	vmm_spin_unlock_irqrestore(&rbd_list_lock, flags);
+
+	/* Unreserver RAM space */
+	vmm_host_ram_free(d->addr, d->size);
+
+	/* Unregister block device */
 	vmm_blockdev_unregister(d->bdev);
+
+	/* Free block device */
 	vmm_blockdev_free(d->bdev);
+
+	/* Free RBD instance */
 	vmm_free(d);
 }
+VMM_EXPORT_SYMBOL(rbd_destroy);
+
+struct rbd *rbd_find(const char *name)
+{
+	bool found;
+	struct dlist *l;
+	struct rbd *d;
+	irq_flags_t flags;
+
+	if (!name) {
+		return NULL;
+	}
+
+	found = FALSE;
+	d = NULL;
+
+	vmm_spin_lock_irqsave(&rbd_list_lock, flags);
+
+	list_for_each(l, &rbd_list) {
+		d = list_entry(l, struct rbd, head);
+		if (strcmp(d->bdev->name, name) == 0) {
+			found = TRUE;
+			break;
+		}
+	}
+
+	vmm_spin_unlock_irqrestore(&rbd_list_lock, flags);
+
+	if (!found) {
+		return NULL;
+	}
+
+	return d;
+}
+VMM_EXPORT_SYMBOL(rbd_find);
+
+struct rbd *rbd_get(int index)
+{
+	bool found;
+	struct dlist *l;
+	struct rbd *retval;
+	irq_flags_t flags;
+
+	if (index < 0) {
+		return NULL;
+	}
+
+	retval = NULL;
+	found = FALSE;
+
+	vmm_spin_lock_irqsave(&rbd_list_lock, flags);
+
+	list_for_each(l, &rbd_list) {
+		retval = list_entry(l, struct rbd, head);
+		if (!index) {
+			found = TRUE;
+			break;
+		}
+		index--;
+	}
+
+	vmm_spin_unlock_irqrestore(&rbd_list_lock, flags);
+
+	if (!found) {
+		return NULL;
+	}
+
+	return retval;
+}
+VMM_EXPORT_SYMBOL(rbd_get);
+
+u32 rbd_count(void)
+{
+	u32 retval = 0;
+	struct dlist *l;
+	irq_flags_t flags;
+
+	vmm_spin_lock_irqsave(&rbd_list_lock, flags);
+
+	list_for_each(l, &rbd_list) {
+		retval++;
+	}
+
+	vmm_spin_unlock_irqrestore(&rbd_list_lock, flags);
+
+	return retval;
+}
+VMM_EXPORT_SYMBOL(rbd_count);
 
 static int rbd_driver_probe(struct vmm_device *dev,
 			    const struct vmm_devtree_nodeid *devid)
