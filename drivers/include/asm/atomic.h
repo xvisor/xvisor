8aa14afeddf338566fa7523c66facc975b2fa391
Pranav Sawargaonkar <pranav.sawargaonkar@gmail.com>
2014-02-11 21:31:06 +0530

    [DRIVERS] Few linux compatible atomic operations
    Map linux-like atomic operations to their Xvisor counterparts.
    
    Signed-off-by: Pranav Sawargaonkar <pranav.sawargaonkar@gmail.com>

--- drivers/include/asm/atomic.h
+++ drivers/include/asm/atomic.h
@@ -6,9 +6,10 @@
 
 #define ATOMIC_INIT(x)		ARCH_ATOMIC_INITIALIZER(x)
 
-#define atomic_inc_return(x)	({ \
-					arch_atomic_inc(x); \
-					arch_atomic_read(x); \
-				})
+#define atomic_inc(x)		arch_atomic_inc(x)
+#define atomic_dec(x)		arch_atomic_dec(x)
+#define atomic_set(x, y)	arch_atomic_write(x, y)
+#define atomic_inc_return(x)	arch_atomic_add_return(x, 1)
+#define atomic_dec_return(x)	arch_atomic_sub_return(x, 1)
 
 #endif /* defined(_ASM_ATOMIC_H) */
