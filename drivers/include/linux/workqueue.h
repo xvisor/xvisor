242f4edf2f6329bf5339a5b622e0fb225e412fff
Pranav Sawargaonkar <pranav.sawargaonkar@gmail.com>
2014-02-11 21:37:31 +0530

    [DRIVERS] Minor improvements to linux compatible workqueue APIs
    This patch adds missing schedule_work() API and few system workqueue
    related global queues mapping to our Xvisor system workqueue.
    
    Signed-off-by: Pranav Sawargaonkar <pranav.sawargaonkar@gmail.com>

--- drivers/include/linux/workqueue.h
+++ drivers/include/linux/workqueue.h
@@ -1,6 +1,7 @@
 #ifndef _LINUX_WORKQUEUE_H_
 #define _LINUX_WORKQUEUE_H_
 
+#include <vmm_macros.h>
 #include <vmm_workqueue.h>
 
 #include <linux/timer.h>
@@ -10,9 +11,12 @@
 #define delayed_work			vmm_delayed_work
 #define workqueue_struct		vmm_workqueue
 
+#define system_wq			NULL
 #define system_long_wq			NULL
+#define system_power_efficient_wq	NULL
 
 #define queue_work(a, b)		vmm_workqueue_schedule_work(a, b)
+#define schedule_work(a)		vmm_workqueue_schedule_work(system_wq, a)
 #define cancel_work_sync(a)		vmm_workqueue_stop_work(a)
 #define cancel_delayed_work_sync(a)	vmm_workqueue_stop_delayed_work(a)
 
@@ -36,6 +40,11 @@ static inline int queue_delayed_work(struct workqueue_struct *wq,
 	u64 nsecs = delay;
 	nsecs = nsecs * (1000000000 / HZ);
 	return vmm_workqueue_schedule_delayed_work(wq, work, nsecs);
+}
+
+static inline struct delayed_work *to_delayed_work(struct work_struct *work)
+{
+	return container_of(work, struct delayed_work, work);
 }
 
 #endif /* _LINUX_WORKQUEUE_H_ */
