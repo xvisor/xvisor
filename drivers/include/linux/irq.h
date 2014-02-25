4c975fa9e74d75cbea0978cc4cd4b4bfb8e4208a
Pranav Sawargaonkar <pranav.sawargaonkar@gmail.com>
2014-02-25 07:42:55 +0530

    [DRIVERS] Adding linux headers (placeholder) for compilation.
    Adding placeholders for irq.h, of_address.h of_irq.h and of_platform.h
    Since many linux drivers includes these files so this will help
    in reducing compilation errors.
    
    Signed-off-by: Pranav Sawargaonkar <pranav.sawargaonkar@gmail.com>

---
+++ drivers/include/linux/irq.h
@@ -0,0 +1,7 @@
+#ifndef _LINUX_IRQ_H
+#define _LINUX_IRQ_H
+
+#include <linux/interrupt.h>
+
+/* This is just a placeholder */
+#endif
---
+++ drivers/include/linux/of_address.h
@@ -0,0 +1,8 @@
+#ifndef __OF_ADDRESS_H
+#define __OF_ADDRESS_H
+
+#include <linux/of.h>
+
+/* This is just a placeholder */
+
+#endif
