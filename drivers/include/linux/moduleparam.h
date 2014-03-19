69f132715e7c60865e76a911927714f6e10ae588
Ankit Jindal <thatsjindal@gmail.com>
2014-03-19 09:47:03 +0530

    [DRIVERS] Add place holder linux/moduleparam.h
    Signed-off-by: Ankit Jindal <thatsjindal@gmail.com>

---
+++ drivers/include/linux/moduleparam.h
@@ -0,0 +1,8 @@
+#ifndef _LINUX_MODULE_PARAM_H
+#define _LINUX_MODULE_PARAM_H
+
+#include <linux/module.h>
+
+/* FIXME: This file is just a place holder in most cases. */
+
+#endif /* _LINUX_MODULE_PARAM_H */
