30f22df26e6d3f90d30e40fc95f80775a75e88a0
Himanshu Chauhan <hschauhan@nulltrace.org>
2014-04-02 23:29:50 +0530

    [x86] Increase map window for i440fx, add some locking.
    Signed-off-by: Himanshu Chauhan <hschauhan@nulltrace.org>

--- emulators/pci/host/i440fx.c
+++ emulators/pci/host/i440fx.c
@@ -6,12 +6,12 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
@@ -29,6 +29,7 @@
 #include <vmm_timer.h>
 #include <vmm_types.h>
 #include <vmm_spinlocks.h>
+#include <vmm_compiler.h>
 #include <vio/vmm_vserial.h>
 #include <libs/stringlib.h>
 #include <emu/pci/pci_emu_core.h>
@@ -61,7 +62,8 @@ static int i440fx_default_log_lvl = I440FX_LOG_LVL_INFO;
 	}while(0);
 
 typedef struct i440fx_dev_registers {
-	u32 one;
+	u16 pmc_cfg;
+	u8 pam_regs[7];
 } i440fx_dev_registers_t;
 
 struct i440fx_state {
@@ -70,24 +72,57 @@ struct i440fx_state {
 	struct vmm_devtree_node *node;
 	struct pci_host_controller *controller;
 	i440fx_dev_registers_t *dev_regs;
+	u32 conf_add;
+	u32 conf_data;
 };
 
-static int i440fx_reg_write(struct i440fx_state *s, u32 addr, 
-			    u32 src_mask, u32 val)
+static u32 i440fx_config_read(struct pci_class *pci_class, u16 reg_offset)
 {
-	addr &= 7;
+	struct pci_host_controller *pcntrl = (struct pci_host_controller *)pci_class;
+	struct i440fx_state *s = container_of(&pcntrl, struct i440fx_state, controller);
 
-	I440FX_LOG(LVL_DEBUG, "Reg: 0x%x Value: 0x%x\n", addr, val);
+	return 0;
+}
+
+static int i440fx_config_write(struct pci_class *pci_class, u16 reg_offset, u32 data)
+{
+	struct pci_host_controller *pcntrl = (struct pci_host_controller *)pci_class;
+	struct i440fx_state *s = container_of(&pcntrl, struct i440fx_state, controller);
+
+	switch(reg_offset) {
+	}
 
 	return VMM_OK;
 }
 
-static int i440fx_reg_read(struct i440fx_state *s, u32 addr, u32 *dst)
+static int i440fx_reg_write(struct i440fx_state *s, u32 addr,
+			    u32 src_mask, u32 val)
 {
-	u32 ret = 0;
+	s->conf_add = val;
 
-	addr &= 7;
-	*dst = ret;
+	return VMM_OK;
+}
+
+static int i440fx_reg_read(struct i440fx_state *s, u32 addr, u32 *dst, u32 size)
+{
+	u16 bus, dev, func, reg_offs;
+
+	if (addr == 4) {
+		if (s->conf_add & 0x80000000) {
+			bus = (s->conf_add >> 16) & 0xff;
+			dev = (s->conf_add >> 11) & 0x1f;
+			func = (s->conf_add >> 8) & 0x7;
+			reg_offs = (s->conf_add >> 2) & 0x3f;
+
+			/* if bus and dev are 0, its bound to PMC */
+			if (bus == 0 && dev == 0) {
+				/* PMC is not a multi-function device */
+				if (func) return VMM_EINVALID;
+				*dst = pci_emu_read_config_space((struct pci_class *)s->controller,
+								 reg_offs, size);
+			}
+		}
+	}
 
 	return VMM_OK;
 }
@@ -98,13 +133,13 @@ static int i440fx_emulator_reset(struct vmm_emudev *edev)
 }
 
 static int i440fx_emulator_read8(struct vmm_emudev *edev,
-				 physical_addr_t offset, 
+				 physical_addr_t offset,
 				 u8 *dst)
 {
 	int rc;
 	u32 regval = 0x0;
 
-	rc = i440fx_reg_read(edev->priv, offset, &regval);
+	rc = i440fx_reg_read(edev->priv, offset, &regval, 1);
 	if (!rc) {
 		*dst = regval & 0xFF;
 	}
@@ -113,13 +148,13 @@ static int i440fx_emulator_read8(struct vmm_emudev *edev,
 }
 
 static int i440fx_emulator_read16(struct vmm_emudev *edev,
-				  physical_addr_t offset, 
+				  physical_addr_t offset,
 				  u16 *dst)
 {
 	int rc;
 	u32 regval = 0x0;
 
-	rc = i440fx_reg_read(edev->priv, offset, &regval);
+	rc = i440fx_reg_read(edev->priv, offset, &regval, 2);
 	if (!rc) {
 		*dst = regval & 0xFFFF;
 	}
@@ -128,28 +163,28 @@ static int i440fx_emulator_read16(struct vmm_emudev *edev,
 }
 
 static int i440fx_emulator_read32(struct vmm_emudev *edev,
-				  physical_addr_t offset, 
+				  physical_addr_t offset,
 				  u32 *dst)
 {
-	return i440fx_reg_read(edev->priv, offset, dst);
+	return i440fx_reg_read(edev->priv, offset, dst, 4);
 }
 
 static int i440fx_emulator_write8(struct vmm_emudev *edev,
-				   physical_addr_t offset, 
+				   physical_addr_t offset,
 				   u8 src)
 {
 	return i440fx_reg_write(edev->priv, offset, 0xFFFFFF00, src);
 }
 
 static int i440fx_emulator_write16(struct vmm_emudev *edev,
-				    physical_addr_t offset, 
+				    physical_addr_t offset,
 				    u16 src)
 {
 	return i440fx_reg_write(edev->priv, offset, 0xFFFF0000, src);
 }
 
 static int i440fx_emulator_write32(struct vmm_emudev *edev,
-				   physical_addr_t offset, 
+				   physical_addr_t offset,
 				   u32 src)
 {
 	return i440fx_reg_write(edev->priv, offset, 0x00000000, src);
@@ -184,8 +219,11 @@ static int i440fx_emulator_probe(struct vmm_guest *guest,
 
 	/* initialize class */
 	class = (struct pci_class *)s->controller;
+	INIT_MUTEX(class->lock);
 	class->conf_header.vendor_id = PCI_VENDOR_ID_INTEL;
 	class->conf_header.device_id = PCI_DEVICE_ID_INTEL_82441;
+	class->config_read = i440fx_config_read;
+	class->config_write = i440fx_config_write;
 
 	rc = vmm_devtree_read_u16(edev->node, "nr_buses",
 				  &s->controller->nr_buses);
@@ -243,8 +281,8 @@ static int i440fx_emulator_remove(struct vmm_emudev *edev)
 
 static struct vmm_devtree_nodeid i440fx_emuid_table[] = {
 	{
-		.type = "pci-host-controller", 
-		.compatible = "i440fx", 
+		.type = "pci-host-controller",
+		.compatible = "i440fx",
 	},
 	{ /* end of list */ },
 };
