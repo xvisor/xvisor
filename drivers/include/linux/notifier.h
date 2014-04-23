3790a83d512f27fcb5f5d5b591f1700b0e2275aa
Anup Patel <anup@brainfault.org>
2014-04-23 20:20:24 +0530

    [DRIVERS] Linux compatible linux/notifier.h for driver porting
    We already have linux-like notifier APIs in vmm_notifier.h hence
    this patch provides linux-compatible notifier APIs in linux/notifier.h.
    
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- drivers/include/linux/notifier.h
+++ drivers/include/linux/notifier.h
@@ -15,8 +15,43 @@
 #include <linux/errno.h>
 #include <linux/mutex.h>
 
-/* FIXME: This file just a place holder for most cases */
+#define	notifier_block				vmm_notifier_block
 
-#define	notifier_block			vmm_notifier_block
+#define notifier_from_errno(err)		vmm_notifier_from_errno(err)
+#define notifier_to_errno(ret)			vmm_notifier_to_errno(ret)
+
+#define atomic_notifier_head			vmm_atomic_notifier_chain
+#define blocking_notifier_head			vmm_blocking_notifier_chain
+#define raw_notifier_head			vmm_raw_notifier_chain
+#define srcu_notifier_head			vmm_atomic_notifier_chain
+
+#define ATOMIC_INIT_NOTIFIER_HEAD(nc)		ATOMIC_INIT_NOTIFIER_CHAIN(nc)
+#define BLOCKING_INIT_NOTIFIER_HEAD(nc)		BLOCKING_INIT_NOTIFIER_CHAIN(nc)
+#define RAW_INIT_NOTIFIER_HEAD(nc)		RAW_INIT_NOTIFIER_CHAIN(nc)
+#define srcu_init_notifier_head(nc)		ATOMIC_INIT_NOTIFIER_CHAIN(nc)
+
+#define srcu_cleanup_notifier_head(nc)		do { (void)nc; } while (0)
+
+#define ATOMIC_NOTIFIER_HEAD(name)		ATOMIC_NOTIFIER_CHAIN(name)
+#define BLOCKING_NOTIFIER_HEAD(name)		BLOCKING_NOTIFIER_CHAIN(name)
+#define RAW_NOTIFIER_HEAD(name)			RAW_NOTIFIER_CHAIN(name)
+
+#define atomic_notifier_chain_register(nc, nb)	vmm_atomic_notifier_register(nc, nb)
+#define blocking_notifier_chain_register(nc, nb)	\
+						vmm_blocking_notifier_register(nc, nb)
+#define raw_notifier_chain_register(nc, nb)	vmm_raw_notifier_register(nc, nb)
+#define srcu_notifier_chain_register(nc, nb)	vmm_atomic_notifier_register(nc, nb)
+
+#define atomic_notifier_chain_unregister(nc, nb) vmm_atomic_notifier_unregister(nc, nb)
+#define blocking_notifier_chain_unregister(nc, nb)	\
+						vmm_blocking_notifier_unregister(nc, nb)
+#define raw_notifier_chain_unregister(nc, nb)	vmm_raw_notifier_unregister(nc, nb)
+#define srcu_notifier_chain_unregister(nc, nb)	vmm_atomic_notifier_unregister(nc, nb)
+
+#define atomic_notifier_call_chain(nc, val, v)	vmm_atomic_notifier_call(nc, val, v)
+#define blocking_notifier_call_chain(nc, val, v)	\
+						vmm_blocking_notifier_call(nc, val, v)
+#define raw_notifier_call_chain(nc, val, v)	vmm_blocking_notifier_call(nc, val, v)
+#define srcu_notifier_call_chain(nc, val, v)	vmm_atomic_notifier_call(nc, val, v)
 
 #endif /* _LINUX_NOTIFIER_H */
