fd280d4870dc77d80d5bee37a3aa4b7229cca0be
Anup Patel <anup@brainfault.org>
2014-05-06 23:25:37 +0530

    [ARM] generic: Add DTS files for booting Xvisor ARM32 on VExpress-A15
    For debugging purpose we might need to boot Xvisor ARM32 (without
    virtualization extension) on VExpress-A15 emulated by ARM Fast Models
    hence this patch adds required DTS files.
    
    Other than debugging, there is no real use-case is using Xvisor ARM32
    on HW having virtualization extensions. We should always use Xvisor ARM32ve
    for HW having virtualization extensions.
    
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- arch/arm/board/generic/dts/vexpress/objects.mk
+++ arch/arm/board/generic/dts/vexpress/objects.mk
@@ -28,4 +28,6 @@ board-dtbs-$(CONFIG_ARMV7A_VE)+=dts/vexpress/a15/one_guest_vexpress-a9.dtb
 board-dtbs-$(CONFIG_ARMV7A_VE)+=dts/vexpress/a15/one_guest_vexpress-a15.dtb
 board-dtbs-$(CONFIG_ARMV7A_VE)+=dts/vexpress/a15/two_guest_pb-a8.dtb
 board-dtbs-$(CONFIG_ARMV7A_VE)+=dts/vexpress/a15/two_guest_vexpress-a15.dtb
+board-dtbs-$(CONFIG_ARMV7A)+=dts/vexpress/a15-nove/one_guest_pb-a8.dtb
+board-dtbs-$(CONFIG_ARMV7A)+=dts/vexpress/a15-nove/one_guest_vexpress-a9.dtb
 
