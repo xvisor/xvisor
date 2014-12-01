a21cf2927c15874d7a8cf30adc52ea10ffbca5c5
Jean-Christophe Dubois <jcd@tribudubois.net>
2014-12-01 09:06:32 +0530

    [DRIVERS] clk: move i.MX clk driver to drivers/clk directory
    Signed-off-by: Jean-Christophe Dubois <jcd@tribudubois.net>

--- drivers/clk/imx/objects.mk
+++ drivers/clk/imx/objects.mk
@@ -1,28 +1,39 @@
 #/**
-# Copyright (C) 2014 Institut de Recherche Technologique SystemX and OpenWide.
+# Copyright (c) 2014 Jean-Christophe Dubois.
 # All rights reserved.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2, or (at your option)
 # any later version.
-#
+# 
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-#
+# 
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 #
 # @file objects.mk
-# @author Jimmy Durand Wesolowski (jimmy.durand-wesolowski@openwide.fr)
-# @brief list of IMX platform objects.
+# @author Jean-Christophe Dubois (jcd@tribudubois.net)
+# @brief list of imx SOC clocking objects
 # */
 
-board-common-objs-$(CONFIG_IMX_CPU)+= imx/cpu.o
-board-common-objs-$(CONFIG_IMX6Q_CLK)+= imx/gpc.o
-board-common-objs-$(CONFIG_IMX6Q_PM)+= imx/pm-imx6q.o
-board-common-objs-$(CONFIG_IMX6_CMD)+= imx/cmd_imx6.o
+drivers-objs-$(CONFIG_COMMON_CLK_MXC)+= clk/imx/clk-imx-drv.o
 
+clk-imx-drv-y += clk-busy.o
+clk-imx-drv-y += clk-fixup-div.o
+clk-imx-drv-y += clk-gate2.o
+clk-imx-drv-y += clk-imx6q.o
+clk-imx-drv-y += clk-pllv3.o
+clk-imx-drv-y += clk.o
+clk-imx-drv-y += clk-fixup-mux.o
+clk-imx-drv-y += clk-pfd.o
+
+%/clk-imx-drv.o: $(foreach obj,$(clk-imx-drv-y),%/$(obj))
+	$(call merge_objs,$@,$^)
+
+%/clk-imx-drv.dep: $(foreach dep,$(clk-imx-drv-y:.o=.dep),%/$(dep))
+	$(call merge_deps,$@,$^)
