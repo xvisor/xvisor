8bb8db77d897582acce5748ecd023ef24f8f18da
Anup Patel <anup@brainfault.org>
2014-12-19 09:38:52 +0530

    [DRIVERS] Using the block device NOP strategy on the M25P80 flash driver.
    Signed-off-by: Jimmy Durand Wesolowski <jimmy.durand-wesolowski@openwide.fr>
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- drivers/mtd/devices/m25p80_blockdev.c
+++ drivers/mtd/devices/m25p80_blockdev.c
@@ -25,19 +25,19 @@
 #include <linux/mtd/mtd.h>
 #include <linux/device.h>
 #include <block/vmm_blockdev.h>
+#include <block/vmm_blockrq_nop.h>
 
 #include "m25p80.h"
 
-
 static void m25p_erase_callback(struct erase_info *info)
 {
+	/* Nothing to do here. */
 }
 
-static int m25p_erase(struct vmm_request_queue *rq,
-		      struct vmm_request *r,
-		      physical_addr_t off,
-		      physical_size_t len,
-		      struct mtd_info *mtd)
+static int m25p_erase_write(struct vmm_request *r,
+			    physical_addr_t off,
+			    physical_size_t len,
+			    struct mtd_info *mtd)
 {
 	struct erase_info info;
 	unsigned int retlen = 0;
@@ -49,70 +49,58 @@ static int m25p_erase(struct vmm_request_queue *rq,
 
 	if (mtd_erase(mtd, &info)) {
 		dev_err(&r->bdev->dev, "Erasing at 0x%08X failed\n", off);
-		goto error;
+		return VMM_EIO;
 	}
 
 	if (mtd_write(mtd, off, len, &retlen, r->data)) {
 		dev_err(&r->bdev->dev, "Writing at 0x%08X failed\n", off);
-		goto error;
+		return VMM_EIO;
 	}
 
 	if (retlen < len) {
 		dev_warn(&r->bdev->dev, "Only 0x%X/0x%X bytes have been "
 			 "written at 0x%08X\n", retlen, len, off);
-		goto error;
+		return VMM_EIO;
 	}
-	vmm_blockdev_complete_request(r);
-	return VMM_OK;
-
-error:
-	vmm_blockdev_fail_request(r);
 	return VMM_OK;
 }
 
-static int m25p_make_request(struct vmm_request_queue *rq,
-			     struct vmm_request *r)
+int m25p_read(struct vmm_blockrq_nop *rqnop,
+	      struct vmm_request *r, void *priv)
 {
+	struct m25p *flash = priv;
 	unsigned int retlen = 0;
-	physical_addr_t off = 0;
-	physical_size_t len = 0;
-	struct m25p *flash = rq->priv;
 
-	off = r->lba << flash->mtd.erasesize_shift;
-	len = r->bcnt << flash->mtd.erasesize_shift;
-	switch (r->type) {
-	case VMM_REQUEST_READ:
-		mtd_read(&flash->mtd, off, len, &retlen, r->data);
+	physical_addr_t off = r->lba << flash->mtd.erasesize_shift;
+	physical_size_t len = r->bcnt << flash->mtd.erasesize_shift;
 
-		if (retlen < len) {
-			vmm_blockdev_fail_request(r);
-		}
-		else {
-			vmm_blockdev_complete_request(r);
-		}
-		break;
-	case VMM_REQUEST_WRITE:
-		while (mtd_block_isbad(&flash->mtd, off)) {
-			vmm_printf("Block at 0x%X is bad, skipping...\n",
-				   off);
-			off += flash->mtd.erasesize;
-		}
-		return m25p_erase(rq, r, off, len, &flash->mtd);
-
-		break;
-	default:
-		vmm_blockdev_fail_request(r);
-		break;
-	};
+	mtd_read(&flash->mtd, off, len, &retlen, r->data);
+	if (retlen < len) {
+		return VMM_EIO;
+	}
 
 	return VMM_OK;
 }
 
-static int m25p_abort_request(struct vmm_request_queue *rq,
-			      struct vmm_request *r)
+int m25p_write(struct vmm_blockrq_nop *rqnop,
+	       struct vmm_request *r, void *priv)
 {
-	/* Do nothing to abort */
-	return VMM_OK;
+	struct m25p *flash = priv;
+	physical_addr_t off = r->lba << flash->mtd.erasesize_shift;
+	physical_size_t len = r->bcnt << flash->mtd.erasesize_shift;
+
+	while (mtd_block_isbad(&flash->mtd, off)) {
+		vmm_printf("%s: block at 0x%X is bad, skipping...\n",
+			   __func__, off);
+		off += flash->mtd.erasesize;
+	}
+
+	return m25p_erase_write(r, off, len, &flash->mtd);
+}
+
+void m25p_flush(struct vmm_blockrq_nop *rqnop, void *priv)
+{
+	/* Nothing to do here. */
 }
 
 int m25p_register_blockdev(struct vmm_device *dev)
@@ -120,6 +108,7 @@ int m25p_register_blockdev(struct vmm_device *dev)
 	int			err = 0;
 	struct spi_device	*spi = vmm_devdrv_get_data(dev);
 	struct vmm_blockdev	*bdev = NULL;
+	struct vmm_blockrq_nop	*rqnop = NULL;
 	struct m25p		*flash = NULL;
 
 	if (!spi)
@@ -143,19 +132,17 @@ int m25p_register_blockdev(struct vmm_device *dev)
 	bdev->block_size = flash->mtd.erasesize;
 
 	/* Setup request queue for block device instance */
-	bdev->rq = vmm_zalloc(sizeof(struct vmm_request_queue));
-	if (!bdev->rq) {
+	rqnop = vmm_blockrq_nop_create(dev->name, 256,
+			m25p_read, m25p_write, m25p_flush, flash);
+	if (!rqnop) {
 		vmm_blockdev_free(flash->blockdev);
 		return VMM_ENOMEM;
 	}
-	INIT_REQUEST_QUEUE(bdev->rq);
-	bdev->rq->make_request = m25p_make_request;
-	bdev->rq->abort_request = m25p_abort_request;
-	bdev->rq->priv = flash;
+	bdev->rq = vmm_blockrq_nop_to_rq(rqnop);
 
 	/* Register block device instance */
 	if (VMM_OK != (err = vmm_blockdev_register(bdev))) {
-		vmm_free(flash->blockdev->rq);
+		vmm_blockrq_nop_destroy(rqnop);
 		vmm_blockdev_free(flash->blockdev);
 		dev_err(dev, "Failed to register blockdev\n");
 		return err;
@@ -167,17 +154,18 @@ int m25p_register_blockdev(struct vmm_device *dev)
 
 int m25p_unregister_blockdev(struct vmm_device *dev)
 {
-	struct spi_device *spi = vmm_devdrv_get_data(dev);
-	struct m25p	*flash = NULL;
+	struct spi_device	*spi = vmm_devdrv_get_data(dev);
+	struct m25p		*flash = NULL;
+	struct vmm_blockrq_nop	*rqnop = NULL;
 
 	if (!spi)
-		return 1;
-	flash = spi_get_drvdata(spi);
+		return VMM_EINVALID;
 
-	if (vmm_blockdev_unregister(flash->blockdev)) {
-		return VMM_EFAIL;
-	}
-	vmm_free(flash->blockdev->rq);
+	flash = spi_get_drvdata(spi);
+	rqnop = vmm_blockrq_nop_from_rq(flash->blockdev->rq);
+
+	vmm_blockdev_unregister(flash->blockdev);
+	vmm_blockrq_nop_destroy(rqnop);
 	vmm_blockdev_free(flash->blockdev);
 
 	return VMM_OK;
