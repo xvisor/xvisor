71bb9015406088bc0753db0195fec8cd287b0748
Anup Patel <anup@brainfault.org>
2014-12-19 09:32:18 +0530

    [DRIVERS] rbd: Fix typo in source header
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- drivers/block/rbd.c
+++ drivers/block/rbd.c
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- * @file rbd.h
+ * @file rbd.c
  * @author Anup Patel (anup@brainfault.org)
  * @brief RAM backed block device driver.
  */
