b0604a367065cbed12318f3ddfc7b613080b1a55
Anup Patel <anup@brainfault.org>
2015-01-08 18:50:15 +0530

    [DRIVERS] usb: Don't include linux compatiblity header in drv/usb.h
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- drivers/include/drv/usb/roothubdesc.h
+++ drivers/include/drv/usb/roothubdesc.h
@@ -36,148 +36,150 @@
 #ifndef __USB_ROOTHUBDESC_H__
 #define __USB_ROOTHUBDESC_H__
 
+#include <vmm_types.h>
+
 /* Device descriptor */
-static __u8 root_hub_dev_desc[] = {
-	0x12,		/* __u8  bLength; */
-	0x01,		/* __u8  bDescriptorType; Device */
-	0x10,		/* __u16 bcdUSB; v1.1 */
+static u8 root_hub_dev_desc[] = {
+	0x12,		/* u8  bLength; */
+	0x01,		/* u8  bDescriptorType; Device */
+	0x10,		/* u16 bcdUSB; v1.1 */
 	0x01,
-	0x09,		/* __u8  bDeviceClass; HUB_CLASSCODE */
-	0x00,		/* __u8  bDeviceSubClass; */
-	0x00,		/* __u8  bDeviceProtocol; */
-	0x08,		/* __u8  bMaxPacketSize0; 8 Bytes */
-	0x00,		/* __u16 idVendor; */
+	0x09,		/* u8  bDeviceClass; HUB_CLASSCODE */
+	0x00,		/* u8  bDeviceSubClass; */
+	0x00,		/* u8  bDeviceProtocol; */
+	0x08,		/* u8  bMaxPacketSize0; 8 Bytes */
+	0x00,		/* u16 idVendor; */
 	0x00,
-	0x00,		/* __u16 idProduct; */
+	0x00,		/* u16 idProduct; */
 	0x00,
-	0x00,		/* __u16 bcdDevice; */
+	0x00,		/* u16 bcdDevice; */
 	0x00,
-	0x01,		/* __u8  iManufacturer; */
-	0x02,		/* __u8  iProduct; */
-	0x03,		/* __u8  iSerialNumber; */
-	0x01,		/* __u8  bNumConfigurations; */
+	0x01,		/* u8  iManufacturer; */
+	0x02,		/* u8  iProduct; */
+	0x03,		/* u8  iSerialNumber; */
+	0x01,		/* u8  bNumConfigurations; */
 };
 
 /* Configuration descriptor */
-static __u8 root_hub_config_desc[] = {
-	0x09,		/* __u8  bLength; */
-	0x02,		/* __u8  bDescriptorType; Configuration */
-	0x19,		/* __u16 wTotalLength; */
+static u8 root_hub_config_desc[] = {
+	0x09,		/* u8  bLength; */
+	0x02,		/* u8  bDescriptorType; Configuration */
+	0x19,		/* u16 wTotalLength; */
 	0x00,
-	0x01,		/* __u8  bNumInterfaces; */
-	0x01,		/* __u8  bConfigurationValue; */
-	0x00,		/* __u8  iConfiguration; */
-	0x40,		/* __u8  bmAttributes;
+	0x01,		/* u8  bNumInterfaces; */
+	0x01,		/* u8  bConfigurationValue; */
+	0x00,		/* u8  iConfiguration; */
+	0x40,		/* u8  bmAttributes;
 			 *       Bit 7: Bus-powered
 			 *       6: Self-powered,
 			 *       5 Remote-wakwup,
 			 *       4..0: resvd
 			 */
-	0x00,		/* __u8  MaxPower; */
+	0x00,		/* u8  MaxPower; */
 	/* interface */
-	0x09,		/* __u8  if_bLength; */
-	0x04,		/* __u8  if_bDescriptorType; Interface */
-	0x00,		/* __u8  if_bInterfaceNumber; */
-	0x00,		/* __u8  if_bAlternateSetting; */
-	0x01,		/* __u8  if_bNumEndpoints; */
-	0x09,		/* __u8  if_bInterfaceClass; HUB_CLASSCODE */
-	0x00,		/* __u8  if_bInterfaceSubClass; */
-	0x00,		/* __u8  if_bInterfaceProtocol; */
-	0x00,		/* __u8  if_iInterface; */
+	0x09,		/* u8  if_bLength; */
+	0x04,		/* u8  if_bDescriptorType; Interface */
+	0x00,		/* u8  if_bInterfaceNumber; */
+	0x00,		/* u8  if_bAlternateSetting; */
+	0x01,		/* u8  if_bNumEndpoints; */
+	0x09,		/* u8  if_bInterfaceClass; HUB_CLASSCODE */
+	0x00,		/* u8  if_bInterfaceSubClass; */
+	0x00,		/* u8  if_bInterfaceProtocol; */
+	0x00,		/* u8  if_iInterface; */
 	/* endpoint */
-	0x07,		/* __u8  ep_bLength; */
-	0x05,		/* __u8  ep_bDescriptorType; Endpoint */
-	0x81,		/* __u8  ep_bEndpointAddress; IN Endpoint 1 */
-	0x03,		/* __u8  ep_bmAttributes; Interrupt */
-	0x02,		/* __u16 ep_wMaxPacketSize; ((MAX_ROOT_PORTS + 1) / 8 */
+	0x07,		/* u8  ep_bLength; */
+	0x05,		/* u8  ep_bDescriptorType; Endpoint */
+	0x81,		/* u8  ep_bEndpointAddress; IN Endpoint 1 */
+	0x03,		/* u8  ep_bmAttributes; Interrupt */
+	0x02,		/* u16 ep_wMaxPacketSize; ((MAX_ROOT_PORTS + 1) / 8 */
 	0x00,
-	0xff,		/* __u8  ep_bInterval; 255 ms */
+	0xff,		/* u8  ep_bInterval; 255 ms */
 };
 
 #ifdef WANT_USB_ROOT_HUB_HUB_DESC
-static unsigned char root_hub_hub_desc[] = {
-	0x09,		/* __u8  bLength; */
-	0x29,		/* __u8  bDescriptorType; Hub-descriptor */
-	0x02,		/* __u8  bNbrPorts; */
-	0x00,		/* __u16 wHubCharacteristics; */
+static u8 root_hub_hub_desc[] = {
+	0x09,		/* u8  bLength; */
+	0x29,		/* u8  bDescriptorType; Hub-descriptor */
+	0x02,		/* u8  bNbrPorts; */
+	0x00,		/* u16 wHubCharacteristics; */
 	0x00,
-	0x01,		/* __u8  bPwrOn2pwrGood; 2ms */
-	0x00,		/* __u8  bHubContrCurrent; 0 mA */
-	0x00,		/* __u8  DeviceRemovable; *** 7 Ports max *** */
-	0xff,		/* __u8  PortPwrCtrlMask; *** 7 ports max *** */
+	0x01,		/* u8  bPwrOn2pwrGood; 2ms */
+	0x00,		/* u8  bHubContrCurrent; 0 mA */
+	0x00,		/* u8  DeviceRemovable; *** 7 Ports max *** */
+	0xff,		/* u8  PortPwrCtrlMask; *** 7 ports max *** */
 };
 #endif
 
-static unsigned char root_hub_str_index0[] = {
-	0x04,		/* __u8  bLength; */
-	0x03,		/* __u8  bDescriptorType; String-descriptor */
-	0x09,		/* __u8  lang ID */
-	0x04,		/* __u8  lang ID */
+static u8 root_hub_str_index0[] = {
+	0x04,		/* u8  bLength; */
+	0x03,		/* u8  bDescriptorType; String-descriptor */
+	0x09,		/* u8  lang ID */
+	0x04,		/* u8  lang ID */
 };
 
-static unsigned char root_hub_str_index1[] = {
-	14,		/* __u8  bLength; */
-	0x03,		/* __u8  bDescriptorType; String-descriptor */
-	'X',		/* __u8  Unicode */
-	0,		/* __u8  Unicode */
-	'v',		/* __u8  Unicode */
-	0,		/* __u8  Unicode */
-	'i',		/* __u8  Unicode */
-	0,		/* __u8  Unicode */
-	's',		/* __u8  Unicode */
-	0,		/* __u8  Unicode */
-	'o',		/* __u8  Unicode */
-	0,		/* __u8  Unicode */
-	'r',		/* __u8  Unicode */
-	0,		/* __u8  Unicode */
+static u8 root_hub_str_index1[] = {
+	14,		/* u8  bLength; */
+	0x03,		/* u8  bDescriptorType; String-descriptor */
+	'X',		/* u8  Unicode */
+	0,		/* u8  Unicode */
+	'v',		/* u8  Unicode */
+	0,		/* u8  Unicode */
+	'i',		/* u8  Unicode */
+	0,		/* u8  Unicode */
+	's',		/* u8  Unicode */
+	0,		/* u8  Unicode */
+	'o',		/* u8  Unicode */
+	0,		/* u8  Unicode */
+	'r',		/* u8  Unicode */
+	0,		/* u8  Unicode */
 };
 
-static unsigned char root_hub_str_index2[] = {
-	32,		/* __u8  bLength; */
-	0x03,		/* __u8  bDescriptorType; String-descriptor */
-	'X',		/* __u8  Unicode */
-	0,		/* __u8  Unicode */
-	'v',		/* __u8  Unicode */
-	0,		/* __u8  Unicode */
-	'i',		/* __u8  Unicode */
-	0,		/* __u8  Unicode */
-	's',		/* __u8  Unicode */
-	0,		/* __u8  Unicode */
-	'o',		/* __u8  Unicode */
-	0,		/* __u8  Unicode */
-	'r',		/* __u8  Unicode */
-	0,		/* __u8  Unicode */
-	' ',		/* __u8  Unicode */
-	0,		/* __u8  Unicode */
-	'R',		/* __u8  Unicode */
-	0,		/* __u8  Unicode */
-	'o',		/* __u8  Unicode */
-	0,		/* __u8  Unicode */
-	'o',		/* __u8  Unicode */
-	0,		/* __u8  Unicode */
-	't',		/* __u8  Unicode */
-	0,		/* __u8  Unicode */
-	' ',		/* __u8  Unicode */
-	0,		/* __u8  Unicode */
-	'H',		/* __u8  Unicode */
-	0,		/* __u8  Unicode */
-	'u',		/* __u8  Unicode */
-	0,		/* __u8  Unicode */
-	'b',		/* __u8  Unicode */
-	0,		/* __u8  Unicode */
+static u8 root_hub_str_index2[] = {
+	32,		/* u8  bLength; */
+	0x03,		/* u8  bDescriptorType; String-descriptor */
+	'X',		/* u8  Unicode */
+	0,		/* u8  Unicode */
+	'v',		/* u8  Unicode */
+	0,		/* u8  Unicode */
+	'i',		/* u8  Unicode */
+	0,		/* u8  Unicode */
+	's',		/* u8  Unicode */
+	0,		/* u8  Unicode */
+	'o',		/* u8  Unicode */
+	0,		/* u8  Unicode */
+	'r',		/* u8  Unicode */
+	0,		/* u8  Unicode */
+	' ',		/* u8  Unicode */
+	0,		/* u8  Unicode */
+	'R',		/* u8  Unicode */
+	0,		/* u8  Unicode */
+	'o',		/* u8  Unicode */
+	0,		/* u8  Unicode */
+	'o',		/* u8  Unicode */
+	0,		/* u8  Unicode */
+	't',		/* u8  Unicode */
+	0,		/* u8  Unicode */
+	' ',		/* u8  Unicode */
+	0,		/* u8  Unicode */
+	'H',		/* u8  Unicode */
+	0,		/* u8  Unicode */
+	'u',		/* u8  Unicode */
+	0,		/* u8  Unicode */
+	'b',		/* u8  Unicode */
+	0,		/* u8  Unicode */
 };
 
-static unsigned char root_hub_str_index3[] = {
-	10,		/* __u8  bLength; */
-	0x03,		/* __u8  bDescriptorType; String-descriptor */
-	'0',		/* __u8  Unicode */
-	0,		/* __u8  Unicode */
-	'0',		/* __u8  Unicode */
-	0,		/* __u8  Unicode */
-	'0',		/* __u8  Unicode */
-	0,		/* __u8  Unicode */
-	'0',		/* __u8  Unicode */
-	0,		/* __u8  Unicode */
+static u8 root_hub_str_index3[] = {
+	10,		/* u8  bLength; */
+	0x03,		/* u8  bDescriptorType; String-descriptor */
+	'0',		/* u8  Unicode */
+	0,		/* u8  Unicode */
+	'0',		/* u8  Unicode */
+	0,		/* u8  Unicode */
+	'0',		/* u8  Unicode */
+	0,		/* u8  Unicode */
+	'0',		/* u8  Unicode */
+	0,		/* u8  Unicode */
 };
 
 #endif
