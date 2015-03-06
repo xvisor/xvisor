aef3aa532261d6ccd59943e48831e20ad035fda5
Anup Patel <anup@brainfault.org>
2015-03-06 21:37:25 +0530

    [CORE] vio: First-cut implementation of virtual disk framework
    This patch adds first-cut untested implementation of virtual disk
    framework.
    
    The goal of virtual disk framework is make life easier for folks
    who develop disk controller emulators. A virtual disk has to be
    attached to a block device for read/write request to work on the
    virtual disk. This new framework provides a common way of tracking
    all virtual disk instances of a guest irrespective to the disk
    controller type emulated for guest.
    
    Signed-off-by: Anup Patel <anup@brainfault.org>

---
+++ core/include/vio/vmm_vdisk.h
@@ -0,0 +1,147 @@
+/**
+ * Copyright (c) 2015 Anup Patel.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * @file vmm_vdisk.h
+ * @author Anup Patel (anup@brainfault.org)
+ * @brief header file for virtual disk framework
+ */
+
+/* The virtual disk framework helps disk controller emulators
+ * in emulating disk read/write operations irrespective to
+ * disk controller type. It also provides a convient way of
+ * tracking various virtual disk instances of a guest.
+ *
+ * Each virtual disk can be attached to a block device. If the
+ * block device is unregistered then virtual disk is dettached
+ * automatically using block device notifiers.
+ */
+
+#ifndef _VMM_VDISK_H__
+#define _VMM_VDISK_H__
+
+#include <vmm_types.h>
+#include <vmm_spinlocks.h>
+#include <vmm_notifier.h>
+#include <block/vmm_blockdev.h>
+#include <libs/list.h>
+
+#define VMM_VDISK_IPRIORITY		(VMM_BLOCKDEV_CLASS_IPRIORITY + 1)
+
+struct vmm_vdisk_request;
+struct vmm_vdisk;
+
+/** Types of block IO request */
+enum vmm_vdisk_request_type {
+	VMM_VDISK_REQUEST_UNKNOWN=0,
+	VMM_VDISK_REQUEST_READ=1,
+	VMM_VDISK_REQUEST_WRITE=2
+};
+
+/** Representation of a virtual disk request  */
+struct vmm_vdisk_request {
+	struct vmm_vdisk *vdisk;
+	struct vmm_request r;
+};
+
+/** Representation of a virtual disk */
+struct vmm_vdisk {
+	struct dlist head;
+	char name[VMM_FIELD_NAME_SIZE];
+	u32 block_size;
+
+	void (*completed)(struct vmm_vdisk *, struct vmm_vdisk_request *);
+	void (*failed)(struct vmm_vdisk *, struct vmm_vdisk_request *);
+
+	vmm_spinlock_t blk_lock; /* Protect blk pointer */
+	struct vmm_blockdev *blk;
+	u32 blk_factor;
+
+	void *priv;
+};
+
+/* Notifier event when virtual disk is created */
+#define VMM_VDISK_EVENT_CREATE		0x01
+/* Notifier event when virtual disk is destroyed */
+#define VMM_VDISK_EVENT_DESTROY		0x02
+
+/** Representation of virtual disk notifier event */
+struct vmm_vdisk_event {
+	struct vmm_vdisk *vdisk;
+	void *data;
+};
+
+/** Register a notifier client to receive virtual disk events */
+int vmm_vdisk_register_client(struct vmm_notifier_block *nb);
+
+/** Unregister a notifier client to not receive virtual disk events */
+int vmm_vdisk_unregister_client(struct vmm_notifier_block *nb);
+
+/** Retrive private context of virtual disk */
+static inline void *vmm_vdisk_priv(struct vmm_vdisk *vdisk)
+{
+	return (vdisk) ? vdisk->priv : NULL;
+}
+
+/** Submit IO request to virtual disk */
+int vmm_vdisk_submit_request(struct vmm_vdisk *vdisk,
+			     struct vmm_vdisk_request *vreq,
+			     enum vmm_vdisk_request_type type,
+			     u64 lba, void *data, u32 data_len);
+
+/* Abort IO request from virtual disk */
+int vmm_vdisk_abort_request(struct vmm_vdisk *vdisk,
+			    struct vmm_vdisk_request *vreq);
+
+/** Flush cached IO from virtual disk */
+int vmm_vdisk_flush_cache(struct vmm_vdisk *vdisk);
+
+/** Block count of virtual disk based on attached block device */
+u64 vmm_vdisk_capacity(struct vmm_vdisk *vdisk);
+
+/** Block size of virtual disk based on attached block device */
+u32 vmm_vdisk_block_size(struct vmm_vdisk *vdisk);
+
+/** Current block device attached to virtual disk */
+int vmm_vdisk_current_block_device(struct vmm_vdisk *vdisk,
+				   char *buf, u32 buf_len);
+
+/** Attach block device to virtual disk */
+void vmm_vdisk_attach_block_device(struct vmm_vdisk *vdisk,
+				   const char *bdev_name);
+
+/** Create a virtual disk */
+struct vmm_vdisk *vmm_vdisk_create(const char *name, u32 block_size,
+	void (*completed)(struct vmm_vdisk *, struct vmm_vdisk_request *),
+	void (*failed)(struct vmm_vdisk *, struct vmm_vdisk_request *),
+	const char *bdev_name,
+	void *priv);
+
+/** Destroy a virtual disk */
+int vmm_vdisk_destroy(struct vmm_vdisk *vdisk);
+
+/** Find a virtual disk with given name */
+struct vmm_vdisk *vmm_vdisk_find(const char *name);
+
+/** Iterate over each virtual disk */
+int vmm_vdisk_iterate(struct vmm_vdisk *start, void *data,
+		      int (*fn)(struct vmm_vdisk *vdisk, void *data));
+
+/** Count of available virtual disks */
+u32 vmm_vdisk_count(void);
+
+#endif
---
+++ core/vio/vmm_vdisk.c
@@ -0,0 +1,579 @@
+/**
+ * Copyright (c) 2015 Anup Patel.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * @file vmm_vdisk.c
+ * @author Anup Patel (anup@brainfault.org)
+ * @brief source code for virtual disk framework
+ */
+
+#include <vmm_error.h>
+#include <vmm_compiler.h>
+#include <vmm_heap.h>
+#include <vmm_mutex.h>
+#include <vmm_stdio.h>
+#include <vmm_modules.h>
+#include <vio/vmm_vdisk.h>
+#include <libs/stringlib.h>
+#include <libs/mathlib.h>
+
+#undef DEBUG
+
+#ifdef DEBUG
+#define DPRINTF(msg...)			vmm_printf(msg)
+#else
+#define DPRINTF(msg...)
+#endif
+
+#define MODULE_DESC			"Virtual Disk Framework"
+#define MODULE_AUTHOR			"Anup Patel"
+#define MODULE_LICENSE			"GPL"
+#define MODULE_IPRIORITY		(VMM_VDISK_IPRIORITY)
+#define	MODULE_INIT			vmm_vdisk_init
+#define	MODULE_EXIT			vmm_vdisk_exit
+
+struct vmm_vdisk_ctrl {
+	struct vmm_mutex vdisk_list_lock;
+        struct dlist vdisk_list;
+	struct vmm_blocking_notifier_chain notifier_chain;
+	struct vmm_notifier_block blk_client;
+};
+
+static struct vmm_vdisk_ctrl vdctrl;
+
+int vmm_vdisk_register_client(struct vmm_notifier_block *nb)
+{
+	return vmm_blocking_notifier_register(&vdctrl.notifier_chain, nb);
+}
+VMM_EXPORT_SYMBOL(vmm_vdisk_register_client);
+
+int vmm_vdisk_unregister_client(struct vmm_notifier_block *nb)
+{
+	return vmm_blocking_notifier_unregister(&vdctrl.notifier_chain, nb);
+}
+VMM_EXPORT_SYMBOL(vmm_vdisk_unregister_client);
+
+static void vdisk_req_completed(struct vmm_request *r)
+{
+	struct vmm_vdisk_request *vreq =
+			container_of(r, struct vmm_vdisk_request, r);
+	struct vmm_vdisk *vdisk = vreq->vdisk;
+
+	if (vdisk->completed) {
+		vdisk->completed(vdisk, vreq);
+	}
+
+	DPRINTF("%s: vdisk=%s lba=0x%llx bcnt=%d\n",
+		__func__, vdisk->name, (u64)r->lba, r->bcnt);
+}
+
+static void vdisk_req_failed(struct vmm_request *r)
+{
+	struct vmm_vdisk_request *vreq =
+			container_of(r, struct vmm_vdisk_request, r);
+	struct vmm_vdisk *vdisk = vreq->vdisk;
+
+	if (vdisk->failed) {
+		vdisk->failed(vdisk, vreq);
+	}
+
+	DPRINTF("%s: vdisk=%s lba=0x%llx bcnt=%d\n",
+		__func__, vdisk->name, (u64)r->lba, r->bcnt);
+}
+
+int vmm_vdisk_submit_request(struct vmm_vdisk *vdisk,
+			     struct vmm_vdisk_request *vreq,
+			     enum vmm_vdisk_request_type type,
+			     u64 lba, void *data, u32 data_len)
+{
+	int rc;
+	irq_flags_t flags;
+
+	if (!vdisk || !vreq || !data) {
+		return VMM_EINVALID;
+	}
+	if (vreq->vdisk != vdisk) {
+		return VMM_EINVALID;
+	}
+	if (data_len < vdisk->block_size) {
+		return VMM_EINVALID;
+	}
+	if ((type < VMM_VDISK_REQUEST_READ) ||
+	    (VMM_VDISK_REQUEST_WRITE < type)) {
+		return VMM_EINVALID;
+	}
+
+	vmm_spin_lock_irqsave_lite(&vdisk->blk_lock, flags);
+	if (vdisk->blk) {
+		vreq->vdisk = vdisk;
+		switch (type) {
+		case VMM_VDISK_REQUEST_READ:
+			vreq->r.type = VMM_REQUEST_READ;
+			break;
+		case VMM_VDISK_REQUEST_WRITE:
+			vreq->r.type = VMM_REQUEST_WRITE;
+			break;
+		default:
+			vreq->r.type = VMM_REQUEST_UNKNOWN;
+			break;
+		};
+		vreq->r.lba = lba * vdisk->blk_factor;
+		vreq->r.bcnt =
+			udiv32(data_len, vdisk->block_size) * vdisk->blk_factor;
+		vreq->r.data = data;
+		vreq->r.completed = vdisk_req_completed;
+		vreq->r.failed = vdisk_req_failed;
+		vreq->r.priv = NULL;
+		rc = vmm_blockdev_submit_request(vdisk->blk, &vreq->r);
+	} else {
+		rc = VMM_ENODEV;
+	}
+	vmm_spin_unlock_irqrestore_lite(&vdisk->blk_lock, flags);
+
+	DPRINTF("%s: vdisk=%s lba=0x%llx bcnt=%d rc=%d\n",
+		__func__, vdisk->name, (u64)vreq->r.lba, vreq->r.bcnt, rc);
+
+	return rc;
+}
+VMM_EXPORT_SYMBOL(vmm_vdisk_submit_request);
+
+int vmm_vdisk_abort_request(struct vmm_vdisk *vdisk,
+			    struct vmm_vdisk_request *vreq)
+{
+	int rc;
+	irq_flags_t flags;
+
+	if (!vdisk || !vreq) {
+		return VMM_EINVALID;
+	}
+	if (vreq->vdisk != vdisk) {
+		return VMM_EINVALID;
+	}
+
+	vmm_spin_lock_irqsave_lite(&vdisk->blk_lock, flags);
+	if (vdisk->blk) {
+		rc = vmm_blockdev_abort_request(&vreq->r);
+	} else {
+		rc = VMM_ENODEV;
+	}
+	vmm_spin_unlock_irqrestore_lite(&vdisk->blk_lock, flags);
+
+	DPRINTF("%s: vdisk=%s lba=0x%llx bcnt=%d rc=%d\n",
+		__func__, vdisk->name, (u64)vreq->r.lba, vreq->r.bcnt, rc);
+
+	return rc;
+}
+VMM_EXPORT_SYMBOL(vmm_vdisk_abort_request);
+
+int vmm_vdisk_flush_cache(struct vmm_vdisk *vdisk)
+{
+	int rc;
+	irq_flags_t flags;
+
+	if (!vdisk) {
+		return VMM_EINVALID;
+	}
+
+	vmm_spin_lock_irqsave_lite(&vdisk->blk_lock, flags);
+	if (vdisk->blk) {
+		rc = vmm_blockdev_flush_cache(vdisk->blk);
+	} else {
+		rc = VMM_ENODEV;
+	}
+	vmm_spin_unlock_irqrestore_lite(&vdisk->blk_lock, flags);
+
+	DPRINTF("%s: vdisk=%s rc=%d\n",
+		__func__, vdisk->name, rc);
+
+	return rc;
+}
+VMM_EXPORT_SYMBOL(vmm_vdisk_flush_cache);
+
+u64 vmm_vdisk_capacity(struct vmm_vdisk *vdisk)
+{
+	u64 ret = 0;
+	irq_flags_t flags;
+
+	if (!vdisk) {
+		return 0;
+	}
+
+	vmm_spin_lock_irqsave_lite(&vdisk->blk_lock, flags);
+	if (vdisk->blk) {
+		ret = udiv64(vdisk->blk->num_blocks, vdisk->blk_factor);
+	} else {
+		ret = 0;
+	}
+	ret = (vdisk->blk) ? vdisk->blk->num_blocks : 0;
+	vmm_spin_unlock_irqrestore_lite(&vdisk->blk_lock, flags);
+
+	return ret;
+}
+VMM_EXPORT_SYMBOL(vmm_vdisk_capacity);
+
+u32 vmm_vdisk_block_size(struct vmm_vdisk *vdisk)
+{
+	return vdisk->block_size;
+}
+VMM_EXPORT_SYMBOL(vmm_vdisk_block_size);
+
+int vmm_vdisk_current_block_device(struct vmm_vdisk *vdisk,
+				   char *name, u32 name_len)
+{
+	int rc;
+	irq_flags_t flags;
+
+	if (!vdisk || !name || !name_len) {
+		return VMM_EINVALID;
+	}
+
+	vmm_spin_lock_irqsave_lite(&vdisk->blk_lock, flags);
+	if (vdisk->blk) {
+		strncpy(name, vdisk->blk->name, name_len);
+		rc = VMM_OK;
+	} else {
+		rc = VMM_ENODEV;
+	}
+	vmm_spin_unlock_irqrestore_lite(&vdisk->blk_lock, flags);
+
+	return rc;
+}
+VMM_EXPORT_SYMBOL(vmm_vdisk_current_block_device);
+
+struct vdisk_attach_priv {
+	struct vmm_vdisk *vdisk;
+	const char *bdev_name;
+};
+
+static int vdisk_attach_iter(struct vmm_blockdev *dev, void *data)
+{
+	irq_flags_t flags;
+	struct vdisk_attach_priv *ap = data;
+	const char *bdev_name = ap->bdev_name;
+	struct vmm_vdisk *vdisk = ap->vdisk;
+
+	if (strncmp(dev->name, bdev_name, sizeof(dev->name))==0) {
+		vmm_spin_lock_irqsave_lite(&vdisk->blk_lock, flags);
+		if (!vdisk->blk &&
+		    (dev->block_size <= vdisk->block_size) &&
+		    !umod32(vdisk->block_size, dev->block_size)) {
+			vdisk->blk = dev;
+			vdisk->blk_factor = udiv32(vdisk->block_size,
+					           vdisk->blk->block_size);
+		}
+		vmm_spin_unlock_irqrestore_lite(&vdisk->blk_lock, flags);
+	}
+
+	return VMM_OK;
+}
+
+void vmm_vdisk_attach_block_device(struct vmm_vdisk *vdisk,
+				   const char *bdev_name)
+{
+	struct vdisk_attach_priv ap;
+
+	if (!vdisk || !bdev_name) {
+		return;
+	}
+
+	ap.vdisk = vdisk;
+	ap.bdev_name = bdev_name;
+	vmm_blockdev_iterate(NULL, &ap, vdisk_attach_iter);
+}
+VMM_EXPORT_SYMBOL(vmm_vdisk_attach_block_device);
+
+void vmm_vdisk_detach_block_device(struct vmm_vdisk *vdisk)
+{
+	irq_flags_t flags;
+
+	if (!vdisk) {
+		return;
+	}
+
+	vmm_spin_lock_irqsave_lite(&vdisk->blk_lock, flags);
+	if (vdisk->blk) {
+		vmm_blockdev_flush_cache(vdisk->blk);
+	}
+	vdisk->blk = NULL;
+	vdisk->blk_factor = 1;
+	vmm_spin_unlock_irqrestore_lite(&vdisk->blk_lock, flags);
+}
+VMM_EXPORT_SYMBOL(vmm_vdisk_detach_block_device);
+
+struct vmm_vdisk *vmm_vdisk_create(const char *name, u32 block_size,
+	void (*completed)(struct vmm_vdisk *, struct vmm_vdisk_request *),
+	void (*failed)(struct vmm_vdisk *, struct vmm_vdisk_request *),
+	const char *bdev_name,
+	void *priv)
+{
+	bool found;
+	struct vmm_vdisk *vdisk;
+	struct vmm_vdisk_event event;
+
+	if (!name || !block_size || !completed || !failed) {
+		return NULL;
+	}
+
+	vdisk = NULL;
+	found = FALSE;
+
+	vmm_mutex_lock(&vdctrl.vdisk_list_lock);
+
+	list_for_each_entry(vdisk, &vdctrl.vdisk_list, head) {
+		if (strcmp(name, vdisk->name) == 0) {
+			found = TRUE;
+			break;
+		}
+	}
+
+	if (found) {
+		vmm_mutex_unlock(&vdctrl.vdisk_list_lock);
+		return NULL;
+	}
+
+	vdisk = vmm_zalloc(sizeof(struct vmm_vdisk));
+	if (!vdisk) {
+		vmm_mutex_unlock(&vdctrl.vdisk_list_lock);
+		return NULL;
+	}
+
+	INIT_LIST_HEAD(&vdisk->head);
+	if (strlcpy(vdisk->name, name, sizeof(vdisk->name)) >=
+	    sizeof(vdisk->name)) {
+		vmm_free(vdisk);
+		vmm_mutex_unlock(&vdctrl.vdisk_list_lock);
+		return NULL;
+	}
+	vdisk->block_size = block_size;
+	vdisk->completed = completed;
+	vdisk->failed = failed;
+	INIT_SPIN_LOCK(&vdisk->blk_lock);
+	vdisk->blk = NULL;
+	vdisk->blk_factor = 1;
+	vdisk->priv = priv;
+
+	list_add_tail(&vdisk->head, &vdctrl.vdisk_list);
+
+	vmm_mutex_unlock(&vdctrl.vdisk_list_lock);
+
+	/* Broadcast create event */
+	event.vdisk = vdisk;
+	event.data = NULL;
+	vmm_blocking_notifier_call(&vdctrl.notifier_chain,
+				   VMM_VDISK_EVENT_CREATE,
+				   &event);
+
+	/* Attach block device */
+	vmm_vdisk_attach_block_device(vdisk, bdev_name);
+
+	return vdisk;
+}
+VMM_EXPORT_SYMBOL(vmm_vdisk_create);
+
+int vmm_vdisk_destroy(struct vmm_vdisk *vdisk)
+{
+	bool found;
+	struct vmm_vdisk *vd;
+	struct vmm_vdisk_event event;
+
+	if (!vdisk) {
+		return VMM_EFAIL;
+	}
+
+	/* Detach current block device */
+	vmm_vdisk_detach_block_device(vdisk);
+
+	/* Broadcast destroy event */
+	event.vdisk = vdisk;
+	event.data = NULL;
+	vmm_blocking_notifier_call(&vdctrl.notifier_chain,
+				   VMM_VDISK_EVENT_DESTROY,
+				   &event);
+
+	vmm_mutex_lock(&vdctrl.vdisk_list_lock);
+
+	if (list_empty(&vdctrl.vdisk_list)) {
+		vmm_mutex_unlock(&vdctrl.vdisk_list_lock);
+		return VMM_EFAIL;
+	}
+
+	vd = NULL;
+	found = FALSE;
+
+	list_for_each_entry(vd, &vdctrl.vdisk_list, head) {
+		if (strcmp(vd->name, vdisk->name) == 0) {
+			found = TRUE;
+			break;
+		}
+	}
+
+	if (!found) {
+		vmm_mutex_unlock(&vdctrl.vdisk_list_lock);
+		return VMM_ENOTAVAIL;
+	}
+
+	list_del(&vd->head);
+
+	vmm_free(vd);
+
+	vmm_mutex_unlock(&vdctrl.vdisk_list_lock);
+
+	return VMM_OK;
+}
+VMM_EXPORT_SYMBOL(vmm_vdisk_destroy);
+
+struct vmm_vdisk *vmm_vdisk_find(const char *name)
+{
+	bool found;
+	struct vmm_vdisk *vdisk;
+
+	if (!name) {
+		return NULL;
+	}
+
+	found = FALSE;
+	vdisk = NULL;
+
+	vmm_mutex_lock(&vdctrl.vdisk_list_lock);
+
+	list_for_each_entry(vdisk, &vdctrl.vdisk_list, head) {
+		if (strcmp(vdisk->name, name) == 0) {
+			found = TRUE;
+			break;
+		}
+	}
+
+	vmm_mutex_unlock(&vdctrl.vdisk_list_lock);
+
+	if (!found) {
+		return NULL;
+	}
+
+	return vdisk;
+}
+VMM_EXPORT_SYMBOL(vmm_vdisk_find);
+
+int vmm_vdisk_iterate(struct vmm_vdisk *start, void *data,
+		      int (*fn)(struct vmm_vdisk *vdisk, void *data))
+{
+	int rc = VMM_OK;
+	bool start_found = (start) ? FALSE : TRUE;
+	struct vmm_vdisk *vd = NULL;
+
+	if (!fn) {
+		return VMM_EINVALID;
+	}
+
+	vmm_mutex_lock(&vdctrl.vdisk_list_lock);
+
+	list_for_each_entry(vd, &vdctrl.vdisk_list, head) {
+		if (!start_found) {
+			if (start && start == vd) {
+				start_found = TRUE;
+			} else {
+				continue;
+			}
+		}
+
+		rc = fn(vd, data);
+		if (rc) {
+			break;
+		}
+	}
+
+	vmm_mutex_unlock(&vdctrl.vdisk_list_lock);
+
+	return rc;
+}
+VMM_EXPORT_SYMBOL(vmm_vdisk_iterate);
+
+u32 vmm_vdisk_count(void)
+{
+	u32 retval = 0;
+	struct vmm_vdisk *vdisk;
+
+	vmm_mutex_lock(&vdctrl.vdisk_list_lock);
+
+	list_for_each_entry(vdisk, &vdctrl.vdisk_list, head) {
+		retval++;
+	}
+
+	vmm_mutex_unlock(&vdctrl.vdisk_list_lock);
+
+	return retval;
+}
+VMM_EXPORT_SYMBOL(vmm_vdisk_count);
+
+static int vdisk_blk_notification(struct vmm_notifier_block *nb,
+				  unsigned long evt, void *data)
+{
+	irq_flags_t flags;
+	struct vmm_vdisk *vdisk;
+	struct vmm_blockdev_event *e = data;
+
+	if (evt != VMM_BLOCKDEV_EVENT_UNREGISTER) {
+		/* We are only interested in unregister events so,
+		 * don't care about this event.
+		 */
+		return NOTIFY_DONE;
+	}
+
+	/* Lock virtual disk list */
+	vmm_mutex_lock(&vdctrl.vdisk_list_lock);
+
+	/* Find virtual disk using block device */
+	list_for_each_entry(vdisk, &vdctrl.vdisk_list, head) {
+		vmm_spin_lock_irqsave_lite(&vdisk->blk_lock, flags);
+		if (vdisk->blk == e->bdev) {
+			vdisk->blk = NULL;
+			vdisk->blk_factor = 1;
+		}
+		vmm_spin_unlock_irqrestore_lite(&vdisk->blk_lock, flags);
+	}
+
+	/* Unlock virtual disk list */
+	vmm_mutex_unlock(&vdctrl.vdisk_list_lock);
+
+	return NOTIFY_OK;
+}
+
+static int __init vmm_vdisk_init(void)
+{
+	memset(&vdctrl, 0, sizeof(vdctrl));
+
+	INIT_MUTEX(&vdctrl.vdisk_list_lock);
+	INIT_LIST_HEAD(&vdctrl.vdisk_list);
+	BLOCKING_INIT_NOTIFIER_CHAIN(&vdctrl.notifier_chain);
+
+	vdctrl.blk_client.notifier_call = &vdisk_blk_notification;
+	vdctrl.blk_client.priority = 0;
+	vmm_blockdev_register_client(&vdctrl.blk_client);
+
+	return VMM_OK;
+}
+
+static void __exit vmm_vdisk_exit(void)
+{
+	vmm_blockdev_unregister_client(&vdctrl.blk_client);
+}
+
+VMM_DECLARE_MODULE(MODULE_DESC,
+			MODULE_AUTHOR,
+			MODULE_LICENSE,
+			MODULE_IPRIORITY,
+			MODULE_INIT,
+			MODULE_EXIT);
