6f6434f43e75dc46ae735c1bbae6f548f784bc5d
Anup Patel <anup@brainfault.org>
2015-05-18 09:48:13 +0530

    [DRIVERS] imx-uart: Make use of new serial port framework
    This patch simplifies imx-uart driver by making use of new serial
    port framework.
    
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- drivers/serial/imx-uart.c
+++ drivers/serial/imx-uart.c
@@ -36,32 +36,28 @@
  *  Copyright (C) 2009 emlix GmbH
  *  Author: Fabian Godehardt (added IrDA support for iMX)
  *
- * The original source is licensed under GPL. 
+ * The original source is licensed under GPL.
  */
 
 #include <vmm_error.h>
 #include <vmm_heap.h>
 #include <vmm_host_io.h>
 #include <vmm_host_irq.h>
-#include <vmm_completion.h>
 #include <vmm_modules.h>
 #include <vmm_devtree.h>
 #include <vmm_devdrv.h>
-#include <vmm_chardev.h>
 #include <libs/stringlib.h>
 #include <libs/mathlib.h>
-#include <drv/imx-uart.h>
+#include <drv/serial.h>
 #include <drv/clk.h>
-#include <linux/err.h>
+#include <drv/imx-uart.h>
 
 #define MODULE_DESC                     "IMX Serial Driver"
 #define MODULE_AUTHOR                   "Jean-Christophe Dubois"
 #define MODULE_LICENSE                  "GPL"
-#define MODULE_IPRIORITY                0
+#define MODULE_IPRIORITY                (SERIAL_IPRIORITY+1)
 #define MODULE_INIT                     imx_driver_init
 #define MODULE_EXIT                     imx_driver_exit
-
-//#define UART_IMX_USE_TXINTR
 
 /*
  * This determines how often we check the modem status signals
@@ -72,11 +68,7 @@
 #define MCTRL_TIMEOUT	(250*HZ/1000)
 
 struct imx_port {
-	struct vmm_chardev cd;
-	struct vmm_completion read_possible;
-#if defined(UART_IMX_USE_TXINTR)
-	struct vmm_completion write_possible;
-#endif
+	struct serial *p;
 	virtual_addr_t base;
 	u32 baudrate;
 	u32 input_clock;
@@ -136,9 +128,8 @@ void imx_lowlevel_init(virtual_addr_t base, u32 baudrate, u32 input_clock)
 	temp &= ~UCR1_UARTEN;
 	vmm_writel(temp, (void *)base + UCR1);
 
-
-	temp = vmm_readl((void *)(base + UCR2));
 	/* disable all UCR2 related interrupts */
+	temp = vmm_readl((void *)(base + UCR2));
 	temp &= ~(UCR2_ATEN | UCR2_ESCI | UCR2_RTSEN);
 	/* Set to 8N1 */
 	temp = (temp & ~(UCR2_PREN | UCR2_STPB)) | UCR2_WS;
@@ -175,8 +166,8 @@ void imx_lowlevel_init(virtual_addr_t base, u32 baudrate, u32 input_clock)
 	temp = udiv32(input_clock, 16 * 100) - 1;
 	vmm_writel(temp, (void *)(base + UBMR));
 
-	/* enable the UART and the receive interrupt */
-	temp = UCR1_RRDYEN | UCR1_UARTEN;
+	/* enable the UART */
+	temp = UCR1_UARTEN;
 	vmm_writel(temp, (void *)(base + UCR1));
 
 	/* Enable FIFOs */
@@ -185,20 +176,18 @@ void imx_lowlevel_init(virtual_addr_t base, u32 baudrate, u32 input_clock)
 		   (void *)(base + UCR2));
 }
 
-#if defined(UART_IMX_USE_TXINTR)
 static void imx_txint(struct imx_port *port)
 {
 	port->mask &= ~UCR1_TRDYEN;
 	vmm_writel(port->mask, (void *)port->base + UCR1);
-	vmm_completion_complete(&port->write_possible);
 }
-#endif
 
 static void imx_rxint(struct imx_port *port)
 {
-	port->mask &= ~UCR1_RRDYEN;
-	vmm_writel(port->mask, (void *)port->base + UCR1);
-	vmm_completion_complete(&port->read_possible);
+	while (imx_lowlevel_can_getc(port->base)) {
+		u8 ch = imx_lowlevel_getc(port->base);
+		serial_rx(port->p, &ch, 1);
+	}
 }
 
 static void imx_rtsint(struct imx_port *port)
@@ -215,19 +204,22 @@ static vmm_irq_return_t imx_irq_handler(int irq, void *dev_id)
 	if (sts & USR1_RRDY) {
 		imx_rxint(port);
 	}
-#if defined(UART_IMX_USE_TXINTR)
+
 	if ((sts & USR1_TRDY) && (port->mask & UCR1_TXMPTYEN)) {
 		imx_txint(port);
 	}
-#endif
 
 	if (sts & USR1_RTSD) {
 		imx_rtsint(port);
 	}
 
-	sts &=
-	    USR1_PARITYERR | USR1_RTSD | USR1_ESCF | USR1_FRAMERR | USR1_TIMEOUT
-	    | USR1_AIRINT | USR1_AWAKE;
+	sts &= USR1_PARITYERR |
+		USR1_RTSD |
+		USR1_ESCF |
+		USR1_FRAMERR |
+		USR1_TIMEOUT |
+		USR1_AIRINT |
+		USR1_AWAKE;
 	if (sts) {
 		vmm_writel(sts, (void *)port->base + USR1);
 	}
@@ -235,98 +227,17 @@ static vmm_irq_return_t imx_irq_handler(int irq, void *dev_id)
 	return VMM_IRQ_HANDLED;
 }
 
-static u8 imx_getc_sleepable(struct imx_port *port)
-{
-	/* Wait until there is data in the FIFO */
-	while (!imx_lowlevel_can_getc(port->base)) {
-		/* Enable the RX interrupt */
-		port->mask |= UCR1_RRDYEN;
-		vmm_writel(port->mask, (void *)port->base + UCR1);
-		/* Wait for completion */
-		vmm_completion_wait(&port->read_possible);
-	}
-
-	/* Read data to destination */
-	return imx_lowlevel_getc(port->base);
-}
-
-static u32 imx_read(struct vmm_chardev *cdev,
-		    u8 *dest, size_t len, off_t __unused *off, bool sleep)
+static u32 imx_tx(struct serial *p, u8 *src, size_t len)
 {
 	u32 i;
-	struct imx_port *port;
+	struct imx_port *port = serial_tx_priv(p);
 
-	if (!(cdev && dest && cdev->priv)) {
-		return 0;
-	}
-
-	port = cdev->priv;
-
-	if (sleep) {
-		for (i = 0; i < len; i++) {
-			dest[i] = imx_getc_sleepable(port);
-		}
-	} else {
-		for (i = 0; i < len; i++) {
-			if (!imx_lowlevel_can_getc(port->base)) {
-				break;
-			}
-			dest[i] = imx_lowlevel_getc(port->base);
-		}
-	}
-
-	return i;
-}
-
-#if defined(UART_IMX_USE_TXINTR)
-static void imx_putc_sleepable(struct imx_port *port, u8 ch)
-{
-	/* Wait until there is space in the FIFO */
-	if (!imx_lowlevel_can_putc(port->base)) {
-		/* Enable the RX interrupt */
-		port->mask |= UCR1_TRDYEN;
-		vmm_writel(port->mask, (void *)port->base + UCR1);
-		/* Wait for completion */
-		vmm_completion_wait(&port->write_possible);
-	}
-
-	/* Write data to FIFO */
-	imx_lowlevel_putc(port->base, ch);
-}
-#endif
-
-static u32 imx_write(struct vmm_chardev *cdev,
-		     u8 *src, size_t len, off_t __unused *off, bool sleep)
-{
-	u32 i;
-	struct imx_port *port;
-
-	if (!(cdev && src && cdev->priv)) {
-		return 0;
-	}
-
-	port = cdev->priv;
-#if defined(UART_IMX_USE_TXINTR)
-	if (sleep) {
-		for (i = 0; i < len; i++) {
-			imx_putc_sleepable(port, src[i]);
-		}
-	} else {
-		for (i = 0; i < len; i++) {
-			if (!imx_lowlevel_can_putc(port->base)) {
-				break;
-			}
-			imx_lowlevel_putc(port->base, src[i]);
-		}
-	}
-#else
 	for (i = 0; i < len; i++) {
 		if (!imx_lowlevel_can_putc(port->base)) {
 			break;
 		}
 		imx_lowlevel_putc(port->base, src[i]);
 	}
-#endif
 
 	return i;
 }
@@ -346,36 +257,11 @@ static int imx_driver_probe(struct vmm_device *dev,
 		goto free_nothing;
 	}
 
-	if (strlcpy(port->cd.name, dev->name, sizeof(port->cd.name)) >=
-	    sizeof(port->cd.name)) {
-		rc = VMM_EOVERFLOW;
-		goto free_port;
-	}
-
-	port->cd.dev.parent = dev;
-	port->cd.ioctl = NULL;
-	port->cd.read = imx_read;
-	port->cd.write = imx_write;
-	port->cd.priv = port;
-
-	INIT_COMPLETION(&port->read_possible);
-#if defined(UART_IMX_USE_TXINTR)
-	INIT_COMPLETION(&port->write_possible);
-#endif
-
 	rc = vmm_devtree_request_regmap(dev->node, &port->base, 0,
 					"iMX UART");
 	if (rc) {
 		goto free_port;
 	}
-
-	port->mask = UCR1_RRDYEN | UCR1_UARTEN;
-
-#if defined(UART_IMX_USE_TXINTR)
-	port->mask |= UCR1_TRDYEN;
-#endif
-
-	vmm_writel(port->mask, (void *)port->base + UCR1);
 
 	if (vmm_devtree_read_u32(dev->node, "baudrate", &port->baudrate)) {
 		port->baudrate = 115200;
@@ -386,73 +272,77 @@ static int imx_driver_probe(struct vmm_device *dev,
 		goto free_reg;
 	}
 
+	/* Setup clocks */
 	clk_ipg = of_clk_get(dev->node, 0);
-	if (IS_ERR(clk_ipg)) {
-		rc = PTR_ERR(clk_ipg);
-		goto free_reg;
-	}
-
 	clk_uart = of_clk_get(dev->node, 1);
-	if (IS_ERR(clk_uart)) {
-		rc = PTR_ERR(clk_uart);
-		goto free_reg;
+	if (!VMM_IS_ERR_OR_NULL(clk_ipg)) {
+		rc = clk_prepare_enable(clk_ipg);
+		if (rc) {
+			goto free_reg;
+		}
+	}
+	if (!VMM_IS_ERR_OR_NULL(clk_uart)) {
+		rc = clk_prepare_enable(clk_uart);
+		if (rc) {
+			goto clk_disable_unprepare_ipg;
+		}
+		old_rate = clk_get_rate(clk_uart);
+		if (clk_set_rate(clk_uart, port->input_clock)) {
+			vmm_printf("Could not set %s clock rate to %u Hz, "
+				   "actual rate: %u Hz\n", __clk_get_name(clk_uart),
+				   port->input_clock, clk_get_rate(clk_uart));
+			rc = VMM_ERANGE;
+			goto clk_disable_unprepare_uart;
+		}
 	}
 
-	rc = clk_prepare_enable(clk_ipg);
-	if (rc) {
-		rc = ENODEV;
-		goto free_reg;
-	}
-
-	rc = clk_prepare_enable(clk_uart);
-	if (rc) {
-		rc = ENODEV;
-		goto clk_disable_unprepare_ipg;
-	}
-
-	old_rate = clk_get_rate(clk_uart);
-	if (clk_set_rate(clk_uart, port->input_clock)) {
-		vmm_printf("Could not set %s clock rate to %u Hz, "
-			   "actual rate: %u Hz\n", __clk_get_name(clk_uart),
-			   port->input_clock, clk_get_rate(clk_uart));
-		rc = ERANGE;
-		goto clk_disable_unprepare_uart;
-	}
-
+	/* Register interrupt handler */
 	rc = vmm_devtree_irq_get(dev->node, &port->irq, 0);
 	if (rc) {
 		goto clk_old_rate;
 	}
-
 	if ((rc = vmm_host_irq_register(port->irq, dev->name,
 					imx_irq_handler, port))) {
-		goto free_irq;
+		goto clk_old_rate;
 	}
 
 	/* Call low-level init function */
 	imx_lowlevel_init(port->base, port->baudrate, port->input_clock);
 
-	port->mask = vmm_readl((void *)port->base + UCR1);
-
-	rc = vmm_chardev_register(&port->cd);
-	if (rc) {
-		/* goto unprepare_clk_ipg; */
+	/* Create Serial Port */
+	port->p = serial_create(dev, 256, imx_tx, port);
+	if (VMM_IS_ERR_OR_NULL(port->p)) {
+		rc = VMM_PTR_ERR(port->p);
 		goto free_irq;
 	}
 
+	/* Save port pointer */
 	dev->priv = port;
+
+	/* Unmask Rx, Mask Tx, and Enable UART */
+	port->mask = vmm_readl((void *)port->base + UCR1);
+	port->mask |= (UCR1_RRDYEN | UCR1_UARTEN);
+	port->mask &= ~(UCR1_TRDYEN);
+	vmm_writel(port->mask, (void *)port->base + UCR1);
 
 	return rc;
 
 free_irq:
 	vmm_host_irq_unregister(port->irq, port);
 clk_old_rate:
-	if (old_rate)
-		clk_set_rate(clk_uart, old_rate);
+	if (!VMM_IS_ERR_OR_NULL(clk_uart)) {
+		if (old_rate) {
+			clk_set_rate(clk_uart, old_rate);
+		}
+	}
 clk_disable_unprepare_uart:
-	clk_disable_unprepare(clk_uart);
+	if (!VMM_IS_ERR_OR_NULL(clk_uart)) {
+		clk_disable_unprepare(clk_uart);
+	}
 clk_disable_unprepare_ipg:
-	clk_disable_unprepare(clk_ipg);
+	if (!VMM_IS_ERR_OR_NULL(clk_ipg)) {
+		clk_disable_unprepare(clk_ipg);
+	}
 free_reg:
 	vmm_devtree_regunmap_release(dev->node, port->base, 0);
 free_port:
@@ -466,12 +356,15 @@ static int imx_driver_remove(struct vmm_device *dev)
 	int rc = VMM_OK;
 	struct imx_port *port = dev->priv;
 
-	if (port) {
-		rc = vmm_chardev_unregister(&port->cd);
-		vmm_devtree_regunmap_release(dev->node, port->base, 0);
-		vmm_free(port);
-		dev->priv = NULL;
+	if (!port) {
+		return VMM_OK;
 	}
+
+	serial_destroy(port->p);
+	vmm_host_irq_unregister(port->irq, port);
+	vmm_devtree_regunmap_release(dev->node, port->base, 0);
+	vmm_free(port);
+	dev->priv = NULL;
 
 	return rc;
 }
