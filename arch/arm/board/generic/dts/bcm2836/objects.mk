f1f75c68664f1bbb05052a792a67788b19e9baaf
Anup Patel <anup@brainfault.org>
2015-07-17 18:09:24 +0530

    [ARM] generic: Initial un-tested DTS files for Raspberry Pi2 (BCM2836 SOC)
    This patch adds un-tested DTS files for Raspberry Pi2.
    
    Signed-off-by: Anup Patel <anup@brainfault.org>

---
+++ arch/arm/board/generic/dts/bcm2836/objects.mk
@@ -0,0 +1,29 @@
+#/**
+# Copyright (c) 2015 Anup Patel.
+# All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+# @file objects.mk
+# @author Anup Patel (anup@brainfault.org)
+# @brief list of BCM2836 DTBs.
+# */
+
+board-dtbs-$(CONFIG_ARMV7A_VE)+=dts/bcm2836/one_guest_pb-a8.dtb
+board-dtbs-$(CONFIG_ARMV7A_VE)+=dts/bcm2836/one_guest_vexpress-a9.dtb
+board-dtbs-$(CONFIG_ARMV7A_VE)+=dts/bcm2836/one_guest_vexpress-a15.dtb
+board-dtbs-$(CONFIG_ARMV7A_VE)+=dts/bcm2836/one_guest_virt-v7.dtb
+board-dtbs-$(CONFIG_ARMV7A_VE)+=dts/bcm2836/two_guest_pb-a8.dtb
+board-dtbs-$(CONFIG_ARMV7A_VE)+=dts/bcm2836/two_guest_vexpress-a15.dtb
