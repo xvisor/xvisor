937075b033c2df526804fbc984ef121db3a72da6
Himanshu Chauhan <hchauhan@xvisor-x86.org>
2015-09-30 22:05:05 +0530

    [CORE] Print boot command only in verbose mode.
    Signed-off-by: Himanshu Chauhan <hchauhan@xvisor-x86.org>

--- core/vmm_main.c
+++ core/vmm_main.c
@@ -143,8 +143,10 @@ static void system_postinit_work(struct vmm_work *work)
 			c = vmm_devtree_attrlen(node,
 						VMM_DEVTREE_BOOTCMD_ATTR_NAME);
 			while (c) {
+#if defined(CONFIG_VERBOSE_MODE)
 				/* Print boot command */
 				vmm_printf("bootcmd: %s\n", str);
+#endif
 				/* Execute boot command */
 				strlcpy(bcmd, str, sizeof(bcmd));
 				cdev = vmm_stdio_device();
