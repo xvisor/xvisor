83e818de4bb5e7ca3d4484a8a0460d45c7fc3e12
Anup Patel <anup@brainfault.org>
2015-10-07 22:59:01 +0530

    [DRIVERS] irq-sun4i: Optimize driver by pre-computing registers addresses
    This patch does minor optimization to Allwinner Sun4i irqchip driver by
    having precomputed register addresses instead on doing "base + <reg offset>".
    
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- drivers/irqchip/irq-sun4i.c
+++ drivers/irqchip/irq-sun4i.c
@@ -48,108 +48,118 @@
 #include <vmm_host_irq.h>
 #include <vmm_host_aspace.h>
 
-static virtual_addr_t aw_vic_base;
-
-/* Register read/write macros */
-#define __p(off)		((void *)(aw_vic_base + (off)))
-#define readl(off)		vmm_readl(__p(off))
-#define writel(val, off)	vmm_writel((val), __p(off))
-
 /* Max number of irqs */
-#define AW_NR_IRQS			  96
+#define AW_NR_BANKS			3
+#define AW_IRQS_PER_BANK		32
+#define AW_NR_IRQS			(AW_NR_BANKS * AW_IRQS_PER_BANK)
 
 /* Interrupt controller registers */
-#define AW_INT_VECTOR_REG                 (0x00)
-#define AW_INT_BASE_ADR_REG               (0x04)
-#define AW_INT_PROTECTION_REG             (0x08)
-#define AW_INT_NMI_CTRL_REG               (0x0c)
-#define AW_INT_IRQ_PENDING_REG0           (0x10)
-#define AW_INT_IRQ_PENDING_REG1           (0x14)
-#define AW_INT_IRQ_PENDING_REG2           (0x18)
-#define AW_INT_FIQ_PENDING_REG0           (0x20)
-#define AW_INT_FIQ_PENDING_REG1           (0x24)
-#define AW_INT_FIQ_PENDING_REG2           (0x28)
-#define AW_INT_SELECT_REG0                (0x30)
-#define AW_INT_SELECT_REG1                (0x34)
-#define AW_INT_SELECT_REG2                (0x38)
-#define AW_INT_ENABLE_REG0                (0x40)
-#define AW_INT_ENABLE_REG1                (0x44)
-#define AW_INT_ENABLE_REG2                (0x48)
-#define AW_INT_MASK_REG0                  (0x50)
-#define AW_INT_MASK_REG1                  (0x54)
-#define AW_INT_MASK_REG2                  (0x58)
-#define AW_INT_RESP_REG0                  (0x60)
-#define AW_INT_RESP_REG1                  (0x64)
-#define AW_INT_RESP_REG2                  (0x68)
-#define AW_INT_FASTFORCE_REG0             (0x70)
-#define AW_INT_FASTFORCE_REG1             (0x74)
-#define AW_INT_FASTFORCE_REG2             (0x78)
-#define AW_INT_SRCPRIO_REG0               (0x80)
-#define AW_INT_SRCPRIO_REG1               (0x84)
-#define AW_INT_SRCPRIO_REG2               (0x88)
-#define AW_INT_SRCPRIO_REG3               (0x8c)
-#define AW_INT_SRCPRIO_REG4               (0x90)
+#define AW_INT_VECTOR_REG		(0x00)
+#define AW_INT_BASE_ADR_REG		(0x04)
+#define AW_INT_PROTECTION_REG		(0x08)
+#define AW_INT_NMI_CTRL_REG		(0x0c)
+#define AW_INT_IRQ_PENDING_REG0		(0x10)
+#define AW_INT_IRQ_PENDING_REG1		(0x14)
+#define AW_INT_IRQ_PENDING_REG2		(0x18)
+#define AW_INT_FIQ_PENDING_REG0		(0x20)
+#define AW_INT_FIQ_PENDING_REG1		(0x24)
+#define AW_INT_FIQ_PENDING_REG2		(0x28)
+#define AW_INT_SELECT_REG0		(0x30)
+#define AW_INT_SELECT_REG1		(0x34)
+#define AW_INT_SELECT_REG2		(0x38)
+#define AW_INT_ENABLE_REG0		(0x40)
+#define AW_INT_ENABLE_REG1		(0x44)
+#define AW_INT_ENABLE_REG2		(0x48)
+#define AW_INT_MASK_REG0		(0x50)
+#define AW_INT_MASK_REG1		(0x54)
+#define AW_INT_MASK_REG2		(0x58)
+#define AW_INT_RESP_REG0		(0x60)
+#define AW_INT_RESP_REG1		(0x64)
+#define AW_INT_RESP_REG2		(0x68)
+#define AW_INT_FASTFORCE_REG0		(0x70)
+#define AW_INT_FASTFORCE_REG1		(0x74)
+#define AW_INT_FASTFORCE_REG2		(0x78)
+#define AW_INT_SRCPRIO_REG0		(0x80)
+#define AW_INT_SRCPRIO_REG1		(0x84)
+#define AW_INT_SRCPRIO_REG2		(0x88)
+#define AW_INT_SRCPRIO_REG3		(0x8c)
+#define AW_INT_SRCPRIO_REG4		(0x90)
 
 /* Non-maskable interrupt number */
 #define AW_INT_IRQNO_ENMI                 0
 
-static void aw_irq_ack(struct vmm_host_irq *irqd)
-{
-	unsigned int irq = irqd->num;
+struct aw_vic {
+	virtual_addr_t base;
+	void *protection;
+	void *nmi_ctrl;
+	void *irq_pend0;
+	void *irq_pend1;
+	void *irq_pend2;
+	void *fiq_pend0;
+	void *fiq_pend1;
+	void *fiq_pend2;
+	void *enable0;
+	void *enable1;
+	void *enable2;
+	void *mask0;
+	void *mask1;
+	void *mask2;
+};
 
-	if (irq < 32) {
-		writel(readl(AW_INT_ENABLE_REG0) & ~(1<<irq), AW_INT_ENABLE_REG0);
-		writel(readl(AW_INT_MASK_REG0) | (1 << irq), AW_INT_MASK_REG0);
-		writel(readl(AW_INT_IRQ_PENDING_REG0) | (1<<irq), AW_INT_IRQ_PENDING_REG0);
-	} else if(irq < 64) {
-		irq -= 32;
-		writel(readl(AW_INT_ENABLE_REG1) & ~(1<<irq), AW_INT_ENABLE_REG1);
-		writel(readl(AW_INT_MASK_REG1) | (1 << irq), AW_INT_MASK_REG1);
-		writel(readl(AW_INT_IRQ_PENDING_REG1) | (1<<irq), AW_INT_IRQ_PENDING_REG1);
-	} else if(irq < 96) {
-		irq -= 64;
-		writel(readl(AW_INT_ENABLE_REG2) & ~(1<<irq), AW_INT_ENABLE_REG2);
-		writel(readl(AW_INT_MASK_REG2) | (1 << irq), AW_INT_MASK_REG2);
-		writel(readl(AW_INT_IRQ_PENDING_REG2) | (1<<irq), AW_INT_IRQ_PENDING_REG2);
+static struct aw_vic awvic;
+
+static void aw_irq_ack(struct vmm_host_irq *d)
+{
+	unsigned int mask = 1 << (d->hwirq & 0x1f);
+
+	if (d->hwirq < 32) {
+		vmm_writel(vmm_readl(awvic.enable0) & ~mask, awvic.enable0);
+		vmm_writel(vmm_readl(awvic.mask0) | mask, awvic.mask0);
+		vmm_writel(vmm_readl(awvic.irq_pend0) | mask, awvic.irq_pend0);
+	} else if (d->hwirq < 64) {
+		vmm_writel(vmm_readl(awvic.enable1) & ~mask, awvic.enable1);
+		vmm_writel(vmm_readl(awvic.mask1) | mask, awvic.mask1);
+		vmm_writel(vmm_readl(awvic.irq_pend1) | mask, awvic.irq_pend1);
+	} else if (d->hwirq < 96) {
+		vmm_writel(vmm_readl(awvic.enable2) & ~mask, awvic.enable2);
+		vmm_writel(vmm_readl(awvic.mask2) | mask, awvic.mask2);
+		vmm_writel(vmm_readl(awvic.irq_pend2) | mask, awvic.irq_pend2);
 	}
 }
 
 /* Mask an IRQ line, which means disabling the IRQ line */
-static void aw_irq_mask(struct vmm_host_irq *irqd)
+static void aw_irq_mask(struct vmm_host_irq *d)
 {
-	unsigned int irq = irqd->num;
+	unsigned int mask = 1 << (d->hwirq & 0x1f);
 
-	if (irq < 32) {
-		writel(readl(AW_INT_ENABLE_REG0) & ~(1<<irq), AW_INT_ENABLE_REG0);
-		writel(readl(AW_INT_MASK_REG0) | (1 << irq), AW_INT_MASK_REG0);
-	} else if(irq < 64) {
-		irq -= 32;
-		writel(readl(AW_INT_ENABLE_REG1) & ~(1<<irq), AW_INT_ENABLE_REG1);
-		writel(readl(AW_INT_MASK_REG1) | (1 << irq), AW_INT_MASK_REG1);
-	} else if(irq < 96) {
-		irq -= 64;
-		writel(readl(AW_INT_ENABLE_REG2) & ~(1<<irq), AW_INT_ENABLE_REG2);
-		writel(readl(AW_INT_MASK_REG2) | (1 << irq), AW_INT_MASK_REG2);
+	if (d->hwirq < 32) {
+		vmm_writel(vmm_readl(awvic.enable0) & ~mask, awvic.enable0);
+		vmm_writel(vmm_readl(awvic.mask0) | mask, awvic.mask0);
+	} else if (d->hwirq < 64) {
+		vmm_writel(vmm_readl(awvic.enable1) & ~mask, awvic.enable1);
+		vmm_writel(vmm_readl(awvic.mask1) | mask, awvic.mask1);
+	} else if (d->hwirq < 96) {
+		vmm_writel(vmm_readl(awvic.enable2) & ~mask, awvic.enable2);
+		vmm_writel(vmm_readl(awvic.mask2) | mask, awvic.mask2);
 	}
 }
 
-static void aw_irq_unmask(struct vmm_host_irq *irqd)
+static void aw_irq_unmask(struct vmm_host_irq *d)
 {
-	unsigned int irq = irqd->num;
+	unsigned int mask = 1 << (d->hwirq & 0x1f);
 
-	if (irq < 32) {
-		writel(readl(AW_INT_ENABLE_REG0) | (1<<irq), AW_INT_ENABLE_REG0);
-		writel(readl(AW_INT_MASK_REG0) & ~(1 << irq), AW_INT_MASK_REG0);
-		if(irq == AW_INT_IRQNO_ENMI) /* must clear pending bit when enabled */
-			writel((1 << AW_INT_IRQNO_ENMI), AW_INT_IRQ_PENDING_REG0);
-	} else if(irq < 64) {
-		irq -= 32;
-		writel(readl(AW_INT_ENABLE_REG1) | (1<<irq), AW_INT_ENABLE_REG1);
-		writel(readl(AW_INT_MASK_REG1) & ~(1 << irq), AW_INT_MASK_REG1);
-	} else if(irq < 96) {
-		irq -= 64;
-		writel(readl(AW_INT_ENABLE_REG2) | (1<<irq), AW_INT_ENABLE_REG2);
-		writel(readl(AW_INT_MASK_REG2) & ~(1 << irq), AW_INT_MASK_REG2);
+	if (d->hwirq < 32) {
+		vmm_writel(vmm_readl(awvic.enable0) | mask, awvic.enable0);
+		vmm_writel(vmm_readl(awvic.mask0) & ~mask, awvic.mask0);
+		/* must clear pending bit when NMI is enabled */
+		if (d->hwirq == AW_INT_IRQNO_ENMI)
+			vmm_writel(mask, awvic.irq_pend0);
+	} else if (d->hwirq < 64) {
+		vmm_writel(vmm_readl(awvic.enable1) | mask, awvic.enable1);
+		vmm_writel(vmm_readl(awvic.mask1) & ~mask, awvic.mask1);
+	} else if (d->hwirq < 96) {
+		vmm_writel(vmm_readl(awvic.enable2) | mask, awvic.enable2);
+		vmm_writel(vmm_readl(awvic.mask2) & ~mask, awvic.mask2);
 	}
 }
 
@@ -166,7 +176,7 @@ static u32 aw_intc_irq_active(u32 cpu_irq_no)
 
 	/* Find pending irq */
 	for (i = 0; i < 3; i++) {
-		if ((s = readl(AW_INT_IRQ_PENDING_REG0 + (i*4)))) {
+		if ((s = vmm_readl(awvic.irq_pend0 + (i*4)))) {
 			i = i*32;
 			while (!(s & 0xF)) {
 				i += 4;
@@ -190,34 +200,55 @@ static int __cpuinit aw_intc_devtree_init(struct vmm_devtree_node *node)
 {
 	int rc;
 	u32 i = 0;
+	void *base;
 
-	rc = vmm_devtree_request_regmap(node, &aw_vic_base, 0,
+	/* Map registers */
+	rc = vmm_devtree_request_regmap(node, &awvic.base, 0,
 					"Allwinner INTC");
 	if (rc) {
 		return rc;
 	}
+	base = (void *)awvic.base;
+
+	/* Precompute register addresses */
+	awvic.protection = base + AW_INT_PROTECTION_REG;
+	awvic.nmi_ctrl = base + AW_INT_NMI_CTRL_REG;
+	awvic.irq_pend0 = base + AW_INT_IRQ_PENDING_REG0;
+	awvic.irq_pend1 = base + AW_INT_IRQ_PENDING_REG1;
+	awvic.irq_pend2 = base + AW_INT_IRQ_PENDING_REG2;
+	awvic.fiq_pend0 = base + AW_INT_FIQ_PENDING_REG0;
+	awvic.fiq_pend1 = base + AW_INT_FIQ_PENDING_REG1;
+	awvic.fiq_pend2 = base + AW_INT_FIQ_PENDING_REG2;
+	awvic.enable0 = base + AW_INT_ENABLE_REG0;
+	awvic.enable1 = base + AW_INT_ENABLE_REG1;
+	awvic.enable2 = base + AW_INT_ENABLE_REG2;
+	awvic.mask0 = base + AW_INT_MASK_REG0;
+	awvic.mask1 = base + AW_INT_MASK_REG1;
+	awvic.mask2 = base + AW_INT_MASK_REG2;
 
 	/* Disable & clear all interrupts */
-	writel(0, AW_INT_ENABLE_REG0);
-	writel(0, AW_INT_ENABLE_REG1);
-	writel(0, AW_INT_ENABLE_REG2);
+	vmm_writel(0, awvic.enable0);
+	vmm_writel(0, awvic.enable1);
+	vmm_writel(0, awvic.enable2);
+	vmm_writel(0xffffffff, awvic.mask0);
+	vmm_writel(0xffffffff, awvic.mask1);
+	vmm_writel(0xffffffff, awvic.mask2);
 
-	writel(0xffffffff, AW_INT_MASK_REG0);
-	writel(0xffffffff, AW_INT_MASK_REG1);
-	writel(0xffffffff, AW_INT_MASK_REG2);
+	/* Clear all pending interrupts */
+	vmm_writel(0xffffffff, awvic.irq_pend0);
+	vmm_writel(0xffffffff, awvic.irq_pend1);
+	vmm_writel(0xffffffff, awvic.irq_pend2);
+	vmm_writel(0xffffffff, awvic.fiq_pend0);
+	vmm_writel(0xffffffff, awvic.fiq_pend1);
+	vmm_writel(0xffffffff, awvic.fiq_pend2);
 
-	writel(0xffffffff, AW_INT_IRQ_PENDING_REG0);
-	writel(0xffffffff, AW_INT_IRQ_PENDING_REG1);
-	writel(0xffffffff, AW_INT_IRQ_PENDING_REG2);
-	writel(0xffffffff, AW_INT_FIQ_PENDING_REG0);
-	writel(0xffffffff, AW_INT_FIQ_PENDING_REG1);
-	writel(0xffffffff, AW_INT_FIQ_PENDING_REG2);
+	/* Enable protection mode */
+	vmm_writel(0x01, awvic.protection);
 
-	/*enable protection mode*/
-	writel(0x01, AW_INT_PROTECTION_REG);
-	/*config the external interrupt source type*/
-	writel(0x00, AW_INT_NMI_CTRL_REG);
+	/* Config the external interrupt source type */
+	vmm_writel(0x00, awvic.nmi_ctrl);
 
+	/* Setup irqdomain and irqchip */
 	for (i = 0; i < AW_NR_IRQS; i++) {
 		vmm_host_irq_set_chip(i, &aw_vic_chip);
 		vmm_host_irq_set_handler(i, vmm_handle_level_irq);
