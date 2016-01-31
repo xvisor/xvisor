46b289f4cf555a9948fb5a905cf34d031d2f3cd8
Jean-Christophe Dubois <jcd@tribudubois.net>
2016-01-31 21:21:18 +0530

    [ARM] Add i.MX6 SMP boot method.
    Cortex A9 cores are started (and reseted) through the SRC device.
    
    This patch adds the required support to set boot address for CPUs and
    to bring them out of sleep state.
    
    Signed-off-by: Jean-Christophe Dubois <jcd@tribudubois.net>

--- arch/arm/board/common/objects.mk
+++ arch/arm/board/common/objects.mk
@@ -29,3 +29,4 @@ board-common-objs-$(CONFIG_ARM_SMP_IPI)+=smp_ipi.o
 board-common-objs-$(CONFIG_ARM_SMP_SPIN_TABLE)+=smp_spin_table.o
 board-common-objs-$(CONFIG_ARM_SCU)+=smp_scu.o
 board-common-objs-$(CONFIG_ARM_SMP_PSCI)+=smp_psci.o
+board-common-objs-$(CONFIG_ARM_SMP_IMX)+=smp_imx.o
---
+++ arch/arm/board/common/smp_imx.c
@@ -0,0 +1,234 @@
+/**
+ * Copyright (c) 2016 Jean-Christophe Dubois.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * @file smp_imx.c
+ * @author Jean-Christophe Dubois (jcd@tribudubois.net)
+ * @brief i.MX SMP boot support
+ *
+ * Adapted from arch/arm/mach-imx/platsmp.c
+ *
+ *  Copyright 2011 Freescale Semiconductor, Inc.
+ *  All Rights Reserved
+ *
+ * The original source is licensed under GPL.
+ */
+
+#include <vmm_error.h>
+#include <vmm_smp.h>
+#include <vmm_cache.h>
+#include <vmm_stdio.h>
+#include <vmm_host_io.h>
+#include <vmm_host_irq.h>
+#include <vmm_host_aspace.h>
+#include <arch_barrier.h>
+
+#include <smp_ops.h>
+
+#define SCU_PM_NORMAL	0
+#define SCU_PM_EINVAL	1
+#define SCU_PM_DORMANT	2
+#define SCU_PM_POWEROFF	3
+
+#define SCU_CTRL		0x00
+#define SCU_CONFIG		0x04
+#define SCU_CPU_STATUS		0x08
+#define SCU_INVALIDATE		0x0c
+#define SCU_FPGA_REVISION	0x10
+
+#define SRC_SCR			0x000
+#define SRC_GPR1		0x020
+#define BP_SRC_SCR_CORE1_RST	14
+#define BP_SRC_SCR_CORE1_ENABLE	22
+
+/*
+ * Get the number of CPU cores from the SCU configuration
+ */
+static u32 scu_get_core_count(void *scu_base)
+{
+	return (vmm_readl(scu_base + SCU_CONFIG) & 0x03) + 1;
+}
+
+static bool smp_imx_core_is_smp(void *scu_base, u32 cpu)
+{
+	return (vmm_readl(scu_base + SCU_CONFIG) >> (4 + cpu)) & 0x01;
+}
+
+/*
+ * Enable the SCU
+ */
+static void scu_enable(void *scu_base)
+{
+	u32 scu_ctrl;
+
+#ifdef CONFIG_ARM_ERRATA_764369
+	/*
+	 * This code is mostly for TEGRA 2 and 3 processors. 
+	 * This in not enabled or tested on Xvisor for now.
+	 * We keep it as we might have to enable it someday.
+	 */
+	/* Cortex-A9 only */
+	if ((read_cpuid(CPUID_ID) & 0xff0ffff0) == 0x410fc090) {
+
+		scu_ctrl = vmm_readl(scu_base + 0x30);
+		if (!(scu_ctrl & 1)) {
+			vmm_writel(scu_ctrl | 0x1, scu_base + 0x30);
+		}
+	}
+#endif
+
+	scu_ctrl = vmm_readl(scu_base + SCU_CTRL);
+	/* already enabled? */
+	if (scu_ctrl & 1) {
+		return;
+	}
+
+	scu_ctrl |= 1;
+	vmm_writel(scu_ctrl, scu_base + SCU_CTRL);
+
+	/*
+	 * Ensure that the data accessed by CPU0 before the SCU was
+	 * initialised is visible to the other CPUs.
+	 */
+	vmm_flush_cache_all();
+}
+
+static virtual_addr_t scu_base;
+
+static const struct vmm_devtree_nodeid scu_matches[] = {
+	{.compatible = "arm,arm11mp-scu"},
+	{.compatible = "arm,cortex-a9-scu"},
+	{ /* end of list */ },
+};
+
+static virtual_addr_t src_base;
+
+static const struct vmm_devtree_nodeid src_matches[] = {
+	{ .compatible = "fsl,imx51-src"},
+	{ .compatible = "fsl,imx6-src"},
+	{ /* sentinel */ }
+};
+
+static int __init smp_imx_init(struct vmm_devtree_node *node,
+				unsigned int cpu)
+{
+	int rc;
+	u32 ncores;
+	struct vmm_devtree_node *scu_node;
+	struct vmm_devtree_node *src_node;
+
+	/* Map SCU base */
+	if (!scu_base) {
+		scu_node = vmm_devtree_find_matching(NULL, scu_matches);
+		if (!scu_node) {
+			return VMM_ENODEV;
+		}
+		rc = vmm_devtree_regmap(scu_node, &scu_base, 0);
+		vmm_devtree_dref_node(scu_node);
+		if (rc) {
+			return rc;
+		}
+	}
+
+	/* Map SRC base */
+	if (!src_base) {
+		src_node = vmm_devtree_find_matching(NULL, src_matches);
+		if (!src_node) {
+			return VMM_ENODEV;
+		}
+		rc = vmm_devtree_regmap(src_node, &src_base, 0);
+		vmm_devtree_dref_node(src_node);
+		if (rc) {
+			return rc;
+		}
+	}
+
+	/* Check core count from SCU */
+	ncores = scu_get_core_count((void *)scu_base);
+	if (ncores <= cpu) {
+		return VMM_ENOSYS;
+	}
+
+	/* Check SCU status */
+	if (!smp_imx_core_is_smp((void *)scu_base, cpu)) {
+		return VMM_ENOSYS;
+	}
+
+	return VMM_OK;
+}
+
+static void smp_imx_set_cpu_jump(int cpu, void *jump_addr)
+{
+	physical_addr_t paddr;
+
+	if (VMM_OK != vmm_host_va2pa((virtual_addr_t)jump_addr,
+				     &paddr)) {
+		vmm_printf("Failed to get cpu jump physical address (0x%X)\n",
+			   jump_addr);
+	}
+	vmm_writel(paddr,
+		       (void *)src_base + SRC_GPR1 + cpu * 8);
+}
+
+static void smp_imx_set_cpu_arg(int cpu, u32 arg)
+{
+	vmm_writel(arg, (void *)src_base + SRC_GPR1 + cpu * 8 + 4);
+}
+
+extern u8 _start_secondary_nopen;
+
+static int __init smp_imx_prepare(unsigned int cpu)
+{
+	/* Enable snooping through SCU */
+	if (scu_base) {
+		scu_enable((void *)scu_base);
+	}
+
+	smp_imx_set_cpu_jump(cpu, (void *)&_start_secondary_nopen);
+
+	smp_imx_set_cpu_arg(cpu, 0);
+
+	return VMM_OK;
+}
+
+static void smp_imx_enable_cpu(int cpu, bool enable)
+{
+	u32 mask, val;
+
+	mask = 1 << (BP_SRC_SCR_CORE1_ENABLE + cpu - 1);
+	val = vmm_readl((void *)src_base + SRC_SCR);
+	val = enable ? val | mask : val & ~mask;
+	val |= 1 << (BP_SRC_SCR_CORE1_RST + cpu - 1);
+	vmm_writel(val, (void *)src_base + SRC_SCR);
+}
+
+static int __init smp_imx_boot(unsigned int cpu)
+{
+	/* Wake up the core through the SRC device */
+	smp_imx_enable_cpu(cpu, true);
+
+	return VMM_OK;
+}
+
+static struct smp_operations smp_imx_ops = {
+	.name = "smp-imx",
+	.cpu_init = smp_imx_init,
+	.cpu_prepare = smp_imx_prepare,
+	.cpu_boot = smp_imx_boot,
+};
+
+SMP_OPS_DECLARE(smp_imx, &smp_imx_ops);
