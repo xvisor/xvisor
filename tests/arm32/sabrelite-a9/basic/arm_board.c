1e0de84335196760cbf9c1c53468215e6d2a04d4
Jimmy Durand Wesolowski <jimmy.durand-wesolowski@openwide.fr>
2016-03-18 20:04:05 +0530

    [TESTS] sabrelite-a9: Remove Virtio Console and default serial console.
    Signed-off-by: Jean Guyomarc'h <jean.guyomarch@openwide.fr>
    Signed-off-by: Jimmy Durand Wesolowski <jimmy.durand-wesolowski@openwide.fr>

--- tests/arm32/sabrelite-a9/basic/arm_board.c
+++ tests/arm32/sabrelite-a9/basic/arm_board.c
@@ -63,9 +63,7 @@ u32 arm_board_linux_machine_type(void)
 
 void arm_board_linux_default_cmdline(char *cmdline, u32 cmdline_sz)
 {
-	arm_strcpy(cmdline, "root=/dev/ram rw earlyprintk console=ttyAMA0");
-	/* VirtIO Console Device */
-	arm_strcat(cmdline, " virtio_mmio.device=4K@0x20300000:86");
+	arm_strcpy(cmdline, "root=/dev/ram rw earlyprintk");
 	/* VirtIO Network Device */
 	arm_strcat(cmdline, " virtio_mmio.device=64K@0x20100000:42");
 	/* SabreLite/Nitrogen6X specific */
