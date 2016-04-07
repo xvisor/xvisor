2da2e44fb9fefddae2932cfff3557ef5e0644c6d
Jimmy Durand Wesolowski <jimmy.durand-wesolowski@openwide.fr>
2016-04-07 21:46:50 +0530

    [TESTS] Continuing i.MX6 guest port.
    Signed-off-by: Jimmy Durand Wesolowski <jimmy.durand-wesolowski@openwide.fr>
    Signed-off-by: Jean Guyomarc'h <jean.guyomarch@openwide.fr>

--- tests/arm32/sabrelite-a9/basic/arm_board.c
+++ tests/arm32/sabrelite-a9/basic/arm_board.c
@@ -53,7 +53,7 @@ u32 arm_board_ram_start(void)
 
 u32 arm_board_ram_size(void)
 {
-	return 0x10000000;
+	return 0x30000000;
 }
 
 u32 arm_board_linux_machine_type(void)
@@ -70,8 +70,8 @@ void arm_board_linux_default_cmdline(char *cmdline, u32 cmdline_sz)
 	arm_strcat(cmdline, " enable_wait_mode=off "
 		   "video=mxcfb0:dev=ldb,LDB-XGA,if=RGB666 video=mxcfb1:off "
 		   "video=mxcfb2:off video=mxcfb3:off fbmem=10M "
-		   "console=ttymxc1,115200 vmalloc=400M consoleblank=0 "
-		   "mxc_hdmi.only_cea=1");
+		   "console=ttymxc1,115200 vmalloc=256M consoleblank=0 "
+		   "mxc_hdmi.only_cea=1 memblock=debug");
 }
 
 u32 arm_board_flash_addr(void)
