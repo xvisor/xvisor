1f625cd26a67fb07a756e79088be8d15aa2ba030
Bhargav Shah <bhargavshah1988@gmail.com>
2017-01-08 15:20:58 +0530

    [ARM] smp_ops: Add support for psci-0.2
    This patch adds PSCI-0.2 support in PSCI SMP OPS.
    
    Signed-off-by: Anup Patel <anup@brainfault.org>
    Signed-off-by: Bhargav Shah <bhargavshah1988@gmail.com>

--- arch/arm/board/common/smp_psci.c
+++ arch/arm/board/common/smp_psci.c
@@ -36,6 +36,18 @@
 #include <psci.h>
 #include <smp_ops.h>
 
+/*
+ * While a 64-bit OS can make calls with SMC32 calling conventions, for some
+ * calls it is necessary to use SMC64 to pass or return 64-bit values.
+ * For such calls PSCI_FN_NATIVE(version, name) will choose the appropriate
+ * (native-width) function ID.
+ */
+#ifdef CONFIG_ARM64
+#define PSCI_FN_NATIVE(version, name)	PSCI_##version##_FN64_##name
+#else
+#define PSCI_FN_NATIVE(version, name)	PSCI_##version##_FN_##name
+#endif
+
 enum psci_function {
 	PSCI_FN_CPU_SUSPEND,
 	PSCI_FN_CPU_ON,
@@ -62,6 +74,41 @@ static int psci_to_xvisor_errno(long errno)
 	};
 }
 
+static int __noinline invoke_psci_fn_smc_raw(unsigned long func,
+					     unsigned long arg0,
+					     unsigned long arg1,
+                                             unsigned long arg2)
+{
+	long ret;
+
+#ifdef CONFIG_ARM64
+        asm volatile(
+                "mov    x0, %1\n\t"
+                "mov    x1, %2\n\t"
+                "mov    x2, %3\n\t"
+                "mov    x3, %4\n\t"
+                "smc    #0    \n\t"
+                "mov    %0, x0\n\t"
+        : "=r" (ret)
+        : "r" (func), "r" (arg0), "r" (arg1), "r" (arg2)
+        : "x0", "x1", "x2", "x3", "cc", "memory");
+#else
+        asm volatile(
+                ".arch_extension sec\n\t"
+                "mov    r0, %1\n\t"
+                "mov    r1, %2\n\t"
+                "mov    r2, %3\n\t"
+                "mov    r3, %4\n\t"
+                "smc    #0    \n\t"
+                "mov    %0, r0\n\t"
+        : "=r" (ret)
+        : "r" (func), "r" (arg0), "r" (arg1), "r" (arg2)
+        : "r0", "r1", "r2", "r3", "cc", "memory");
+#endif
+
+	return ret;
+}
+
 static int __noinline invoke_psci_fn_smc(unsigned long func,
 					 unsigned long arg0,
 					 unsigned long arg1,
@@ -69,30 +116,7 @@ static int __noinline invoke_psci_fn_smc(unsigned long func,
 {
 	long ret;
 
-#ifdef CONFIG_ARM64
-	asm volatile(
-		"mov	x0, %1\n\t"
-		"mov	x1, %2\n\t"
-		"mov	x2, %3\n\t"
-		"mov	x3, %4\n\t"
-		"smc	#0    \n\t"
-		"mov	%0, x0\n\t"
-	: "=r" (ret)
-	: "r" (func), "r" (arg0), "r" (arg1), "r" (arg2)
-	: "x0", "x1", "x2", "x3", "cc", "memory");
-#else
-	asm volatile(
-		".arch_extension sec\n\t"
-		"mov	r0, %1\n\t"
-		"mov	r1, %2\n\t"
-		"mov	r2, %3\n\t"
-		"mov	r3, %4\n\t"
-		"smc	#0    \n\t"
-		"mov	%0, r0\n\t"
-	: "=r" (ret)
-	: "r" (func), "r" (arg0), "r" (arg1), "r" (arg2)
-	: "r0", "r1", "r2", "r3", "cc", "memory");
-#endif
+	ret = invoke_psci_fn_smc_raw(func, arg0, arg1, arg2);
 
 	return psci_to_xvisor_errno(ret);
 }
@@ -121,8 +145,84 @@ int psci_migrate(unsigned long cpuid)
 				  cpuid, 0, 0);
 }
 
+static u32 psci_get_version(void)
+{
+	return invoke_psci_fn_smc_raw(PSCI_0_2_FN_PSCI_VERSION, 0, 0, 0);
+}
+
+static void psci_0_2_set_functions(void)
+{
+	psci_function_id[PSCI_FN_CPU_SUSPEND] =
+				PSCI_FN_NATIVE(0_2, CPU_SUSPEND);
+	psci_function_id[PSCI_FN_CPU_ON] = PSCI_FN_NATIVE(0_2, CPU_ON);
+	psci_function_id[PSCI_FN_CPU_OFF] = PSCI_0_2_FN_CPU_OFF;
+	psci_function_id[PSCI_FN_MIGRATE] = PSCI_FN_NATIVE(0_2, MIGRATE);
+}
+
+int psci_0_2_init(struct vmm_devtree_node *psci)
+{
+	u32 ver = psci_get_version();
+
+	vmm_linfo("psci", "PSCIv%d.%d detected in firmware.\n",
+		  PSCI_VERSION_MAJOR(ver),
+		  PSCI_VERSION_MINOR(ver));
+
+	if (PSCI_VERSION_MAJOR(ver) == 0 && PSCI_VERSION_MINOR(ver) < 2) {
+		vmm_lerror("psci", "Conflicting PSCI version detected.\n");
+		return VMM_EINVALID;
+	}
+
+	psci_0_2_set_functions();
+
+	return VMM_OK;
+}
+
+int psci_0_1_init(struct vmm_devtree_node *psci)
+{
+	int rc;
+
+	/* retrieve the "cpu_on" attribute */
+	rc = vmm_devtree_read_u32(psci, "cpu_on",
+			&psci_function_id[PSCI_FN_CPU_ON]);
+	if (rc) {
+		vmm_printf("%s: Can't find 'cpu_on' attribute\n",
+			   __func__);
+		return VMM_ENOSYS;
+	}
+
+	/* retrieve the "cpu_suspend" attribute */
+	rc = vmm_devtree_read_u32(psci, "cpu_suspend",
+			&psci_function_id[PSCI_FN_CPU_SUSPEND]);
+	if (rc) {
+		vmm_printf("%s: Can't find 'cpu_suspend' attribute\n",
+			   __func__);
+		return VMM_ENOSYS;
+	}
+
+	/* retrieve the "cpu_off" attribute */
+	rc = vmm_devtree_read_u32(psci, "cpu_off",
+			&psci_function_id[PSCI_FN_CPU_OFF]);
+	if (rc) {
+		vmm_printf("%s: Can't find 'cpu_off' attribute\n",
+			   __func__);
+		return VMM_ENOSYS;
+	}
+
+	/* retrieve the "migrate" attribute */
+	rc = vmm_devtree_read_u32(psci, "migrate",
+			&psci_function_id[PSCI_FN_MIGRATE]);
+	if (rc) {
+		vmm_printf("%s: Can't find 'migrate' attribute\n",
+			   __func__);
+		return VMM_ENOSYS;
+	}
+
+	return VMM_OK;
+}
+
 static struct vmm_devtree_nodeid psci_matches[] = {
-	{.compatible = "arm,psci"},
+	{ .compatible = "arm,psci", .data = psci_0_1_init },
+	{ .compatible = "arm,psci-0.2", .data = psci_0_2_init },
 	{ /* end of list */ },
 };
 
@@ -131,6 +231,8 @@ static void __init psci_ops_init(void)
 	int rc;
 	const char *method;
 	struct vmm_devtree_node *psci;
+	const struct vmm_devtree_nodeid *id;
+	int (*psci_init)(struct vmm_devtree_node *);
 
 	/* Look for node with PSCI compatible string */
 	psci = vmm_devtree_find_matching(NULL, psci_matches);
@@ -138,12 +240,13 @@ static void __init psci_ops_init(void)
 		/* Skip if no PSCI node available */
 		return;
 	}
+	id = vmm_devtree_match_node(psci_matches, psci);
+	psci_init = id->data;
 
 	/* it should have a "method" attibute equal to "smc" */
 	rc = vmm_devtree_read_string(psci, "method", &method);
 	if (rc) {
-		vmm_printf("%s: Can't find 'method' attribute\n",
-			   __func__);
+		vmm_lerror("psci", "Can't find 'method' attribute\n");
 		goto done;
 	}
 
@@ -157,39 +260,9 @@ static void __init psci_ops_init(void)
 	}
 #endif
 
-	/* retrieve the "cpu_on" attribute */
-	rc = vmm_devtree_read_u32(psci, "cpu_on",
-			&psci_function_id[PSCI_FN_CPU_ON]);
+	rc = psci_init(psci);
 	if (rc) {
-		vmm_printf("%s: Can't find 'cpu_on' attribute\n",
-			   __func__);
-		goto done;
-	}
-
-	/* retrieve the "cpu_suspend" attribute */
-	rc = vmm_devtree_read_u32(psci, "cpu_suspend",
-			&psci_function_id[PSCI_FN_CPU_SUSPEND]);
-	if (rc) {
-		vmm_printf("%s: Can't find 'cpu_suspend' attribute\n",
-			   __func__);
-		goto done;
-	}
-
-	/* retrieve the "cpu_off" attribute */
-	rc = vmm_devtree_read_u32(psci, "cpu_off",
-			&psci_function_id[PSCI_FN_CPU_OFF]);
-	if (rc) {
-		vmm_printf("%s: Can't find 'cpu_off' attribute\n",
-			   __func__);
-		goto done;
-	}
-
-	/* retrieve the "migrate" attribute */
-	rc = vmm_devtree_read_u32(psci, "migrate",
-			&psci_function_id[PSCI_FN_MIGRATE]);
-	if (rc) {
-		vmm_printf("%s: Can't find 'migrate' attribute\n",
-			   __func__);
+		vmm_lerror("psci", "init failed error %d\n", rc);
 		goto done;
 	}
 
