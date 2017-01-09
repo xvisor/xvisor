53740365e0ffd9dbcd1cf8492c4b58fba9a5357f
Bhargav Shah <bhargavshah1988@gmail.com>
2017-01-09 20:33:04 +0530

    [ARM] defterm: Added support for zynq defterm
    This patch adds defterm support for xilinx zynq.
    
    Signed-off-by: Bhargav Shah <bhargavshah1988@gmail.com>

--- arch/arm/board/common/defterm.c
+++ arch/arm/board/common/defterm.c
@@ -530,6 +530,66 @@ static struct defterm_ops bcm283x_mu_ops = {
 
 #endif
 
+#if defined(CONFIG_SERIAL_ZYNQ_UART)
+
+#include <drv/serial/zynq-uart.h>
+
+struct zynq_uart_priv uart_port;
+
+static int zynq_uart_defterm_getc(u8 *ch)
+{
+	if (!zynq_uart_lowlevel_can_getc(uart_port.regs)) {
+		return VMM_EFAIL;
+	}
+	*ch = zynq_uart_lowlevel_getc(uart_port.regs);
+	return VMM_OK;
+}
+
+static int zynq_uart_defterm_putc(u8 ch)
+{
+	if (!zynq_uart_lowlevel_can_putc(uart_port.regs)) {
+		return VMM_EFAIL;
+	}
+
+	zynq_uart_lowlevel_putc(uart_port.regs, ch);
+	return VMM_OK;
+}
+
+static int __init zynq_uart_defterm_init(struct vmm_devtree_node *node)
+{
+	int rc;
+
+	rc = vmm_devtree_regmap(node, (virtual_addr_t*)&uart_port.regs, 0);
+	if (rc) {
+		return rc;
+	}
+	rc = vmm_devtree_clock_frequency(node,
+				&uart_port.input_clock);
+	if (rc) {
+		return rc;
+	}
+
+	if (vmm_devtree_read_u32(node, "baudrate",
+					&uart_port.baudrate)) {
+		uart_port.baudrate = 115200;
+	}
+
+	zynq_uart_lowlevel_init(&uart_port);
+
+	return VMM_OK;
+}
+
+static struct defterm_ops zynq_uart_ops = {
+	.putc = zynq_uart_defterm_putc,
+	.getc = zynq_uart_defterm_getc,
+	.init = zynq_uart_defterm_init
+};
+#else
+
+#define CONFIG_SERIAL_ZYNQ_UART unknown_ops
+
+#endif
+
 static struct vmm_devtree_nodeid defterm_devid_table[] = {
 	{ .compatible = "arm,pl011", .data = &pl011_ops },
 	{ .compatible = "ns8250", .data = &uart8250_ops },
@@ -548,6 +608,8 @@ static struct vmm_devtree_nodeid defterm_devid_table[] = {
 	{ .compatible = "samsung,exynos4210-uart", .data = &samsung_ops },
 	{ .compatible = "renesas,scif", .data = &scif_ops },
 	{ .compatible = "brcm,bcm283x-mu", .data = &bcm283x_mu_ops },
+	{ .compatible = "cdns,uart-r1p12", .data = &zynq_uart_ops },
+	{ .compatible = "xlnx,xuartps", .data = &zynq_uart_ops },
 	{ /* end of list */ },
 };
 
