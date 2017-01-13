df43a8d5a73aaea2562195d146fda40545100d3b
Bhargav Shah <bhargavshah1988@gmail.com>
2017-01-13 21:15:52 +0530

    [ARM] Add initial DTS files for xilinx zynqmp
    This patch adds initial DTS Files for zynqmp.
    It also includes Xvisor DTS file for zero guest.
    
    Signed-off-by: Bhargav Shah <bhargavshah1988@gmail.com>

---
+++ arch/arm/board/generic/dts/zynqmp/objects.mk
@@ -0,0 +1,24 @@
+#/**
+# Copyright (c) 2017 Bhargav Shah.
+# All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+# @file objects.mk
+# @author Bhargav Shah (bhargavshah1988@gmail.com)
+# @brief list of ZynqMP DTBs.
+# */
+
+board-dtbs-$(CONFIG_ARMV8) += dts/zynqmp/zcu102/zero_guest.dtb
