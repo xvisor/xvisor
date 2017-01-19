886f019ab9546cf9396316cf835f704efd6dbf2d
Tsung-Han Lin <tsunghan.tw@gmail.com>
2017-01-19 21:25:17 +0530

    [ARM] arm64: remove unnecessary memory barrier in cpu_locks.c
    Since spinlock implementation uses ARM's one-way barriers, ldaxr and
    stlr, there is no need to insert explicit memory barrier around the
    codes.
    
    Signed-off-by: Tsung-Han Lin <tsunghan.tw@gmail.com>

--- arch/arm/cpu/arm64/cpu_locks.c
+++ arch/arm/cpu/arm64/cpu_locks.c
@@ -29,6 +29,7 @@
 
 bool __lock arch_spin_lock_check(arch_spinlock_t *lock)
 {
+	arch_smp_mb();
 	return (lock->lock == __ARCH_SPIN_UNLOCKED) ? FALSE : TRUE;
 }
 
@@ -48,8 +49,6 @@ void __lock arch_spin_lock(arch_spinlock_t *lock)
 	: "=&r" (tmp), "+Q" (lock->lock)
 	: "r" (cpu), "r" (__ARCH_SPIN_UNLOCKED)
 	: "cc", "memory");
-
-	arch_smp_mb();
 }
 
 int arch_spin_trylock(arch_spinlock_t *lock)
@@ -70,7 +69,6 @@ int arch_spin_trylock(arch_spinlock_t *lock)
 	: "cc", "memory");
 
 	if (tmp == 0) {
-		arch_smp_mb();	/* do mb if we succeeded */
 		return 1;
 	} else {
 		return 0;
@@ -79,8 +77,6 @@ int arch_spin_trylock(arch_spinlock_t *lock)
 
 void __lock arch_spin_unlock(arch_spinlock_t *lock)
 {
-	arch_smp_mb();
-
 	__asm__ __volatile__(
 "	stlr	%w1, %0\n"
 	: "=Q" (lock->lock) : "r" (__ARCH_SPIN_UNLOCKED)
@@ -89,6 +85,7 @@ void __lock arch_spin_unlock(arch_spinlock_t *lock)
 
 bool __lock arch_write_lock_check(arch_rwlock_t *lock)
 {
+	arch_smp_mb();
 	return (lock->lock & __ARCH_RW_LOCKED) ? TRUE : FALSE;
 }
 
@@ -109,8 +106,6 @@ void __lock arch_write_lock(arch_rwlock_t *lock)
 	: "=&r" (tmp)
 	: "r" (&lock->lock), "r" (__ARCH_RW_LOCKED)
 	: "memory");
-
-	arch_smp_mb();
 }
 
 int __lock arch_write_trylock(arch_rwlock_t *lock)
@@ -127,7 +122,6 @@ int __lock arch_write_trylock(arch_rwlock_t *lock)
 	: "memory");
 
 	if (tmp == 0) {
-		 arch_smp_mb();
 		 return 1;
 	} else {
 		 return 0;
@@ -143,6 +137,7 @@ void __lock arch_write_unlock(arch_rwlock_t *lock)
 
 bool __lock arch_read_lock_check(arch_rwlock_t *lock)
 {
+	arch_smp_mb();
 	return (lock->lock == __ARCH_RW_UNLOCKED) ? FALSE : TRUE;
 }
 
@@ -178,7 +173,6 @@ int __lock arch_read_trylock(arch_rwlock_t *lock)
 	: "memory");
 
 	if (tmp2 == 0) {
-		arch_smp_mb();	/* do mb if we succeeded */
 		return 1;
 	} else {
 		return 0;
@@ -188,8 +182,6 @@ int __lock arch_read_trylock(arch_rwlock_t *lock)
 void __lock arch_read_unlock(arch_rwlock_t *lock)
 {
 	unsigned int tmp, tmp2;
-
-	arch_smp_mb();
 
 	asm volatile(
 	"1:	ldxr	%w0, [%2]\n"
