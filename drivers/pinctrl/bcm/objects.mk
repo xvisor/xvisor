4d5633f5a1b848c1f6324fea7343f4086cbef334
Vincent BONNEVALLE <vincent.bonnevalle@smile.fr>
2017-05-18 22:28:53 +0530

    [DRIVERS] pinctrl: Add BCM2835 pinctrl driver
    This patch adds BCM2835 pinctrl driver ported from Linux sources.
    
    Signed-off-by: Vincent BONNEVALLE <vincent.bonnevalle@smile.fr>
    Signed-off-by: Anup Patel <anup@brainfault.org>

---
+++ drivers/pinctrl/bcm/objects.mk
@@ -0,0 +1,24 @@
+#/**
+# Copyright (c) 2017 Anup Patel.
+# All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+# @file objects.mk
+# @author Anup Patel (anup@brainfault.org)
+# @brief list of Broadcom pinctrl driver objects
+# */
+
+drivers-objs-$(CONFIG_PINCTRL_BCM2835)+= pinctrl/bcm/pinctrl-bcm2835.o
