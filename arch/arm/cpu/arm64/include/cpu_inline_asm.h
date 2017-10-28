732e571e465dd17eea98ed154cb7cbb17ea431e5
Anup Patel <anup@brainfault.org>
2017-10-28 21:10:11 +0530

    [ARM] arm64: Fix cpu_supports_fpu() for VFPv4
    The cpu_supports_fpu() only checks if VFP version is v3 which
    means it fails for VFPv4. This patch fixes cpu_supports_fpu()
    so that it returns true for all VFP version >= v3.
    
    The patch also fixes cpu_supports_asimd() on similar lines.
    
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- arch/arm/cpu/arm64/include/cpu_inline_asm.h
+++ arch/arm/cpu/arm64/include/cpu_inline_asm.h
@@ -175,13 +175,13 @@ asm(
 #define cpu_supports_asimd()	({ u64 pfr0; \
 				   asm volatile("mrs %0, id_aa64pfr0_el1" \
 						: "=r"(pfr0)); \
-				   ((pfr0 & ID_AA64PFR0_ASIMD_MASK) == 0); \
+				   ((pfr0 & ID_AA64PFR0_ASIMD_MASK) != 0xf); \
 				})
 
 #define cpu_supports_fpu()	({ u64 pfr0; \
 				   asm volatile("mrs %0, id_aa64pfr0_el1" \
 						: "=r"(pfr0)); \
-				   ((pfr0 & ID_AA64PFR0_FPU_MASK) == 0); \
+				   ((pfr0 & ID_AA64PFR0_FPU_MASK) != 0xf); \
 				})
 
 #define cpu_supports_el0_a32()	({ u64 pfr0; \
