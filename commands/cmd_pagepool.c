d561fda1a15a81b35a79bbc0288fcfe7f5fbb4bf
Anup Patel <anup@brainfault.org>
2018-06-17 19:14:56 +0530

    COMMANDS: cmd_pageool: Add state sub-command
    This patch adds "state" sub-command to "pagepool" command which
    can be used to display detailed state of each page pool type.
    
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- commands/cmd_pagepool.c
+++ commands/cmd_pagepool.c
@@ -42,6 +42,7 @@ static void cmd_pagepool_usage(struct vmm_chardev *cdev)
 	vmm_cprintf(cdev, "Usage:\n");
 	vmm_cprintf(cdev, "   pagepool help\n");
 	vmm_cprintf(cdev, "   pagepool info\n");
+	vmm_cprintf(cdev, "   pagepool state\n");
 }
 
 static int cmd_pagepool_info(struct vmm_chardev *cdev)
@@ -79,6 +80,58 @@ static int cmd_pagepool_info(struct vmm_chardev *cdev)
 	return VMM_OK;
 }
 
+static int cmd_pagepool_state(struct vmm_chardev *cdev)
+{
+	int i;
+	u32 _entry_count, entry_count = 0;
+	u32 _hugepage_count, hugepage_count = 0;
+	u32 _page_count, page_count = 0;
+	u32 _page_avail_count, page_avail_count = 0;
+	virtual_size_t _space, space = 0;
+
+	vmm_cprintf(cdev, "----------------------------------------"
+			  "---------------------------------------\n");
+
+	vmm_cprintf(cdev, " %-20s %-11s %-10s %-10s %-11s %-11s\n",
+		    "Name", "Space (KB)", "Entries", "Hugepages",
+		    "AvailPages", "TotalPages");
+
+	vmm_cprintf(cdev, "----------------------------------------"
+			  "---------------------------------------\n");
+
+	for (i = 0; i < VMM_PAGEPOOL_MAX; i++) {
+		_space = vmm_pagepool_space(i);
+		_entry_count = vmm_pagepool_entry_count(i);
+		_hugepage_count = vmm_pagepool_hugepage_count(i);
+		_page_count = vmm_pagepool_page_count(i);
+		_page_avail_count = vmm_pagepool_page_avail_count(i);
+
+		vmm_cprintf(cdev, " %-20s %-11d %-10d %-10d %-11d %-11d\n",
+			    vmm_pagepool_name(i), (u32)(_space >> 10),
+			    _entry_count, _hugepage_count,
+			    _page_avail_count, _page_count);
+
+		space += _space;
+		entry_count += _entry_count;
+		hugepage_count += _hugepage_count;
+		page_count += _page_count;
+		page_avail_count += _page_avail_count;
+	}
+
+	vmm_cprintf(cdev, "----------------------------------------"
+			  "---------------------------------------\n");
+
+	vmm_cprintf(cdev, " %-20s %-11d %-10d %-10d %-11d %-11d\n",
+		    "TOTAL", (u32)(space >> 10),
+		    entry_count, hugepage_count,
+		    page_avail_count, page_count);
+
+	vmm_cprintf(cdev, "----------------------------------------"
+			  "---------------------------------------\n");
+
+	return VMM_OK;
+}
+
 static int cmd_pagepool_exec(struct vmm_chardev *cdev, int argc, char **argv)
 {
 	if (argc == 2) {
@@ -87,6 +140,8 @@ static int cmd_pagepool_exec(struct vmm_chardev *cdev, int argc, char **argv)
 			return VMM_OK;
 		} else if (strcmp(argv[1], "info") == 0) {
 			return cmd_pagepool_info(cdev);
+		} else if (strcmp(argv[1], "state") == 0) {
+			return cmd_pagepool_state(cdev);
 		}
 	}
 	cmd_pagepool_usage(cdev);
