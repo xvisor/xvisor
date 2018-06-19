cc597644f0e5b2d3686ce1ab8d258df3fec85ca1
Anup Patel <anup@brainfault.org>
2018-06-19 21:43:28 +0530

    ARM: separate directory for Freescale DTS files
    This patch adds Freescale DTS directory and moves all relevant DTS
    files under it.
    
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- arch/arm/board/generic/dts/freescale/objects.mk
+++ arch/arm/board/generic/dts/freescale/objects.mk
@@ -1,5 +1,5 @@
 #/**
-# Copyright (c) 2014 Anup Patel.
+# Copyright (c) 2014 Jean-Christophe Dubois.
 # All rights reserved.
 #
 # This program is free software; you can redistribute it and/or modify
@@ -9,7 +9,7 @@
 # 
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public License
@@ -17,8 +17,13 @@
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 #
 # @file objects.mk
+# @author Jean-Christophe Dubois (jcd@tribudubois.net)
 # @author Anup Patel (anup@brainfault.org)
-# @brief list of IMX25 DTBs.
+# @brief list of freescale DTBs.
 # */
 
-board-dtbs-$(CONFIG_ARMV5)+=dts/imx25/imx25-pdk.dtb
+board-dtbs-$(CONFIG_ARMV7A)+=dts/freescale/imx6dl-sabrelite.dtb
+board-dtbs-$(CONFIG_ARMV7A)+=dts/freescale/sabrelite/one_guest_sabrelite.dtb
+board-dtbs-$(CONFIG_ARMV7A)+=dts/freescale/sabrelite/two_guest_sabrelite.dtb
+board-dtbs-$(CONFIG_ARMV6)+=dts/freescale/imx31-kzm.dtb
+board-dtbs-$(CONFIG_ARMV5)+=dts/freescale/imx25-pdk.dtb
