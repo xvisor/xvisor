f1dcdd91e54611e009b7f7fda8f57dc077a5ab0a
Anup Patel <anup@brainfault.org>
2018-06-27 22:07:50 +0530

    CORE: Add vmm_const.h for sharing constants between assembly and C
    This patch adds vmm_const.h which contains helper macros and
    define to enable sharing constants between assembly and C sources.
    
    Signed-off-by: Anup Patel <anup@brainfault.org>

---
+++ core/include/vmm_const.h
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2018 Anup Patel.
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * @file vmm_const.h
+ * @author Anup Patel (anup@brainfault.org)
+ * @brief header file for sharing constants between assembly and C code
+ *
+ * The source has been largely adapted from Linux 4.x or higher:
+ * include/uapi/linux/const.h
+ *
+ * The original code is licensed under the GPL.
+ */
+#ifndef _VMM_CONST_H__
+#define _VMM_CONST_H__
+
+/* Some constant macros are used in both assembler and
+ * C code.  Therefore we cannot annotate them always with
+ * 'UL' and other type specifiers unilaterally.  We
+ * use the following macros to deal with this.
+ *
+ * Similarly, _AT() will cast an expression with a type in C, but
+ * leave it unchanged in asm.
+ */
+
+#ifdef __ASSEMBLY__
+#define _AC(X,Y)	X
+#define _AT(T,X)	X
+#else
+#define __AC(X,Y)	(X##Y)
+#define _AC(X,Y)	__AC(X,Y)
+#define _AT(T,X)	((T)(X))
+#endif
+
+#define _UL(x)		(_AC(x, UL))
+#define _ULL(x)		(_AC(x, ULL))
+
+#define _BITUL(x)	(_UL(1) << (x))
+#define _BITULL(x)	(_ULL(1) << (x))
+
+#define UL(x)		(_UL(x))
+#define ULL(x)		(_ULL(x))
+
+#endif
