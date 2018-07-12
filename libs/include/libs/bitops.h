b85e709c03d7be0ebf255140384f85f1149e1c09
Anup Patel <anup@brainfault.org>
2018-07-12 19:18:27 +0530

    LIBS: bitops: Add GENMASK() and GENMASK_ULL() macros
    This patch adds GENMASK() and GENMASK_ULL() macros which help
    generate arbitary bit mask.
    
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- libs/include/libs/bitops.h
+++ libs/include/libs/bitops.h
@@ -51,6 +51,18 @@
 #define BIT_ULL_MASK(nr)		(1ULL << ((nr) % BITS_PER_LONG_LONG))
 #define BIT_ULL_WORD(nr)		((nr) / BITS_PER_LONG_LONG)
 
+/*
+ * Create a contiguous bitmask starting at bit position @l and ending at
+ * position @h. For example
+ * GENMASK_ULL(39, 21) gives us the 64bit vector 0x000000ffffe00000.
+ */
+#define GENMASK(h, l) \
+	(((~0UL) - (1UL << (l)) + 1) & (~0UL >> (BITS_PER_LONG - 1 - (h))))
+
+#define GENMASK_ULL(h, l) \
+	(((~0ULL) - (1ULL << (l)) + 1) & \
+	 (~0ULL >> (BITS_PER_LONG_LONG - 1 - (h))))
+
 /**
  * ffs - Find first bit set
  * @x: the word to search
