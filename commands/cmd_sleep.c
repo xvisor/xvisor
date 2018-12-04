c66d4a92b3c5211af5e832629d7571f66525db73
Anup Patel <anup@brainfault.org>
2018-12-04 18:52:37 +0530

    COMMANDS: sleep: Add more sub-commands
    This patch adds following sub-commands to sleep command:
    secs - Sleep for given number of seconds
    msecs - Sleep for given number of milliseconds
    usecs - Sleep for given number of microseconds
    test_secs - Test sleep seconds several times to measure latency
    test_msecs - Test sleep milliseconds several times to measure latency
    test_usecs - Test sleep microseconds several times to measure latency
    
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- commands/cmd_sleep.c
+++ commands/cmd_sleep.c
@@ -26,7 +26,9 @@
 #include <vmm_version.h>
 #include <vmm_modules.h>
 #include <vmm_cmdmgr.h>
+#include <vmm_timer.h>
 #include <vmm_delay.h>
+#include <libs/mathlib.h>
 
 #define MODULE_DESC			"Command sleep"
 #define MODULE_AUTHOR			"Himanshu Chauhan"
@@ -37,27 +39,141 @@
 
 static void cmd_sleep_usage(struct vmm_chardev *cdev)
 {
-	vmm_cprintf(cdev, "Usage: ");
-	vmm_cprintf(cdev, "   sleep <number of seconds>\n");
+	vmm_cprintf(cdev, "Usage:\n");
+	vmm_cprintf(cdev, "   sleep help\n");
+	vmm_cprintf(cdev, "   sleep secs <number_of_seconds>\n");
+	vmm_cprintf(cdev, "   sleep msecs <number_of_milliseconds>\n");
+	vmm_cprintf(cdev, "   sleep usecs <number_of_microseconds>\n");
+	vmm_cprintf(cdev, "   sleep test_secs <number_of_iterations> "
+			  "<seconds_per_iteration>\n");
+	vmm_cprintf(cdev, "   sleep test_msecs <number_of_iterations> "
+			  "<milliseconds_per_iteration>\n");
+	vmm_cprintf(cdev, "   sleep test_usecs <number_of_iterations> "
+			  "<microseconds_per_iteration>\n");
 }
 
-static int cmd_sleep_exec(struct vmm_chardev *cdev, int argc, char **argv)
+enum cmd_sleep_type {
+	CMD_SLEEP_TYPE_SECS=0,
+	CMD_SLEEP_TYPE_MSECS=1,
+	CMD_SLEEP_TYPE_USECS=2,
+};
+
+static int cmd_sleep_normal(struct vmm_chardev *cdev,
+			    enum cmd_sleep_type type, int val)
 {
-	if (argc < 2) {
-		cmd_sleep_usage(cdev);
-		return VMM_EFAIL;
+	if (val <= 0) {
+		return VMM_EINVALID;
 	}
 
-	int secs = atoi(argv[1]);
-
-	vmm_ssleep(secs);
+	switch (type) {
+	case CMD_SLEEP_TYPE_SECS:
+		vmm_ssleep(val);
+		break;
+	case CMD_SLEEP_TYPE_MSECS:
+		vmm_msleep(val);
+		break;
+	case CMD_SLEEP_TYPE_USECS:
+		vmm_usleep(val);
+		break;
+	default:
+		return VMM_EINVALID;
+	};
 
 	return VMM_OK;
 }
 
+static int cmd_sleep_test(struct vmm_chardev *cdev,
+			  enum cmd_sleep_type type, int iter, int val)
+{
+	int i;
+	u64 delta, avgdelta = 0;
+	u64 mult, start_tstamp, end_tstamp;
+
+	if ((iter <= 0) || (val <= 0)) {
+		return VMM_EINVALID;
+	}
+
+	for (i = 0; i < iter; i++) {
+		vmm_cprintf(cdev, "iter=%d ", i);
+		switch (type) {
+		case CMD_SLEEP_TYPE_SECS:
+			start_tstamp = vmm_timer_timestamp();
+			vmm_ssleep(val);
+			end_tstamp = vmm_timer_timestamp();
+			mult = 1000000000;
+			break;
+		case CMD_SLEEP_TYPE_MSECS:
+			start_tstamp = vmm_timer_timestamp();
+			vmm_msleep(val);
+			end_tstamp = vmm_timer_timestamp();
+			mult = 1000000;
+			break;
+		case CMD_SLEEP_TYPE_USECS:
+			start_tstamp = vmm_timer_timestamp();
+			vmm_usleep(val);
+			end_tstamp = vmm_timer_timestamp();
+			mult = 1000;
+			break;
+		default:
+			return VMM_EINVALID;
+		};
+		delta = (end_tstamp - start_tstamp) - (val * mult);
+		avgdelta += delta;
+		vmm_cprintf(cdev, "delta %"PRId64" nanoseconds\n", delta);
+	}
+
+	avgdelta = udiv64(avgdelta, (u64)iter);
+	vmm_cprintf(cdev, "average delta %"PRId64" nanoseconds\n", avgdelta);
+
+	return VMM_OK;
+}
+
+static int cmd_sleep_exec(struct vmm_chardev *cdev, int argc, char **argv)
+{
+	int rc = VMM_EINVALID, iter, val;
+
+	if (argc == 2) {
+		if (strcmp(argv[1], "help") == 0) {
+			cmd_sleep_usage(cdev);
+			rc = VMM_OK;
+		}
+	} else if (argc == 3) {
+		val = atoi(argv[2]);
+		if (strcmp(argv[1], "secs") == 0) {
+			rc = cmd_sleep_normal(cdev,
+					CMD_SLEEP_TYPE_SECS, val);
+		} else if (strcmp(argv[1], "msecs") == 0) {
+			rc = cmd_sleep_normal(cdev,
+					CMD_SLEEP_TYPE_MSECS, val);
+		} else if (strcmp(argv[1], "usecs") == 0) {
+			rc = cmd_sleep_normal(cdev,
+					CMD_SLEEP_TYPE_USECS, val);
+		}
+	} else if (argc == 4) {
+		iter = atoi(argv[2]);
+		val = atoi(argv[3]);
+		if (strcmp(argv[1], "test_secs") == 0) {
+			rc = cmd_sleep_test(cdev,
+					CMD_SLEEP_TYPE_SECS, iter, val);
+		} else if (strcmp(argv[1], "test_msecs") == 0) {
+			rc = cmd_sleep_test(cdev,
+					CMD_SLEEP_TYPE_MSECS, iter, val);
+		} else if (strcmp(argv[1], "test_usecs") == 0) {
+			rc = cmd_sleep_test(cdev,
+					CMD_SLEEP_TYPE_USECS, iter, val);
+		}
+	}
+
+	if (rc) {
+		cmd_sleep_usage(cdev);
+	}
+
+	return rc;
+}
+
 static struct vmm_cmd cmd_sleep = {
 	.name = "sleep",
-	.desc = "Sleep the terminal thread of execution for given seconds",
+	.desc = "Make the terminal thread sleep for given time",
 	.usage = cmd_sleep_usage,
 	.exec = cmd_sleep_exec,
 };
@@ -73,8 +189,8 @@ static void __exit cmd_sleep_exit(void)
 }
 
 VMM_DECLARE_MODULE(MODULE_DESC,
-			MODULE_AUTHOR,
-			MODULE_LICENSE,
-			MODULE_IPRIORITY,
-			MODULE_INIT,
-			MODULE_EXIT);
+		   MODULE_AUTHOR,
+		   MODULE_LICENSE,
+		   MODULE_IPRIORITY,
+		   MODULE_INIT,
+		   MODULE_EXIT);
