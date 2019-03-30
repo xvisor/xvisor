ab7f70bbcb08e9ef29c04a9b63aef4a0f26da851
Anup Patel <anup@brainfault.org>
2019-03-30 11:51:14 +0530

    RISC-V: Implement page fault handler for normal VCPUs
    This patch provides implementation of page fault handler for
    normal VCPUs. This fault handler will fill-up Stage2 page table
    on-demand. The virtual regions will not be given read-write-execute
    permission which will force load/store access fault on virtual
    regions and we will do all load/store emulation in access fault
    handler.
    
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- arch/riscv/cpu/generic/cpu_vcpu_trap.c
+++ arch/riscv/cpu/generic/cpu_vcpu_trap.c
@@ -22,20 +22,117 @@
  */
 
 #include <vmm_error.h>
+#include <vmm_stdio.h>
+#include <vmm_host_aspace.h>
+#include <vmm_guest_aspace.h>
+#include <libs/stringlib.h>
+#include <cpu_mmu.h>
 #include <cpu_vcpu_trap.h>
 
 int cpu_vcpu_page_fault(struct vmm_vcpu *vcpu,
 			arch_regs_t *regs,
 			unsigned long cause,
-			unsigned long stval)
+			unsigned long fault_addr)
 {
-	return VMM_EFAIL;
+	int rc, rc1;
+	u32 reg_flags = 0x0, pg_reg_flags = 0x0;
+	struct cpu_page pg;
+	physical_addr_t inaddr, outaddr;
+	physical_size_t size, availsz;
+
+	memset(&pg, 0, sizeof(pg));
+
+	inaddr = fault_addr & PGTBL_L0_MAP_MASK;
+	size = PGTBL_L0_BLOCK_SIZE;
+
+	rc = vmm_guest_physical_map(vcpu->guest, inaddr, size,
+				    &outaddr, &availsz, &reg_flags);
+	if (rc) {
+		vmm_printf("%s: guest_phys=0x%"PRIPADDR" size=0x%"PRIPSIZE
+			   " map failed\n", __func__, inaddr, size);
+		return rc;
+	}
+
+	if (availsz < PGTBL_L0_BLOCK_SIZE) {
+		vmm_printf("%s: availsz=0x%"PRIPSIZE" insufficent for "
+			   "guest_phys=0x%"PRIPADDR"\n",
+			   __func__, availsz, inaddr);
+		return VMM_ENOSPC;
+	}
+
+	pg.ia = inaddr;
+	pg.sz = size;
+	pg.oa = outaddr;
+	pg_reg_flags = reg_flags;
+
+	if (reg_flags & (VMM_REGION_ISRAM | VMM_REGION_ISROM)) {
+		inaddr = fault_addr & PGTBL_L1_MAP_MASK;
+		size = PGTBL_L1_BLOCK_SIZE;
+		rc = vmm_guest_physical_map(vcpu->guest, inaddr, size,
+				    &outaddr, &availsz, &reg_flags);
+		if (!rc && (availsz >= PGTBL_L1_BLOCK_SIZE)) {
+			pg.ia = inaddr;
+			pg.sz = size;
+			pg.oa = outaddr;
+			pg_reg_flags = reg_flags;
+		}
+#ifdef CONFIG_64BIT
+		inaddr = fault_addr & PGTBL_L2_MAP_MASK;
+		size = PGTBL_L2_BLOCK_SIZE;
+		rc = vmm_guest_physical_map(vcpu->guest, inaddr, size,
+				    &outaddr, &availsz, &reg_flags);
+		if (!rc && (availsz >= PGTBL_L2_BLOCK_SIZE)) {
+			pg.ia = inaddr;
+			pg.sz = size;
+			pg.oa = outaddr;
+			pg_reg_flags = reg_flags;
+		}
+#endif
+	}
+
+	pg.user = 1;
+	if (pg_reg_flags & VMM_REGION_VIRTUAL) {
+		pg.read = 0;
+		pg.write = 0;
+		pg.execute = 0;
+	} else if (pg_reg_flags & VMM_REGION_READONLY) {
+		pg.read = 1;
+		pg.write = 0;
+		pg.execute = 1;
+	} else {
+		pg.read = 1;
+		pg.write = 1;
+		pg.execute = 1;
+	}
+	pg.valid = 1;
+
+	/* Try to map the page in Stage2 */
+	rc = cpu_mmu_map_page(riscv_guest_priv(vcpu->guest)->pgtbl, &pg);
+	if (rc) {
+		/* On SMP Guest, two different VCPUs may try to map same
+		 * Guest region in Stage2 at the same time. This may cause
+		 * cpu_mmu_map_page() to fail for one of the Guest VCPUs.
+		 *
+		 * To take care of this situation, we recheck Stage2 mapping
+		 * when cpu_mmu_map_page() fails.
+		 */
+		memset(&pg, 0, sizeof(pg));
+		rc1 = cpu_mmu_get_page(riscv_guest_priv(vcpu->guest)->pgtbl,
+				       fault_addr, &pg);
+		if (rc1) {
+			return rc1;
+		}
+		rc = VMM_OK;
+	}
+
+	return rc;
 }
 
 int cpu_vcpu_access_fault(struct vmm_vcpu *vcpu,
 			  arch_regs_t *regs,
 			  unsigned long cause,
-			  unsigned long stval)
+			  unsigned long fault_addr)
 {
+	/* TODO: */
 	return VMM_EFAIL;
 }
