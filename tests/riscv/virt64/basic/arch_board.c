3f220f4b078b7078f52b4fe11ffd09025f212fc0
Anup Patel <anup@brainfault.org>
2019-04-02 13:56:24 +0530

    TESTS: riscv: Use earlycon=uart8250 for virt32 and virt64 guest
    Instead of earlycon=sbi, prefer earlycon=uart8250 in kernel args
    passed to Linux on virt32 and virt64 guest types. This is because
    final console for Linux on virt32 and virt64 guest types will be
    uart8250 but Xvisor will emulate uart8250 and sbi console as
    logically different vmm_vserial instances.
    
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- tests/riscv/virt64/basic/arch_board.c
+++ tests/riscv/virt64/basic/arch_board.c
@@ -6,12 +6,12 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
@@ -90,7 +90,7 @@ physical_size_t arch_board_ram_size(void)
 void arch_board_linux_default_cmdline(char *cmdline, u32 cmdline_sz)
 {
 	basic_strcpy(cmdline, "root=/dev/ram rw "
-			      "earlycon=sbi "
+			      "earlycon=uart8250,mmio,0x10000000 "
 			      "console=ttyS0,115200");
 }
 
