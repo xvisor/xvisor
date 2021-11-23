f5a5656675811a369f4c573edd917a29c49e8844
Anup Patel <anup@brainfault.org>
2021-11-23 18:05:01 +0530

    TESTS: common: Update default linux and busybox version in scripts
    Use latest linux and busybox version in the release tarball scripts.
    
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- tests/common/scripts/build-images.sh
+++ tests/common/scripts/build-images.sh
@@ -22,8 +22,8 @@ BUILD_OUTPUT_PATH=`pwd`/build
 BUILD_INSTALL_PATH=`pwd`/install
 BUILD_XVISOR_SOURCE_PATH=`pwd`
 BUILD_TARBALL_PATH=`pwd`/tarball
-BUILD_LINUX_VERSION="5.4.68"
-BUILD_BUSYBOX_VERSION="1.32.0"
+BUILD_LINUX_VERSION="5.15.3"
+BUILD_BUSYBOX_VERSION="1.33.1"
 
 while getopts "d:hj:l:i:o:p:" o; do
 	case "${o}" in
